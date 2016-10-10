ba3672cfcbea16639819221a22a991452d21ddb0
Salvatore Orlando <salv.orlando@gmail.com>
2016-10-11 00:33:37 +0200

    Kubernetes client function for namespaces and policies
    Add functions to watch and retrieve kubernetes namespaces and
    network policies.
    
    The patch also includes unit tests to cover the newly added functions.
    
    Signed-off-by: Salvatore Orlando <salv.orlando@gmail.com>

--- ovn_k8s/tests/unit/test_kubernetes.py
+++ ovn_k8s/tests/unit/test_kubernetes.py
@@ -36,6 +36,10 @@ class MockResponse(object):
     def __bool__(self):
         return self.status_code == 200
 
+    @classmethod
+    def ok_from_dict(cls, data):
+        return cls(json.dumps(data))
+
 
 class TestKubernetes(unittest.TestCase):
 
@@ -68,7 +72,21 @@ class TestKubernetes(unittest.TestCase):
     def test_watch_endpoints(self):
         self._test_watch('services', kubernetes.watch_services)
 
-    def _test_get_pod_annotations(self, annotations):
+    def test_watch_namespaces(self):
+        self._test_watch('namespaces', kubernetes.watch_namespaces)
+
+    def test_watch_network_policies(self):
+        with mock.patch('requests.get') as req_mock:
+            kubernetes.watch_network_policies('http://meh:8080', 'meh_ns')
+            self._verify_mock_call(
+                req_mock,
+                'http://meh:8080/apis/extensions/v1beta1/'
+                'namespaces/meh_ns/networkpolicies?watch=true',
+                headers=self.exp_headers,
+                stream=True)
+
+    def _test_get_annotations(self, annotations, namespace, get_func,
+                              resource=None, resource_id=None):
         if annotations:
             ann_data = {'annotations': annotations}
         else:
@@ -77,14 +95,26 @@ class TestKubernetes(unittest.TestCase):
         with mock.patch('requests.get') as req_mock:
             req_mock.return_value = MockResponse(
                 '{"metadata": %s}' % json.dumps(ann_data or {}))
-            ann = kubernetes.get_pod_annotations('http://meh:8080',
-                                                 'meh_ns',
-                                                 'meh_pod')
-            self._verify_mock_call(
-                req_mock,
-                'http://meh:8080/api/v1/namespaces/meh_ns/pods/meh_pod',
-                headers=self.exp_headers)
+            if resource:
+                ann = get_func('http://meh:8080', namespace, resource_id)
+                self._verify_mock_call(
+                    req_mock,
+                    'http://meh:8080/api/v1/namespaces/%s/%s/%s' % (
+                        namespace, resource, resource_id),
+                    headers=self.exp_headers)
+            else:
+                ann = get_func('http://meh:8080', namespace)
+                self._verify_mock_call(
+                    req_mock,
+                    'http://meh:8080/api/v1/namespaces/%s' % namespace,
+                    headers=self.exp_headers)
+
             self.assertEqual(annotations, ann)
+
+    def _test_get_pod_annotations(self, annotations):
+        self._test_get_annotations(annotations, 'meh_ns',
+                                   kubernetes.get_pod_annotations,
+                                   'pods', 'meh_pod')
 
     def test_get_pod_annotations(self):
         self._test_get_pod_annotations({'foo': 'bar'})
@@ -103,6 +133,29 @@ class TestKubernetes(unittest.TestCase):
             self._verify_mock_call(
                 req_mock,
                 'http://meh:8080/api/v1/namespaces/meh_ns/pods/meh_pod',
+                headers=self.exp_headers)
+            self.assertIsNone(ann)
+
+    def _test_get_ns_annotations(self, annotations):
+        self._test_get_annotations(annotations, 'meh_ns',
+                                   kubernetes.get_ns_annotations)
+
+    def test_get_ns_annotations(self):
+        self._test_get_ns_annotations({'foo': 'bar'})
+
+    def test_get_ns_annotations_empty(self):
+        self._test_get_ns_annotations(None)
+
+    def test_get_ns_annotation_failure(self):
+        # NOTE: this method currently returns None in case of failure. No info
+        # about the failure is returned. This might change in the future.
+        with mock.patch('requests.get') as req_mock:
+            req_mock.return_value = MockResponse("Error", status_code=404)
+            ann = kubernetes.get_ns_annotations('http://meh:8080',
+                                                'meh_ns')
+            self._verify_mock_call(
+                req_mock,
+                'http://meh:8080/api/v1/namespaces/meh_ns',
                 headers=self.exp_headers)
             self.assertIsNone(ann)
 
@@ -162,6 +215,31 @@ class TestKubernetes(unittest.TestCase):
                               kubernetes.get_service, response_code=500,
                               expected_exc=Exception)
 
+    def _test_get_namespace(self, namespace, response_code=200,
+                            expected_exc=None):
+        with mock.patch('requests.get') as req_mock:
+            req_mock.return_value = MockResponse("{}", response_code)
+            if not expected_exc:
+                kubernetes.get_namespace('http://meh:8080', namespace)
+                self._verify_mock_call(
+                    req_mock,
+                    'http://meh:8080/api/v1/namespaces/%s' % namespace,
+                    headers=self.exp_headers)
+            else:
+                self.assertRaises(expected_exc, kubernetes.get_namespace,
+                                  'http://meh:8080', namespace)
+
+    def test_get_namespace(self):
+        self._test_get_namespace('meh_ns')
+
+    def test_get_non_existent_namespace_raises_NotFound(self):
+        self._test_get_namespace('meh_ns', response_code=404,
+                                 expected_exc=exceptions.NotFound)
+
+    def test_get_namespace_failure_raises_Exception(self):
+        self._test_get_namespace('meh_ns', response_code=500,
+                                 expected_exc=Exception)
+
     def _test_get_all_objects(self, resource, get_func):
         with mock.patch('requests.get') as req_mock:
             get_func('http://meh:8080')
@@ -175,6 +253,67 @@ class TestKubernetes(unittest.TestCase):
 
     def test_get_all_pods(self):
         self._test_get_all_objects('pods', kubernetes.get_all_pods)
+
+    def test_get_pods_by_namespacse(self):
+        with mock.patch('requests.get') as req_mock:
+            kubernetes.get_pods_by_namespace('http://meh:8080', 'meh_ns')
+            self._verify_mock_call(
+                req_mock,
+                'http://meh:8080/api/v1/namespaces/meh_ns/pods',
+                headers=self.exp_headers,
+                params={})
+
+    def test_get_pods_by_namespace_with_selector(self):
+        pod_selector = {'meh': ['maybe', 'dunno']}
+        with mock.patch('requests.get') as req_mock:
+            kubernetes.get_pods_by_namespace('http://meh:8080', 'meh_ns',
+                                             pod_selector=pod_selector)
+            self._verify_mock_call(
+                req_mock,
+                'http://meh:8080/api/v1/namespaces/meh_ns/pods',
+                headers=self.exp_headers,
+                params={'labelSelector': ['meh in (maybe,dunno)']})
+
+    def test_get_network_policies(self):
+        with mock.patch('requests.get') as req_mock:
+            kubernetes.get_network_policies('http://meh:8080', 'meh_ns')
+            self._verify_mock_call(
+                req_mock,
+                'http://meh:8080/apis/extensions/v1beta1/namespaces/'
+                'meh_ns/networkpolicies',
+                headers=self.exp_headers)
+
+    def test_is_namespace_isolated_no_annotations(self):
+        with mock.patch(
+                'ovn_k8s.common.kubernetes.get_ns_annotations') as req_mock:
+            req_mock.return_value = None
+            self.assertFalse(kubernetes.is_namespace_isolated(
+                'http://meh:8080', 'meh'))
+
+    def test_is_namespace_isolated_no_isolation_annotation(self):
+        with mock.patch(
+                'ovn_k8s.common.kubernetes.get_ns_annotations') as req_mock:
+            req_mock.return_value = {'some_ann': 'some_value'}
+            self.assertFalse(kubernetes.is_namespace_isolated(
+                'http://meh:8080', 'meh'))
+
+    def test_is_namespace_isolated_ingress_default_deny(self):
+        with mock.patch(
+                'ovn_k8s.common.kubernetes.get_ns_annotations') as req_mock:
+            req_mock.return_value = {
+                kubernetes.K8S_ISOLATION_ANN:
+                json.dumps({'ingress': {'isolation': 'DefaultDeny'}})}
+            self.assertTrue(kubernetes.is_namespace_isolated(
+                'http://meh:8080', 'meh'))
+
+    def test_is_namespace_isolated_ingress_other(self):
+        with mock.patch(
+                'ovn_k8s.common.kubernetes.get_ns_annotations') as req_mock:
+            req_mock.return_value = {
+                kubernetes.K8S_ISOLATION_ANN:
+                json.dumps({'ingress': {'isolation': 'meh'}})}
+            self.assertFalse(kubernetes.is_namespace_isolated(
+                'http://meh:8080', 'meh'))
 
 
 class TestKubernetesAuth(TestKubernetes):
