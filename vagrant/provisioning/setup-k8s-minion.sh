d1de836cefaad8f42c6b27acc669a90278a6d8b7
Gurucharan Shetty <guru@ovn.org>
2017-01-13 01:01:59 -0800

    vagrant: Remove commented out code.
    Signed-off-by: Gurucharan Shetty <guru@ovn.org>

--- vagrant/provisioning/setup-k8s-minion.sh
+++ vagrant/provisioning/setup-k8s-minion.sh
@@ -9,20 +9,6 @@ set -o xtrace
 
 MASTER_IP=$1
 
-# Install k8s
-
-# Download Kubernetes ... yes, it's huge
-#mkdir k8s
-#pushd k8s
-#wget https://github.com/kubernetes/kubernetes/releases/download/v1.3.7/kubernetes.tar.gz
-#tar xvzf kubernetes.tar.gz
-
-# Now untar kubernetes-server-linux-amd64.tar.gz
-#mkdir server
-#cd server
-#tar xvzf ../kubernetes/server/linux/kubernetes-server-linux-amd64.tar.gz
-#popd
-
 # Install CNI
 pushd ~/
 wget https://github.com/containernetworking/cni/releases/download/v0.2.0/cni-v0.2.0.tgz
