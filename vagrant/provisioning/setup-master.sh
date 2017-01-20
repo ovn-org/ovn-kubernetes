ae4ecfb2d323fc3eb2e0b5f0611d19dbbe2630c0
Gurucharan Shetty <guru@ovn.org>
2017-01-20 13:19:46 -0800

    vagrant: Use SSL as the default connectivity for OVN.
    Signed-off-by: Gurucharan Shetty <guru@ovn.org>

--- vagrant/provisioning/setup-master.sh
+++ vagrant/provisioning/setup-master.sh
@@ -15,6 +15,9 @@ GW_IP=$2
 MASTER_NAME=$3
 MASTER_SUBNET=$4
 
+# Comment out the next line, if you prefer TCP instead of SSL.
+SSL="true"
+
 # FIXME(mestery): Remove once Vagrant boxes allow apt-get to work again
 sudo rm -rf /var/lib/apt/lists/*
 
@@ -52,17 +55,48 @@ sudo /etc/init.d/openvswitch-switch force-reload-kmod
 sudo /usr/share/openvswitch/scripts/ovn-ctl stop_northd
 sudo /usr/share/openvswitch/scripts/ovn-ctl start_northd
 
-sudo ovn-nbctl set-connection ptcp:6641
-sudo ovn-sbctl set-connection ptcp:6642
+if [ -n "$SSL" ]; then
+    # Install SSL certificates
+    pushd /etc/openvswitch
+    sudo ovs-pki -d /vagrant/pki init --force
+    sudo ovs-pki req ovnsb && sudo ovs-pki self-sign ovnsb
+    sudo ovn-sbctl set-ssl /etc/openvswitch/ovnsb-privkey.pem \
+        /etc/openvswitch/ovnsb-cert.pem  /vagrant/pki/switchca/cacert.pem
 
-sudo ovs-vsctl set Open_vSwitch . external_ids:ovn-remote="tcp:$OVERLAY_IP:6642" \
-                                  external_ids:ovn-nb="tcp:$OVERLAY_IP:6641" \
+    sudo ovs-pki req ovnnb && sudo ovs-pki self-sign ovnnb
+    sudo ovn-nbctl set-ssl /etc/openvswitch/ovnnb-privkey.pem \
+        /etc/openvswitch/ovnnb-cert.pem  /vagrant/pki/switchca/cacert.pem
+
+    sudo ovs-pki req ovncontroller
+    sudo ovs-pki -b -d /vagrant/pki sign ovncontroller switch
+    popd
+
+    sudo ovn-nbctl set-connection pssl:6641
+    sudo ovn-sbctl set-connection pssl:6642
+
+    sudo ovs-vsctl set Open_vSwitch . external_ids:ovn-remote="ssl:$OVERLAY_IP:6642" \
+                                  external_ids:ovn-nb="ssl:$OVERLAY_IP:6641" \
                                   external_ids:ovn-encap-ip=$OVERLAY_IP \
                                   external_ids:ovn-encap-type=geneve
 
+    # Set ovn-controller SSL options in /etc/default/ovn-host
+    sudo bash -c 'cat >> /etc/default/ovn-host <<EOF
+OVN_CTL_OPTS="--ovn-controller-ssl-key=/etc/openvswitch/ovncontroller-privkey.pem  --ovn-controller-ssl-cert=/etc/openvswitch/ovncontroller-cert.pem --ovn-controller-ssl-bootstrap-ca-cert=/etc/openvswitch/ovnsb-ca.cert"
+EOF'
+
+else
+    # Plain TCP.
+    sudo ovn-nbctl set-connection tcp:6641
+    sudo ovn-sbctl set-connection tcp:6642
+
+    sudo ovs-vsctl set Open_vSwitch . external_ids:ovn-remote="tcp:$OVERLAY_IP:6642" \
+                                  external_ids:ovn-nb="tcp:$OVERLAY_IP:6641" \
+                                  external_ids:ovn-encap-ip=$OVERLAY_IP \
+                                  external_ids:ovn-encap-type=geneve
+fi
+
 # Re-start OVN controller
-sudo /usr/share/openvswitch/scripts/ovn-ctl stop_controller
-sudo /usr/share/openvswitch/scripts/ovn-ctl start_controller
+sudo /etc/init.d/ovn-host restart
 
 # Set k8s API server IP
 sudo ovs-vsctl set Open_vSwitch . external_ids:k8s-api-server="0.0.0.0:8080"
