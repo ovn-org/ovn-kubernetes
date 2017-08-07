bf0a26ab718a56282d77b31403923374672af6b0
Dan Williams <dcbw@redhat.com>
2017-08-07 12:54:07 -0500

    go-controller: add support for SSL-based connections
    Signed-off-by: Dan Williams <dcbw@redhat.com>

--- go-controller/cmd/ovnkube/ovnkube.go
+++ go-controller/cmd/ovnkube/ovnkube.go
@@ -7,21 +7,49 @@ import (
 	"strings"
 
 	"github.com/Sirupsen/logrus"
+
 	"k8s.io/client-go/kubernetes"
 	restclient "k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	certutil "k8s.io/client-go/util/cert"
 
 	ovnfactory "github.com/openvswitch/ovn-kubernetes/go-controller/pkg/factory"
+	ovncluster "github.com/openvswitch/ovn-kubernetes/go-controller/pkg/cluster"
 )
 
 func main() {
 	// auth flags
 	kubeconfig := flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	server := flag.String("apiserver", "https://localhost:8443", "url to the kubernetes apiserver")
-	rootCAFile := flag.String("ca-cert", "", "CA cert for the api server")
+	server := flag.String("apiserver", "https://localhost:8443", "URL to the Kubernetes apiserver")
+	rootCAFile := flag.String("ca-cert", "", "CA cert for the Kubernetes api server")
 	token := flag.String("token", "", "Bearer token to use for establishing ovn infrastructure")
 	clusterSubnet := flag.String("cluster-subnet", "11.11.0.0/16", "Cluster wide IP subnet to use")
+
+	// IP address and port of the northbound API server
+	ovnNorth := flag.String("ovn-north-db", "", "IP address and port of the OVN northbound API (eg, ssl://1.2.3.4:6641).  Leave empty to use a local unix socket.")
+
+	// SSL-related flags for securing the northbound API server
+	ovnNorthServerPrivKey := flag.String("ovn-north-server-privkey", "", "Private key that the OVN northbound API should use for securing the API.  Leave empty to use local unix socket.")
+	ovnNorthServerCert := flag.String("ovn-north-server-cert", "", "Server certificate that the OVN northbound API should use for securing the API.  Leave empty to use local unix socket.")
+	ovnNorthServerCACert := flag.String("ovn-north-server-cacert", "", "CA certificate that the OVN northbound API should use for securing the API.  Leave empty to use local unix socket.")
+
+	// SSL-related flags for clients connecting to the northbound API
+	ovnNorthClientPrivKey := flag.String("ovn-north-client-privkey", "", "Private key that the client should use for talking to the OVN database.  Leave empty to use local unix socket.")
+	ovnNorthClientCert := flag.String("ovn-north-client-cert", "", "Client certificate that the client should use for talking to the OVN database.  Leave empty to use local unix socket.")
+	ovnNorthClientCACert := flag.String("ovn-north-client-cacert", "", "CA certificate that the client should use for talking to the OVN database.  Leave empty to use local unix socket.")
+
+	// IP address and port of the southbound database server
+	ovnSouth := flag.String("ovn-south-db", "", "IP address and port of the OVN southbound database (eg, ssl://1.2.3.4:6642).")
+
+	// SSL-related flags for securing the southbound database server
+	ovnSouthServerPrivKey := flag.String("ovn-south-server-privkey", "", "Private key that the OVN southbound database should use for securing the API.")
+	ovnSouthServerCert := flag.String("ovn-south-server-cert", "", "Server certificate that the OVN southbound database should use for securing the API.")
+	ovnSouthServerCACert := flag.String("ovn-south-server-cacert", "", "CA certificate that the OVN southbound database should use for securing the API.")
+
+	// SSL-related flags for clients connecting to the southbound database
+	ovnSouthClientPrivKey := flag.String("ovn-south-client-privkey", "", "Private key that the client should use for talking to the OVN database.")
+	ovnSouthClientCert := flag.String("ovn-south-client-cert", "", "Client certificate that the client should use for talking to the OVN database.")
+	ovnSouthClientCACert := flag.String("ovn-south-client-cacert", "", "CA certificate that the client should use for talking to the OVN database.")
 
 	// mode flags
 	netController := flag.Bool("net-controller", false, "Flag to start the central controller that watches pods/services/policies")
@@ -66,7 +94,28 @@ func main() {
 		if err != nil {
 			panic(err.Error)
 		}
+
+		clusterController.NorthDBClientAuth, err = ovncluster.NewOvnDBAuth(*ovnNorth, *ovnNorthClientPrivKey, *ovnNorthClientCert, *ovnNorthClientCACert, false)
+		if err != nil {
+			panic(err.Error())
+		}
+		clusterController.SouthDBClientAuth, err = ovncluster.NewOvnDBAuth(*ovnSouth, *ovnSouthClientPrivKey, *ovnSouthClientCert, *ovnSouthClientCACert, false)
+		if err != nil {
+			panic(err.Error())
+		}
 	}
+
+	if *master != "" {
+		clusterController.NorthDBServerAuth, err = ovncluster.NewOvnDBAuth(*ovnNorth, *ovnNorthServerPrivKey, *ovnNorthServerCert, *ovnNorthServerCACert, true)
+		if err != nil {
+			panic(err.Error())
+		}
+		clusterController.SouthDBServerAuth, err = ovncluster.NewOvnDBAuth(*ovnSouth, *ovnSouthServerPrivKey, *ovnSouthServerCert, *ovnSouthServerCACert, true)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
 	ovnController := factory.CreateOvnController()
 
 	if *node != "" {
--- go-controller/pkg/cluster/cluster.go
+++ go-controller/pkg/cluster/cluster.go
@@ -1,7 +1,9 @@
 package cluster
 
 import (
+	"fmt"
 	"net"
+	"net/url"
 
 	"github.com/openshift/origin/pkg/util/netutils"
 	"github.com/openvswitch/ovn-kubernetes/go-controller/pkg/kube"
@@ -19,10 +21,105 @@ type OvnClusterController struct {
 	ClusterIPNet     *net.IPNet
 	HostSubnetLength uint32
 
+	NorthDBServerAuth *OvnDBAuth
+	NorthDBClientAuth *OvnDBAuth
+
+	SouthDBServerAuth *OvnDBAuth
+	SouthDBClientAuth *OvnDBAuth
+
 	StartNodeWatch func(handler cache.ResourceEventHandler)
 }
+
+// OvnDBScheme describes the OVN database connection transport method
+type OvnDBScheme string
 
 const (
 	// OvnHostSubnet is the constant string representing the annotation key
 	OvnHostSubnet = "ovn_host_subnet"
+
+	// OvnDBSchemeSSL specifies SSL as the OVN database transport method
+	OvnDBSchemeSSL  OvnDBScheme = "ssl"
+	// OvnDBSchemeTCP specifies TCP as the OVN database transport method
+	OvnDBSchemeTCP  OvnDBScheme = "tcp"
+	// OvnDBSchemeUnix specifies Unix domains sockets as the OVN database transport method
+	OvnDBSchemeUnix OvnDBScheme = "unix"
 )
+
+// OvnDBAuth describes an OVN database location and authentication method
+type OvnDBAuth struct {
+	URL string
+	PrivKey string
+	Cert string
+	CACert string
+
+	server bool
+	host   string
+	port   string
+	scheme OvnDBScheme
+}
+
+// NewOvnDBAuth returns an OvnDBAuth object describing the connection to an
+// OVN database, given a connection description string and authentication
+// details
+func NewOvnDBAuth(urlString, privkey, cert, cacert string, server bool) (*OvnDBAuth, error) {
+	if urlString == "" {
+		if privkey != "" || cert != "" || cacert != "" {
+			return nil, fmt.Errorf("certificate or key given; perhaps you mean to use the 'ssl' scheme?")
+		}
+		return &OvnDBAuth{
+			server: server,
+			scheme: OvnDBSchemeUnix,
+		}, nil
+	}
+
+	url, err := url.Parse(urlString)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse OVN DB URL %q: %v", urlString, err)
+	}
+	host, port, err := net.SplitHostPort(url.Host)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse OVN DB host/port %q: %v", url.Host, err)
+	}
+	// OVN requires the --db argument to be an IP, not a DNS name
+	ip := net.ParseIP(host)
+	if ip == nil {
+		return nil, fmt.Errorf("OVN DB host %q must be an IP address, not a DNS name", url.Host)
+	}
+
+	var scheme OvnDBScheme
+	switch {
+	case url.Scheme == "ssl":
+		scheme = OvnDBSchemeSSL
+		if privkey == "" || cert == "" || cacert == "" {
+			return nil, fmt.Errorf("must specify private key, certificate, and CA certificate for 'ssl' scheme")
+		}
+	case url.Scheme == "tcp":
+		scheme = OvnDBSchemeTCP
+		if privkey != "" || cert != "" || cacert != "" {
+			return nil, fmt.Errorf("certificate or key given; perhaps you mean to use the 'ssl' scheme?")
+		}
+	default:
+		return nil, fmt.Errorf("unknown OVN DB scheme %q", url.Scheme)
+	}
+
+	return &OvnDBAuth{
+		URL:     urlString,
+		PrivKey: privkey,
+		Cert:    cert,
+		CACert:  cacert,
+		server:  server,
+		host:    host,
+		port:    port,
+		scheme:  scheme,
+	}, nil
+}
+
+// GetURL returns a URL suitable for passing to ovn-northd which describes the
+// transport mechanism for connection to the database
+func (a *OvnDBAuth) GetURL() string {
+	// FIXME: support specific IP Addresses or non-default Unix socket paths
+	if a.server {
+		return fmt.Sprintf("p%s:%s", a.scheme, a.port)
+	}
+	return fmt.Sprintf("%s:%s:%s", a.scheme, a.host, a.port)
+}
