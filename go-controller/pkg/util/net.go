00b81b1f4dbafbebce01e909021e8b2aebc5839a
Rajat Chopra <rchopra@redhat.com>
2018-01-10 19:02:40 -0500

    fix random to be random enough across multiple initializations
    Signed-off-by: Rajat Chopra <rchopra@redhat.com>

--- go-controller/pkg/util/net.go
+++ go-controller/pkg/util/net.go
@@ -5,12 +5,14 @@ import (
 	"math/big"
 	"math/rand"
 	"net"
+	"time"
 )
 
 // GenerateMac generates mac address.
 func GenerateMac() string {
 	prefix := "00:00:00"
-	mac := fmt.Sprintf("%s:%02X:%02X:%02X", prefix, rand.Intn(255), rand.Intn(255), rand.Intn(255))
+	newRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	mac := fmt.Sprintf("%s:%02X:%02X:%02X", prefix, newRand.Intn(255), newRand.Intn(255), newRand.Intn(255))
 	return mac
 }
 
