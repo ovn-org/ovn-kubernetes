220ac95126a0d54b5f0217e2a0a66dd3c5e229e9
Dan Williams <dcbw@redhat.com>
2018-02-13 21:41:41 -0600

    go-controller: consolidate common kubeclient creation code
    Signed-off-by: Dan Williams <dcbw@redhat.com>

--- go-controller/pkg/util/kube.go
+++ go-controller/pkg/util/kube.go
@@ -1,24 +1,48 @@
 package util
 
 import (
-	restclient "k8s.io/client-go/rest"
-	certutil "k8s.io/client-go/util/cert"
+	"fmt"
+	"strings"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/cert"
+
+	"github.com/openvswitch/ovn-kubernetes/go-controller/pkg/config"
 )
 
-// CreateConfig creates the restclient.Config object from token, ca file and the apiserver
-// (similar to what is obtainable from kubeconfig file)
-func CreateConfig(server, token, rootCAFile string) (*restclient.Config, error) {
-	tlsClientConfig := restclient.TLSClientConfig{}
-	if rootCAFile != "" {
-		if _, err := certutil.NewPool(rootCAFile); err != nil {
+// NewClientset creates a Kubernetes clientset from either a kubeconfig,
+// TLS properties, or an apiserver URL
+func NewClientset(conf *config.KubernetesConfig) (*kubernetes.Clientset, error) {
+	var kconfig *rest.Config
+	var err error
+
+	if conf.Kubeconfig != "" {
+		// uses the current context in kubeconfig
+		kconfig, err = clientcmd.BuildConfigFromFlags("", conf.Kubeconfig)
+	} else if strings.HasPrefix(conf.APIServer, "https") {
+		if conf.APIServer == "" || conf.Token == "" || conf.CACert == "" {
+			return nil, fmt.Errorf("TLS-secured apiservers require token and CA certificate")
+		}
+
+		if _, err := cert.NewPool(conf.CACert); err != nil {
 			return nil, err
 		}
-		tlsClientConfig.CAFile = rootCAFile
+
+		kconfig = &rest.Config{
+			Host:            conf.APIServer,
+			BearerToken:     conf.Token,
+			TLSClientConfig: rest.TLSClientConfig{CAFile: conf.CACert},
+		}
+	} else if strings.HasPrefix(conf.APIServer, "http") {
+		kconfig, err = clientcmd.BuildConfigFromFlags(conf.APIServer, "")
+	} else {
+		err = fmt.Errorf("a kubeconfig file or server/token/tls credentials are required")
+	}
+	if err != nil {
+		return nil, err
 	}
 
-	return &restclient.Config{
-		Host:            server,
-		BearerToken:     token,
-		TLSClientConfig: tlsClientConfig,
-	}, nil
+	return kubernetes.NewForConfig(kconfig)
 }
