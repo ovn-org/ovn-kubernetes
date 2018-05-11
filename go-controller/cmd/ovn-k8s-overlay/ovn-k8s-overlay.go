8314cdffc3d7c9039a955ef473f0a1fc299e970c
Rajat Chopra <rchopra@redhat.com>
2018-05-10 20:52:12 -0400

    add a version flag to config so --version shows the latest release number
    Signed-off-by: Rajat Chopra <rchopra@redhat.com>

--- go-controller/cmd/ovn-k8s-overlay/ovn-k8s-overlay.go
+++ go-controller/cmd/ovn-k8s-overlay/ovn-k8s-overlay.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"github.com/openvswitch/ovn-kubernetes/go-controller/cmd/ovn-k8s-overlay/app"
+	"github.com/openvswitch/ovn-kubernetes/go-controller/pkg/config"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
@@ -12,7 +13,7 @@ func main() {
 	c := cli.NewApp()
 	c.Name = "ovn-k8s-overlay"
 	c.Usage = "run ovn-k8s-overlay to init master, minion, gateway"
-	c.Version = "0.0.2"
+	c.Version = config.Version
 
 	c.Commands = []cli.Command{
 		app.InitGatewayCmd,
