240aa34d0c4c192a9b5bf991c172799f9f5f66dc
Rajat Chopra <rchopra@redhat.com>
2018-06-14 16:30:32 -0400

    cosmetic changes
    Signed-off-by: Rajat Chopra <rchopra@redhat.com>

--- go-controller/pkg/cni/cniserver_windows.go
+++ go-controller/pkg/cni/cniserver_windows.go
@@ -6,11 +6,15 @@ import (
 	"fmt"
 	"net"
 
+	"github.com/sirupsen/logrus"
+
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	utilwait "k8s.io/apimachinery/pkg/util/wait"
 )
 
-// Start the Server's local HTTP server on a root-owned Unix domain socket.
+// Start the Server's local HTTP server on a TCP port
+// TODO: use HTTPS for better security
+// TODO: even better to use named pipes instead
 // requestFunc will be called to handle pod setup/teardown operations on each
 // request to the Server's HTTP server, and should return a PodResult
 // when the operation has completed.
@@ -26,10 +30,16 @@ func (s *Server) Start(requestFunc cniRequestFunc) error {
 	}
 
 	s.SetKeepAlivesEnabled(false)
+	// TODO: We should have the ovnkube as a service on Windows as well so it can benefit from automatic restarts and so on.
+	// We have two options:
+	//  - use service wrappers to be able to register ovnkube as a Windows service;
+	//  - add support in ovn-kubernetes such that ovnkube will be able to run without additional wrappers as a Windows service (example for kubernetes: kubernetes/kubernetes#60144 )
+
 	go utilwait.Forever(func() {
 		if err := s.Serve(l); err != nil {
 			utilruntime.HandleError(fmt.Errorf("CNI server Serve() failed: %v", err))
 		}
 	}, 0)
+	logrus.Infof("CNI server started")
 	return nil
 }
