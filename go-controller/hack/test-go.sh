d941489d3be2e0548f91c852b747de8f35633210
Alin-Gheorghe Balutoiu <alinbalutoiu@gmail.com>
2018-11-15 18:49:04 +0100

    Return proper exit code for make check
    In the test-go.sh the last command is popd which will be the exit
    code of the script. This means that even if running the actual tests
    will fail, the exit code of the script will always be 0.
    
    This change will also fix travis ci to fail when tests are failing.
    
    Signed-off-by: Alin-Gheorghe Balutoiu <alinbalutoiu@gmail.com>

--- go-controller/hack/test-go.sh
+++ go-controller/hack/test-go.sh
@@ -16,5 +16,7 @@ fi
 GO=`which go`
 # sudo is required because some testcases manipulate network namespaces
 sudo -E bash -c "umask 0; CGO_ENABLED=0 ${GO} test "${goflags[@]:+${goflags[@]}}" ${PKGS}"
+retcode=$?
 
 popd
+exit $retcode
