f7f597c32557e8cc245ad526a78d42ca50509d70
Girish Moodalbail <gmoodalbail@nvidia.com>
2019-04-16 14:42:09 -0700

    daemonset: parameterize daemonset.sh to generate custom yaml
    Adds following options
     --image
     --image-pull-policy
     --net-cidr
     --svc-cidr
     --k8s-apiserver
    
    to daemonset.sh so that one could expand the template yaml files to
    their specificaton. When none of the above options is provided, then it
    will fallback to default behavior.
    
    Signed-off-by: Girish Moodalbail <gmoodalbail@nvidia.com>

--- dist/images/daemonset.sh
+++ dist/images/daemonset.sh
@@ -1,26 +1,74 @@
 #!/bin/bash
 #set -x
 
+#Always exit on errors
+set -e
+
 # This is people that are nut using the ansible install.
 # The script expands the templates into yaml files in ../yaml
 
+OVN_IMAGE=""
+OVN_IMAGE_PULL_POLICY=""
+OVN_NET_CIDR=""
+OVN_SVC_DIDR=""
+ONV_K8S_APISERVER=""
+
+# Parse parameters given as arguments to this script.
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        --image)
+            OVN_IMAGE=$VALUE
+            ;;
+        --image-pull-policy)
+            OVN_IMAGE_PULL_POLICY=$VALUE
+            ;;
+        --net-cidr)
+            OVN_NET_CIDR=$VALUE
+            ;;
+        --svc-cidr)
+            OVN_SVC_CIDR=$VALUE
+            ;;
+        --k8s-apiserver)
+            OVN_K8S_APISERVER=$VALUE
+            ;;
+        *)
+            echo "WARNING: unknown parameter \"$PARAM\""
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+# The options provided on the CLI overrides the values in ../ansible/hosts
 
 # Create the daemonsets with the desired image
 # The image name is from ../ansible/hosts
 # The daemonset.yaml files are templates in ../ansible/templates
 # They are expanded into daemonsets in ../yaml
 
-image=$(awk -F = '/^ovn_image=/{ print $2 }' ../ansible/hosts | sed 's/\"//g')
-if [[ ${image} == "" ]]
+if [[ ${OVN_IMAGE} == "" ]]
 then
-  image="docker.io/ovnkube/ovn-daemonset:latest"
+    image=$(awk -F = '/^ovn_image=/{ print $2 }' ../ansible/hosts | sed 's/\"//g')
+    if [[ ${image} == "" ]]
+    then
+      image="docker.io/ovnkube/ovn-daemonset:latest"
+    fi
+else
+    image=$OVN_IMAGE
 fi
 echo "image: ${image}"
 
-policy=$(awk -F = '/^ovn_image_pull_policy/{ print $2 }' ../ansible/hosts)
-if [[ ${policy} == "" ]]
+if [[ ${OVN_IMAGE_PULL_POLICY} == "" ]]
 then
-  policy="IfNotPresent"
+    policy=$(awk -F = '/^ovn_image_pull_policy/{ print $2 }' ../ansible/hosts)
+    if [[ ${policy} == "" ]]
+    then
+      policy="IfNotPresent"
+    fi
+else
+    policy=$OVN_IMAGE_PULL_POLICY
 fi
 echo "imagePullPolicy: ${policy}"
 
@@ -41,25 +89,40 @@ s,${policy_str},${policy}," ../templates/ovnkube-db.yaml.j2 > ../yaml/ovnkube-db
 # net_cidr=10.128.0.0/14/23
 # svc_cidr=172.30.0.0/16
 
-net_cidr=$(awk -F = '/^net_cidr=/{ print $2 }' ../ansible/hosts)
-svc_cidr=$(awk -F = '/^svc_cidr=/{ print $2 }' ../ansible/hosts)
+if [[ ${OVN_NET_CIDR} == "" ]]
+then
+    net_cidr=$(awk -F = '/^net_cidr=/{ print $2 }' ../ansible/hosts)
+    if [[ ${net_cidr} == "" ]]
+    then
+      net_cidr="10.128.0.0/14/23"
+    fi
+else
+    net_cidr=$OVN_NET_CIDR
+fi
 
-if [[ ${net_cidr} == "" ]]
+if [[ ${OVN_SVC_CIDR} == "" ]]
 then
-  net_cidr="10.128.0.0/14/23"
+    svc_cidr=$(awk -F = '/^svc_cidr=/{ print $2 }' ../ansible/hosts)
+    if [[ ${svc_cidr} == "" ]]
+    then
+      svc_cidr="172.30.0.0/16"
+    fi
+else
+    svc_cidr=$OVN_SVC_CIDR
 fi
-if [[ ${svc_cidr} == "" ]]
-then
-  svc_cidr="172.30.0.0/16"
-fi
+
+k8s_apiserver=${OVN_K8S_APISERVER:-10.0.2.16:6443}
 
 net_cidr_repl="{{ net_cidr | default('10.128.0.0/14/23') }}"
 svc_cidr_repl="{{ svc_cidr | default('172.30.0.0/16') }}"
+k8s_apiserver_repl="{{ k8s_apiserver.stdout }}"
 
 echo "net_cidr: ${net_cidr}"
 echo "svc_cidr: ${svc_cidr}"
+echo "k8s_apiserver: ${k8s_apiserver}"
 
 sed "s,${net_cidr_repl},${net_cidr},
-s,${svc_cidr_repl},${svc_cidr}," ../templates/ovn-setup.yaml.j2 > ../yaml/ovn-setup.yaml
+s,${svc_cidr_repl},${svc_cidr},
+s,${k8s_apiserver_repl},${k8s_apiserver}," ../templates/ovn-setup.yaml.j2 > ../yaml/ovn-setup.yaml
 
 exit 0
