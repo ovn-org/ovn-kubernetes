ovn-org/ovn-kubernetes

commit 8ddd5fb1d1bd651f3058a2d064ad6310e423c454

ovnkube.sh: idempotently set endpoints
This was crashing when the daemonset restarted, because it would try and create
endpoints that already exist. Change it to "kubectl apply"

dist/images/ovnkube.sh
@@ -498,8 +498,8 @@ cleanup-ovs-server () {
   /usr/share/openvswitch/scripts/ovs-ctl stop
 }
 
-# create the ovnkube_db endpoint for other pods to query the OVN DB IP
-create_ovnkube_db_ep () {
+# set the ovnkube_db endpoint for other pods to query the OVN DB IP
+set_ovnkube_db_ep () {
   # delete any endpoint by name ovnkube-db
   kubectl --server=${K8S_APISERVER} --token=${k8s_token} --certificate-authority=${K8S_CACERT} \
     delete ep -n ${ovn_kubernetes_namespace} ovnkube-db 2>/dev/null
@@ -507,7 +507,7 @@ create_ovnkube_db_ep () {
   # create a new endpoint for the headless onvkube-db service without selectors
   # using the current host has the endpoint IP
   ovn_db_host=$(getent ahosts $(hostname) | head -1 | awk '{ print $1 }')
-  kubectl --server=${K8S_APISERVER} --token=${k8s_token} --certificate-authority=${K8S_CACERT} create -f - << EOF
+  kubectl --server=${K8S_APISERVER} --token=${k8s_token} --certificate-authority=${K8S_CACERT} apply -f - << EOF
 apiVersion: v1
 kind: Endpoints
 metadata:
@@ -580,7 +580,7 @@ sb-ovsdb () {
   ovn-sbctl set-connection ptcp:${ovn_sb_port} -- set connection . inactivity_probe=0
 
   # create the ovnkube_db endpoint for other pods to query the OVN DB IP
-  create_ovnkube_db_ep
+  set_ovnkube_db_ep
 
   tail --follow=name /var/log/openvswitch/ovsdb-server-sb.log &
   ovn_tail_pid=$!

commit 6ba616bcc2d2cd4dbc00272815683f18eb128541

remove unnecessary deletion of endpoint before we apply the endpoint
Fixes: 8ddd5fb1d1bd (ovnkube.sh: idempotently set endpoints)

Signed-off-by: Yun Zhou <yunz@nvidia.com>
Signed-off-by: Girish Moodalbail <gmoodalbail@nvidia.com>

dist/images/ovndb-vip.sh
@@ -62,12 +62,8 @@ check_ovn_daemonset_version () {
   exit 1
 }
 
-# create the ovnkube_db endpoint for other pods to query the OVN DB IP
-create_ovnkube_db_ep () {
-  # delete any endpoint by name ovnkube-db
-  kubectl --server=${K8S_APISERVER} --token=${k8s_token} --certificate-authority=${K8S_CACERT} \
-    delete ep -n ${ovn_kubernetes_namespace} ovnkube-db 2>/dev/null
-
+# set the ovnkube_db endpoint for other pods to query the OVN DB IP
+set_ovnkube_db_ep () {
   # create a new endpoint for the headless onvkube-db service without selectors
   # using the provided VIP
   kubectl --server=${K8S_APISERVER} --token=${k8s_token} --certificate-authority=${K8S_CACERT} apply -f - << EOF
@@ -216,7 +212,7 @@ run-ovndb () {
 
         # now we can create the ovnkube-db endpoint with the ${ovndb_vip} as the IP address. with that
         # the waiting ovnkube-node and ovnkube-master PODs will continue
-        create_ovnkube_db_ep
+        set_ovnkube_db_ep
       elif [[ $ret -eq 1 ]]; then
 	echo pcs node unstandby ${ovn_pod_host}
         pcs node unstandby ${ovn_pod_host}

dist/images/ovnkube.sh
@@ -500,10 +500,6 @@ cleanup-ovs-server () {
 
 # set the ovnkube_db endpoint for other pods to query the OVN DB IP
 set_ovnkube_db_ep () {
-  # delete any endpoint by name ovnkube-db
-  kubectl --server=${K8S_APISERVER} --token=${k8s_token} --certificate-authority=${K8S_CACERT} \
-    delete ep -n ${ovn_kubernetes_namespace} ovnkube-db 2>/dev/null
-
   # create a new endpoint for the headless onvkube-db service without selectors
   # using the current host has the endpoint IP
   ovn_db_host=$(getent ahosts $(hostname) | head -1 | awk '{ print $1 }')