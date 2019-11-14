ovn-org/ovn-kubernetes

commit 59c177d3c3263dc27428f8c37c75b8fc0d0f7608

Cleaning up error/logging/panic handling in different places

go-controller/pkg/cluster/node.go
@@ -28,41 +28,30 @@ func isOVNControllerReady(name string) (bool, error) {
 
 	pid, err := ioutil.ReadFile(runDir + "ovn-controller.pid")
 	if err != nil {
-		logrus.Errorf("unknown pid for ovn-controller process: %v", err)
-		return false, err
+		return false, fmt.Errorf("unknown pid for ovn-controller process: %v", err)
 	}
 
-	if err := wait.PollImmediate(500*time.Millisecond,
-		300*time.Second,
-		func() (bool, error) {
-			ctlFile := runDir + fmt.Sprintf("ovn-controller.%s.ctl",
-				strings.TrimSuffix(string(pid), "\n"))
-			ret, _, err := util.RunOVSAppctl("-t", ctlFile,
-				"connection-status")
-			if err == nil {
-				logrus.Infof("node %s connection status = %s",
-					name, ret)
-				return ret == "connected", nil
-			}
-			return false, err
-		}); err != nil {
-		logrus.Errorf("timed out waiting sbdb for node %s: %v", name, err)
+	err = wait.PollImmediate(500*time.Millisecond, 300*time.Second, func() (bool, error) {
+		ctlFile := runDir + fmt.Sprintf("ovn-controller.%s.ctl", strings.TrimSuffix(string(pid), "\n"))
+		ret, _, err := util.RunOVSAppctl("-t", ctlFile, "connection-status")
+		if err == nil {
+			logrus.Infof("node %s connection status = %s", name, ret)
+			return ret == "connected", nil
+		}
 		return false, err
+	})
+	if err != nil {
+		return false, fmt.Errorf("timed out waiting sbdb for node %s: %v", name, err)
 	}
 
-	if err := wait.PollImmediate(500*time.Millisecond,
-		300*time.Second,
-		func() (bool, error) {
-			flows, _, err := util.RunOVSOfctl("dump-flows", "br-int")
-			if err == nil {
-				return len(flows) > 0, nil
-			}
-			return false, err
-		}); err != nil {
-		logrus.Errorf("timed out dumping br-int flow entries for node %s: %v",
-			name, err)
-		return false, err
+	err = wait.PollImmediate(500*time.Millisecond, 300*time.Second, func() (bool, error) {
+		flows, _, err := util.RunOVSOfctl("dump-flows", "br-int")
+		return len(flows) > 0, err
+	})
+	if err != nil {
+		return false, fmt.Errorf("timed out dumping br-int flow entries for node %s: %v", name, err)
 	}
+
 	return true, nil
 }
 
@@ -74,7 +63,6 @@ func (cluster *OvnClusterController) StartClusterNode(name string) error {
 	var subnet *net.IPNet
 	var clusterSubnets []string
 	var cidr string
-	var connected bool
 
 	var wg sync.WaitGroup
 
@@ -85,25 +73,22 @@ func (cluster *OvnClusterController) StartClusterNode(name string) error {
 	messages := make(chan error)
 
 	// First wait for the node logical switch to be created by the Master, timeout is 300s.
-	if err := wait.PollImmediate(500*time.Millisecond, 300*time.Second, func() (bool, error) {
-		node, err = cluster.Kube.GetNode(name)
-		if err != nil {
-			logrus.Errorf("Error starting node %s, no node found - %v", name, err)
-			return false, nil
+	err = wait.PollImmediate(500*time.Millisecond, 300*time.Second, func() (bool, error) {
+		if node, err = cluster.Kube.GetNode(name); err != nil {
+			return false, fmt.Errorf("error retrieving node %s: %v", name, err)
 		}
 		if cidr, _, err = util.RunOVNNbctl("get", "logical_switch", node.Name, "other-config:subnet"); err != nil {
-			return false, nil
+			return false, fmt.Errorf("error retrieving logical switch: %v", err)
 		}
 		return true, nil
-	}); err != nil {
-		logrus.Errorf("timed out waiting for node %q logical switch: %v", name, err)
-		return err
+	})
+	if err != nil {
+		return fmt.Errorf("timed out waiting for node's: %q logical switch: %v", name, err)
 	}
 
 	_, subnet, err = net.ParseCIDR(cidr)
 	if err != nil {
-		logrus.Errorf("Invalid hostsubnet found for node %s - %v", node.Name, err)
-		return err
+		return fmt.Errorf("invalid hostsubnet found for node %s: %v", node.Name, err)
 	}
 
 	logrus.Infof("Node %s ready for ovn initialization with subnet %s", node.Name, subnet.String())
@@ -118,12 +103,8 @@ func (cluster *OvnClusterController) StartClusterNode(name string) error {
 		return err
 	}
 
-	connected, err = isOVNControllerReady(name)
-	if err != nil {
+	if _, err = isOVNControllerReady(name); err != nil {
 		return err
-	}
-	if !connected {
-		return nil
 	}
 
 	type readyFunc func(string) (bool, error)
@@ -138,10 +119,8 @@ func (cluster *OvnClusterController) StartClusterNode(name string) error {
 	wg.Add(len(readyFuncs))
 
 	// Set management port macAddress as annotation
-	err = cluster.Kube.SetAnnotationsOnNode(node, mgmtPortAnnotations)
-	if err != nil {
-		logrus.Errorf("Failed to set node %s mgmt port macAddress annotation: %v", node.Name, mgmtPortAnnotations)
-		return err
+	if err := cluster.Kube.SetAnnotationsOnNode(node, mgmtPortAnnotations); err != nil {
+		return fmt.Errorf("Failed to set node %s mgmt port macAddress annotation: %v", node.Name, mgmtPortAnnotations)
 	}
 
 	portName := "k8s-" + node.Name
@@ -163,8 +142,7 @@ func (cluster *OvnClusterController) StartClusterNode(name string) error {
 
 	for i := range messages {
 		if i != nil {
-			logrus.Errorf("Timeout error while obtaining addresses for %s (%v)", portName, i)
-			return i
+			return fmt.Errorf("Timeout error while obtaining addresses for %s (%v)", portName, i)
 		}
 	}
 
@@ -200,14 +178,15 @@ func validateOVNConfigEndpoint(ep *kapi.Endpoints) bool {
 
 }
 
-func updateOVNConfig(ep *kapi.Endpoints) {
+func updateOVNConfig(ep *kapi.Endpoints) error {
 	if !validateOVNConfigEndpoint(ep) {
-		logrus.Errorf("endpoint %s is not in the right format to configure OVN", ep.Name)
-		return
+		return fmt.Errorf("endpoint %s is not of the right format to configure OVN", ep.Name)
 	}
+
 	var southboundDBPort string
 	var northboundDBPort string
 	var masterIPList []string
+
 	for _, ovnDB := range ep.Subsets[0].Ports {
 		if ovnDB.Name == "south" {
 			southboundDBPort = strconv.Itoa(int(ovnDB.Port))
@@ -216,22 +195,23 @@ func updateOVNConfig(ep *kapi.Endpoints) {
 			northboundDBPort = strconv.Itoa(int(ovnDB.Port))
 		}
 	}
+
 	for _, address := range ep.Subsets[0].Addresses {
 		masterIPList = append(masterIPList, address.IP)
 	}
-	err := config.UpdateOVNNodeAuth(masterIPList, southboundDBPort, northboundDBPort)
-	if err != nil {
-		logrus.Errorf(err.Error())
-		return
+
+	if err := config.UpdateOVNNodeAuth(masterIPList, southboundDBPort, northboundDBPort); err != nil {
+		return err
 	}
+
 	for _, auth := range []config.OvnAuthConfig{config.OvnNorth, config.OvnSouth} {
 		if err := auth.SetDBAuth(); err != nil {
-			logrus.Errorf(err.Error())
-			return
+			return err
 		}
 		logrus.Infof("OVN databases reconfigured, masterIP %s, northbound-db %s, southbound-db %s", ep.Subsets[0].Addresses[0].IP, northboundDBPort, southboundDBPort)
 	}
 
+	return nil
 }
 
 //watchConfigEndpoints starts the watching of Endpoint resource and calls back to the appropriate handler logic
@@ -241,17 +221,19 @@ func (cluster *OvnClusterController) watchConfigEndpoints() error {
 			AddFunc: func(obj interface{}) {
 				ep := obj.(*kapi.Endpoints)
 				if ep.Name == "ovnkube-db" {
-					updateOVNConfig(ep)
-					return
+					if err := updateOVNConfig(ep); err != nil {
+						logrus.Errorf(err.Error())
+					}
 				}
 			},
 			UpdateFunc: func(old, new interface{}) {
 				epNew := new.(*kapi.Endpoints)
 				epOld := old.(*kapi.Endpoints)
 				if !reflect.DeepEqual(epNew.Subsets, epOld.Subsets) && epNew.Name == "ovnkube-db" {
-					updateOVNConfig(epNew)
+					if err := updateOVNConfig(epNew); err != nil {
+						logrus.Errorf(err.Error())
+					}
 				}
-
 			},
 		}, nil)
 	return err

go-controller/cmd/ovnkube/ovnkube.go
@@ -138,8 +138,7 @@ func setupPIDFile(pidfile string) error {
 		// get the pid and see if it exists
 		pid, err := ioutil.ReadFile(pidfile)
 		if err != nil {
-			logrus.Errorf("pidfile %s exists but can't be read", pidfile)
-			return err
+			return fmt.Errorf("pidfile %s exists but can't be read: %v", pidfile, err)
 		}
 		_, err1 := os.Stat("/proc/" + string(pid[:]) + "/cmdline")
 		if os.IsNotExist(err1) {
@@ -148,8 +147,7 @@ func setupPIDFile(pidfile string) error {
 				logrus.Errorf("failed to write pidfile %s (%v). Ignoring..", pidfile, err)
 			}
 		} else {
-			logrus.Errorf("pidfile %s exists and ovnkube is running", pidfile)
-			os.Exit(1)
+			return fmt.Errorf("pidfile %s exists and ovnkube is running", pidfile)
 		}
 	}
 
@@ -160,32 +158,30 @@ func runOvnKube(ctx *cli.Context) error {
 	pidfile := ctx.String("pidfile")
 	if pidfile != "" {
 		defer delPidfile(pidfile)
-		err := setupPIDFile(pidfile)
-		if err != nil {
+		if err := setupPIDFile(pidfile); err != nil {
 			return err
 		}
 	}
+
 	exec := kexec.New()
-	_, err := config.InitConfig(ctx, exec, nil)
-	if err != nil {
+	if _, err := config.InitConfig(ctx, exec, nil); err != nil {
 		return err
 	}
 
-	if err = util.SetExec(exec); err != nil {
-		logrus.Errorf("Failed to initialize exec helper: %v", err)
-		return err
+	if err := util.SetExec(exec); err != nil {
+		return fmt.Errorf("failed to initialize exec helper: %v", err)
 	}
 
 	clientset, err := util.NewClientset(&config.Kubernetes)
 	if err != nil {
-		panic(err.Error())
+		return err
 	}
 
 	// create factory and start the controllers asked for
 	stopChan := make(chan struct{})
 	factory, err := factory.NewWatchFactory(clientset, stopChan)
 	if err != nil {
-		panic(err.Error())
+		return err
 	}
 
 	master := ctx.String("init-master")
@@ -194,14 +190,17 @@ func runOvnKube(ctx *cli.Context) error {
 	cleanupNode := ctx.String("cleanup-node")
 	if cleanupNode != "" {
 		if master != "" || node != "" {
-			panic("Cannot specify cleanup-node together with 'init-node or 'init-master'.")
+			return fmt.Errorf("cannot specify cleanup-node together with 'init-node or 'init-master'")
 		}
 
 		if err := ovncluster.CleanupClusterNode(cleanupNode); err != nil {
-			logrus.Errorf(err.Error())
-			panic(err.Error())
+			return err
 		}
 		return nil
+	}
+
+	if master == "" && node == "" {
+		return fmt.Errorf("need to run ovnkube in either master and/or node mode")
 	}
 
 	// start the prometheus server
@@ -209,36 +208,29 @@ func runOvnKube(ctx *cli.Context) error {
 		ovncluster.StartMetricsServer(config.Kubernetes.MetricsBindAddress)
 	}
 
-	if master != "" || node != "" {
-		if master != "" {
-			if runtime.GOOS == "windows" {
-				panic("Windows is not supported as master node")
-			}
-
-			// run the HA master controller to init the master
-			ovnHAController := ovn.NewHAMasterController(clientset, factory, master)
-			err := ovnHAController.StartHAMasterController()
-			if err != nil {
-				logrus.Errorf(err.Error())
-				panic(err.Error())
-			}
+	if master != "" {
+		if runtime.GOOS == "windows" {
+			return fmt.Errorf("Windows is not supported as master node")
 		}
 
-		if node != "" {
-			if config.Kubernetes.Token == "" {
-				panic("Cannot initialize node without service account 'token'. Please provide one with --k8s-token argument")
-			}
-			clusterController := ovncluster.NewClusterController(clientset, factory)
-			err := clusterController.StartClusterNode(node)
-			if err != nil {
-				logrus.Errorf(err.Error())
-				panic(err.Error())
-			}
+		// run the HA master controller to init the master
+		ovnHAController := ovn.NewHAMasterController(clientset, factory, master)
+		if err := ovnHAController.StartHAMasterController(); err != nil {
+			return err
 		}
-
-		// run forever
-		select {}
 	}
 
-	return fmt.Errorf("need to run ovnkube in either master and/or node mode")
+	if node != "" {
+		if config.Kubernetes.Token == "" {
+			return fmt.Errorf("cannot initialize node without service account 'token'. Please provide one with --k8s-token argument")
+		}
+
+		clusterController := ovncluster.NewClusterController(clientset, factory)
+		if err := clusterController.StartClusterNode(node); err != nil {
+			return err
+		}
+	}
+
+	// run forever
+	select {}
 }

go-controller/pkg/config/config.go
@@ -274,14 +274,14 @@ func RestoreDefaultConfig() {
 // copy members of struct 'src' into the corresponding field in struct 'dst'
 // if the field in 'src' is a non-zero int or a non-zero-length string. This
 // function should be called with pointers to structs.
-func overrideFields(dst, src interface{}) {
+func overrideFields(dst, src interface{}) error {
 	dstStruct := reflect.ValueOf(dst).Elem()
 	srcStruct := reflect.ValueOf(src).Elem()
 	if dstStruct.Kind() != srcStruct.Kind() || dstStruct.Kind() != reflect.Struct {
-		panic("mismatched value types")
+		return fmt.Errorf("mismatched value types")
 	}
 	if dstStruct.NumField() != srcStruct.NumField() {
-		panic("mismatched struct types")
+		return fmt.Errorf("mismatched struct types")
 	}
 
 	// Iterate over each field in dst/src Type so we can get the tags,
@@ -301,12 +301,10 @@ func overrideFields(dst, src interface{}) {
 		dstField := dstStruct.FieldByName(structField.Name)
 		srcField := srcStruct.FieldByName(structField.Name)
 		if !dstField.IsValid() || !srcField.IsValid() {
-			panic(fmt.Sprintf("invalid struct %q field %q",
-				dstType.Name(), structField.Name))
+			return fmt.Errorf("invalid struct %q field %q", dstType.Name(), structField.Name)
 		}
 		if dstField.Kind() != srcField.Kind() {
-			panic(fmt.Sprintf("mismatched struct %q fields %q",
-				dstType.Name(), structField.Name))
+			return fmt.Errorf("mismatched struct %q fields %q", dstType.Name(), structField.Name)
 		}
 		switch srcField.Kind() {
 		case reflect.String:
@@ -326,14 +324,15 @@ func overrideFields(dst, src interface{}) {
 				dstField.Set(srcField)
 			}
 		default:
-			panic(fmt.Sprintf("unhandled struct %q field %q type %v",
-				dstType.Name(), structField.Name, srcField.Kind()))
+			return fmt.Errorf("unhandled struct %q field %q type %v", dstType.Name(), structField.Name, srcField.Kind())
 		}
 	}
 	if !handled {
 		// No tags found in the struct so we don't know how to override
-		panic(fmt.Sprintf("failed to find 'gcfg' tags in struct %q", dstType.Name()))
+		return fmt.Errorf("failed to find 'gcfg' tags in struct %q", dstType.Name())
 	}
+
+	return nil
 }
 
 var cliConfig config
@@ -700,7 +699,10 @@ func buildKubernetesConfig(exec kexec.Interface, cli, file *config, saPath strin
 	if _, err2 := os.Stat(filepath.Join(saPath, kubeServiceAccountFileCACert)); err2 == nil {
 		saConfig.CACert = filepath.Join(saPath, kubeServiceAccountFileCACert)
 	}
-	overrideFields(&Kubernetes, &saConfig)
+
+	if err := overrideFields(&Kubernetes, &saConfig); err != nil {
+		return err
+	}
 
 	// Grab default values from OVS external IDs
 	if defaults.K8sAPIServer {
@@ -723,12 +725,20 @@ func buildKubernetesConfig(exec kexec.Interface, cli, file *config, saPath strin
 		APIServer:  os.Getenv("K8S_APISERVER"),
 		Token:      os.Getenv("K8S_TOKEN"),
 	}
-	overrideFields(&Kubernetes, &envConfig)
+
+	if err := overrideFields(&Kubernetes, &envConfig); err != nil {
+		return err
+	}
 
 	// Copy config file values over default values
-	overrideFields(&Kubernetes, &file.Kubernetes)
+	if err := overrideFields(&Kubernetes, &file.Kubernetes); err != nil {
+		return err
+	}
+
 	// And CLI overrides over config file and default values
-	overrideFields(&Kubernetes, &cli.Kubernetes)
+	if err := overrideFields(&Kubernetes, &cli.Kubernetes); err != nil {
+		return err
+	}
 
 	if Kubernetes.Kubeconfig != "" && !pathExists(Kubernetes.Kubeconfig) {
 		return fmt.Errorf("kubernetes kubeconfig file %q not found", Kubernetes.Kubeconfig)
@@ -759,7 +769,9 @@ func buildKubernetesConfig(exec kexec.Interface, cli, file *config, saPath strin
 
 func buildGatewayConfig(ctx *cli.Context, cli, file *config) error {
 	// Copy config file values over default values
-	overrideFields(&Gateway, &file.Gateway)
+	if err := overrideFields(&Gateway, &file.Gateway); err != nil {
+		return err
+	}
 
 	cli.Gateway.Mode = GatewayMode(ctx.String("gateway-mode"))
 	if cli.Gateway.Mode == GatewayModeDisabled {
@@ -772,7 +784,9 @@ func buildGatewayConfig(ctx *cli.Context, cli, file *config) error {
 		}
 	}
 	// And CLI overrides over config file and default values
-	overrideFields(&Gateway, &cli.Gateway)
+	if err := overrideFields(&Gateway, &cli.Gateway); err != nil {
+		return err
+	}
 
 	if Gateway.Mode != GatewayModeDisabled {
 		validModes := []string{string(GatewayModeShared), string(GatewayModeLocal)}
@@ -805,10 +819,14 @@ func buildGatewayConfig(ctx *cli.Context, cli, file *config) error {
 
 func buildMasterHAConfig(ctx *cli.Context, cli, file *config) error {
 	// Copy config file values over default values
-	overrideFields(&MasterHA, &file.MasterHA)
+	if err := overrideFields(&MasterHA, &file.MasterHA); err != nil {
+		return err
+	}
 
 	// And CLI overrides over config file and default values
-	overrideFields(&MasterHA, &cli.MasterHA)
+	if err := overrideFields(&MasterHA, &cli.MasterHA); err != nil {
+		return err
+	}
 
 	if MasterHA.HAElectionLeaseDuration <= MasterHA.HAElectionRenewDeadline {
 		return fmt.Errorf("Invalid HA election lease duration '%d'. "+
@@ -825,8 +843,13 @@ func buildMasterHAConfig(ctx *cli.Context, cli, file *config) error {
 }
 
 func buildDefaultConfig(cli, file *config) error {
-	overrideFields(&Default, &file.Default)
-	overrideFields(&Default, &cli.Default)
+	if err := overrideFields(&Default, &file.Default); err != nil {
+		return err
+	}
+
+	if err := overrideFields(&Default, &cli.Default); err != nil {
+		return err
+	}
 
 	// Legacy cluster-subnet CLI option overrides config file or --cluster-subnets
 	if clusterSubnet != "" {
@@ -885,6 +908,7 @@ func initConfigWithPath(ctx *cli.Context, exec kexec.Interface, saPath string, d
 	var retConfigFile string
 	var configFile string
 	var configFileIsDefault bool
+	var err error
 
 	configFile, configFileIsDefault = getConfigFilePath(ctx)
 
@@ -916,12 +940,21 @@ func initConfigWithPath(ctx *cli.Context, exec kexec.Interface, saPath string, d
 	}
 
 	// Build config that needs no special processing
-	overrideFields(&CNI, &cfg.CNI)
-	overrideFields(&CNI, &cliConfig.CNI)
+	if err = overrideFields(&CNI, &cfg.CNI); err != nil {
+		return "", err
+	}
+	if err = overrideFields(&CNI, &cliConfig.CNI); err != nil {
+		return "", err
+	}
 
 	// Logging setup
-	overrideFields(&Logging, &cfg.Logging)
-	overrideFields(&Logging, &cliConfig.Logging)
+	if err = overrideFields(&Logging, &cfg.Logging); err != nil {
+		return "", err
+	}
+	if err = overrideFields(&Logging, &cliConfig.Logging); err != nil {
+		return "", err
+	}
+
 	logrus.SetLevel(logrus.Level(Logging.Level))
 	if Logging.File != "" {
 		var file *os.File
@@ -1067,9 +1100,14 @@ func buildOvnAuth(exec kexec.Interface, northbound bool, cliAuth, confAuth *OvnA
 		auth.PrivKey = "/etc/openvswitch/ovn" + direction + "-privkey.pem"
 		auth.Cert = "/etc/openvswitch/ovn" + direction + "-cert.pem"
 	}
+
 	// Build the final auth config with overrides from CLI and config file
-	overrideFields(auth, confAuth)
-	overrideFields(auth, cliAuth)
+	if err := overrideFields(auth, confAuth); err != nil {
+		return nil, err
+	}
+	if err := overrideFields(auth, cliAuth); err != nil {
+		return nil, err
+	}
 
 	if address == "" {
 		if auth.PrivKey != "" || auth.Cert != "" || auth.CACert != "" {

go-controller/pkg/config/config_test.go
@@ -51,7 +51,7 @@ func writeConfigFile(cfgFile *os.File, randomOptData bool, args ...string) error
 			section = "ovnsouth"
 			arg = arg[4:]
 		default:
-			panic("shouldn't get here")
+			return fmt.Errorf("unexpected argument passed")
 		}
 
 		if randomOptData {

commit b93ffbe1ca0e22b95d3988d92257c67d0087f373

fix node restart issue
it is ok to fail while retrieving node's subnet information. we
eventually timeout and error out.

fixes: 59c177d3c326 (Cleaning up error/logging/panic handling in
       different places)
Signed-off-by: Girish Moodalbail <gmoodalbail@nvidia.com>

go-controller/pkg/cluster/node.go
@@ -111,10 +111,12 @@ func (cluster *OvnClusterController) StartClusterNode(name string) error {
 	// First wait for the node logical switch to be created by the Master, timeout is 300s.
 	err = wait.PollImmediate(500*time.Millisecond, 300*time.Second, func() (bool, error) {
 		if node, err = cluster.Kube.GetNode(name); err != nil {
-			return false, fmt.Errorf("error retrieving node %s: %v", name, err)
+			logrus.Errorf("error retrieving node %s: %v", name, err)
+			return false, nil
 		}
 		if cidr, _, err = util.RunOVNNbctl("get", "logical_switch", node.Name, "other-config:subnet"); err != nil {
-			return false, fmt.Errorf("error retrieving logical switch: %v", err)
+			logrus.Errorf("error retrieving logical switch: %v", err)
+			return false, nil
 		}
 		return true, nil
 	})