ovn-org/ovn-kubernetes

commit 75f836d91e1eeadd984089eb3f650734918972cf

create br-nexthop OVS internal port with fixed MAC address
the MAC address of br-nexthop port is re-generated upon every reboot.
OVN SB remembers the old MAC address in it's MAC_Binding table and this
causes communication issue. just like how physical NICs have fixed MAC
addresses, create these interfaces with the fixed MAC address of
00:00:a9:fe:21:01 where in the last 4 hex octets correspond to
169.255.33.1

Fixes #946

Signed-off-by: Girish Moodalbail <gmoodalbail@nvidia.com>

go-controller/pkg/cluster/gateway_localnet.go
@@ -24,7 +24,10 @@ const (
 	localnetGatewayIP            = "169.254.33.2/24"
 	localnetGatewayNextHop       = "169.254.33.1"
 	localnetGatewayNextHopSubnet = "169.254.33.1/24"
-	iptableNodePortChain         = "OVN-KUBE-NODEPORT"
+	// fixed MAC address for the br-nexthop interface. the last 4 hex bytes
+	// translates to the br-nexthop's IP address
+	localnetGatewayNextHopMac = "00:00:a9:fe:21:01"
+	iptableNodePortChain      = "OVN-KUBE-NODEPORT"
 )
 
 type iptRule struct {
@@ -132,9 +135,11 @@ func initLocalnetGateway(nodeName string,
 
 	// Create a localnet bridge nexthop
 	localnetBridgeNextHop := "br-nexthop"
-	_, stderr, err = util.RunOVSVsctl("--may-exist", "add-port",
-		localnetBridgeName, localnetBridgeNextHop, "--", "set",
-		"interface", localnetBridgeNextHop, "type=internal")
+	_, stderr, err = util.RunOVSVsctl(
+		"--may-exist", "add-port", localnetBridgeName, localnetBridgeNextHop,
+		"--", "set", "interface", localnetBridgeNextHop, "type=internal",
+		"--", "set", "interface", localnetBridgeNextHop,
+		fmt.Sprintf("mac=%s", strings.ReplaceAll(localnetGatewayNextHopMac, ":", "\\:")))
 	if err != nil {
 		return nil, fmt.Errorf("Failed to create localnet bridge next hop %s"+
 			", stderr:%s (%v)", localnetBridgeNextHop, stderr, err)

go-controller/pkg/cluster/gateway_init_linux_test.go
@@ -276,7 +276,8 @@ var _ = Describe("Gateway Init Operations", func() {
 			fexec.AddFakeCmdsNoOutputNoError([]string{
 				"ovs-vsctl --timeout=15 --may-exist add-br br-local",
 				"ip link set br-local up",
-				"ovs-vsctl --timeout=15 --may-exist add-port br-local br-nexthop -- set interface br-nexthop type=internal",
+				"ovs-vsctl --timeout=15 --may-exist add-port br-local br-nexthop -- set interface br-nexthop type=internal " +
+					"-- set interface br-nexthop mac=00\\:00\\:a9\\:fe\\:21\\:01",
 				"ip link set br-nexthop up",
 				"ip addr flush dev br-nexthop",
 				"ip addr add 169.254.33.1/24 dev br-nexthop",

commit beee634049431b050e5ef2ab8babdc95712e00fd

set other_config:hwaaddr on br-local before you add br-nexthop
The issue is that br-local was inheriting the lowest MAC from its
port and in this case it was br-nexthop. So, change the order in
which the mac address is fixed for br-local bridge. First, create the
br-local bridge and set other_config:hwaddr with mac_in_use value. Next,
create the br-nexthop interface with fixed mac address.

Fixes: 75f836d91e1e (create br-nexthop OVS internal port with fixed MAC
address)

Signed-off-by: Girish Moodalbail <gmoodalbail@nvidia.com>

go-controller/pkg/cluster/gateway_localnet.go
@@ -128,6 +128,11 @@ func initLocalnetGateway(nodeName string,
 			", stderr:%s (%v)", localnetBridgeName, stderr, err)
 	}
 
+	ifaceID, macAddress, err := bridgedGatewayNodeSetup(nodeName, localnetBridgeName, localnetBridgeName, true)
+	if err != nil {
+		return nil, fmt.Errorf("failed to set up shared interface gateway: %v", err)
+	}
+
 	_, _, err = util.RunIP("link", "set", localnetBridgeName, "up")
 	if err != nil {
 		return nil, fmt.Errorf("failed to up %s (%v)", localnetBridgeName, err)
@@ -163,11 +168,6 @@ func initLocalnetGateway(nodeName string,
 	if err != nil {
 		return nil, fmt.Errorf("failed to assign ip address to %s (%v)",
 			localnetBridgeNextHop, err)
-	}
-
-	ifaceID, macAddress, err := bridgedGatewayNodeSetup(nodeName, localnetBridgeName, localnetBridgeName, true)
-	if err != nil {
-		return nil, fmt.Errorf("failed to set up shared interface gateway: %v", err)
 	}
 
 	l3GatewayConfig := map[string]string{

go-controller/pkg/cluster/gateway_init_linux_test.go
@@ -282,11 +282,6 @@ var _ = Describe("Gateway Init Operations", func() {
 			fexec := ovntest.NewFakeExec()
 			fexec.AddFakeCmdsNoOutputNoError([]string{
 				"ovs-vsctl --timeout=15 --may-exist add-br br-local",
-				"ip link set br-local up",
-				"ovs-vsctl --timeout=15 --may-exist add-port br-local br-nexthop -- set interface br-nexthop type=internal mtu_request=" + mtu + " mac=00\\:00\\:a9\\:fe\\:21\\:01",
-				"ip link set br-nexthop up",
-				"ip addr flush dev br-nexthop",
-				"ip addr add 169.254.33.1/24 dev br-nexthop",
 			})
 			fexec.AddFakeCmd(&ovntest.ExpectedCmd{
 				Cmd:    "ovs-vsctl --timeout=15 --if-exists get interface br-local mac_in_use",
@@ -295,6 +290,11 @@ var _ = Describe("Gateway Init Operations", func() {
 			fexec.AddFakeCmdsNoOutputNoError([]string{
 				"ovs-vsctl --timeout=15 set bridge br-local other-config:hwaddr=" + brLocalnetMAC,
 				"ovs-vsctl --timeout=15 set Open_vSwitch . external_ids:ovn-bridge-mappings=" + util.PhysicalNetworkName + ":br-local",
+				"ip link set br-local up",
+				"ovs-vsctl --timeout=15 --may-exist add-port br-local br-nexthop -- set interface br-nexthop type=internal mtu_request=" + mtu + " mac=00\\:00\\:a9\\:fe\\:21\\:01",
+				"ip link set br-nexthop up",
+				"ip addr flush dev br-nexthop",
+				"ip addr add 169.254.33.1/24 dev br-nexthop",
 			})
 
 			err := util.SetExec(fexec)