d3f694fd410153df2a53444d92cd4d678f55266b
Dan Williams <dcbw@redhat.com>
2019-12-20 23:09:05 -0600

    Revert "create br-nexthop OVS internal port with fixed MAC address"
    This reverts commit 75f836d91e1eeadd984089eb3f650734918972cf.
    
    Turns out this breaks stuff. Not sure why yet, but it somehow
    prevents ARP from working correctly on br-local & br-nexthop.

--- go-controller/pkg/cluster/gateway_localnet.go
+++ go-controller/pkg/cluster/gateway_localnet.go
@@ -24,10 +24,7 @@ const (
 	localnetGatewayIP            = "169.254.33.2/24"
 	localnetGatewayNextHop       = "169.254.33.1"
 	localnetGatewayNextHopSubnet = "169.254.33.1/24"
-	// fixed MAC address for the br-nexthop interface. the last 4 hex bytes
-	// translates to the br-nexthop's IP address
-	localnetGatewayNextHopMac = "00:00:a9:fe:21:01"
-	iptableNodePortChain      = "OVN-KUBE-NODEPORT"
+	iptableNodePortChain         = "OVN-KUBE-NODEPORT"
 )
 
 type iptRule struct {
@@ -138,8 +135,7 @@ func initLocalnetGateway(nodeName string,
 	_, stderr, err = util.RunOVSVsctl(
 		"--may-exist", "add-port", localnetBridgeName, localnetBridgeNextHop,
 		"--", "set", "interface", localnetBridgeNextHop, "type=internal",
-		"mtu_request="+fmt.Sprintf("%d", config.Default.MTU),
-		fmt.Sprintf("mac=%s", strings.ReplaceAll(localnetGatewayNextHopMac, ":", "\\:")))
+		"mtu_request="+fmt.Sprintf("%d", config.Default.MTU))
 	if err != nil {
 		return nil, fmt.Errorf("Failed to create localnet bridge next hop %s"+
 			", stderr:%s (%v)", localnetBridgeNextHop, stderr, err)
