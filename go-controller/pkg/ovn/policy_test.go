ovn-org/ovn-kubernetes

commit c3def15ad986284f200eb07cc17b2fdf17e6f66b

Add multicast support.
Enable IGMP Snoop (when supported) and IGMP relay to allow multicast
connectivity across nodes.

Enforce the following network policies for IP multicast traffic:
- a default deny-all network policy is applied to all IP multicast
  traffic. This is implemented with two ACLs:
  a) one ACL dropping egress multicast traffic from all pods:
     this is to protect OVN controller from processing IP multicast
     reports from nodes that are not allowed to receive multicast
     traffic.
  b) one ACL dropping ingress multicast traffic to all pods.
- when multicast is explicitly enabled in the namespace, IP multicast
  traffic is forwarded only to pods in the same namespace. This is done
  by adding:
  a) a port group containing all logical ports associated with the
     namespace.
  b) one "from-lport" ACL allowing egress multicast traffic from the
     in the namespace.
  c) one "to-lport" ACL allowing ingress multicast traffic to pods in
     the namespace. This matches only traffic originated by pods in
     the same namespace (based on the namespace address set).

Add a new namespace annotation to allow enabling of multicast:
"k8s.ovn.org/multicast-enabled".

Signed-off-by: Dumitru Ceara <dceara@redhat.com>

go-controller/pkg/ovn/namespace.go
@@ -10,6 +10,11 @@ import (
 	kapi "k8s.io/api/core/v1"
 )
 
+const (
+	// Annotation used to enable/disable multicast in the namespace
+	nsMulticastAnnotation = "k8s.ovn.org/multicast-enabled"
+)
+
 func (oc *Controller) syncNamespaces(namespaces []interface{}) {
 	expectedNs := make(map[string]bool)
 	for _, nsInterface := range namespaces {
@@ -49,29 +54,40 @@ func (oc *Controller) waitForNamespaceEvent(namespace string) error {
 	return nil
 }
 
-func (oc *Controller) addPodToNamespaceAddressSet(ns string, ip net.IP) {
+func (oc *Controller) addPodToNamespace(ns string, ip net.IP,
+	logicalPort string) {
 	mutex := oc.getNamespaceLock(ns)
 	if mutex == nil {
 		return
 	}
 	defer mutex.Unlock()
 
-	// If pod has already been added, nothing to do.
-	address := ip.String()
-	if oc.namespaceAddressSet[ns][address] {
+	if oc.namespacePolicies[ns] == nil {
 		return
 	}
 
-	oc.namespaceAddressSet[ns][address] = true
+	// If pod has already been added, nothing to do.
+	address := ip.String()
+	if oc.namespaceAddressSet[ns][address] != "" {
+		return
+	}
+
+	oc.namespaceAddressSet[ns][address] = logicalPort
 	addresses := make([]string, 0)
 	for address := range oc.namespaceAddressSet[ns] {
 		addresses = append(addresses, address)
 	}
 
 	oc.setAddressSet(hashedAddressSet(ns), addresses)
+
+	// Enforce the default deny multicast policy
+	if oc.multicastSupport {
+		oc.podAddDefaultDenyMulticastPolicy(logicalPort)
+	}
 }
 
-func (oc *Controller) deletePodFromNamespaceAddressSet(ns string, ip net.IP) {
+func (oc *Controller) deletePodFromNamespace(ns string, ip net.IP,
+	logicalPort string) {
 	if ip == nil {
 		return
 	}
@@ -83,7 +99,7 @@ func (oc *Controller) deletePodFromNamespaceAddressSet(ns string, ip net.IP) {
 	defer mutex.Unlock()
 
 	address := ip.String()
-	if !oc.namespaceAddressSet[ns][address] {
+	if oc.namespaceAddressSet[ns][address] == "" {
 		return
 	}
 
@@ -94,6 +110,51 @@ func (oc *Controller) deletePodFromNamespaceAddressSet(ns string, ip net.IP) {
 	}
 
 	oc.setAddressSet(hashedAddressSet(ns), addresses)
+
+	//Remove the port from the default deny multicast policy
+	if oc.multicastSupport {
+		oc.podDeleteDefaultDenyMulticastPolicy(logicalPort)
+	}
+}
+
+// Creates an explicit "allow" policy for multicast traffic within the
+// namespace if multicast is enabled. Otherwise, removes the "allow" policy.
+// Traffic will be dropped by the default multicast deny ACL.
+func (oc *Controller) multicastUpdateNamespace(ns *kapi.Namespace) {
+	if !oc.multicastSupport {
+		return
+	}
+
+	enabled := (ns.Annotations[nsMulticastAnnotation] == "true")
+	enabledOld := oc.multicastEnabled[ns.Name]
+
+	if enabledOld == enabled {
+		return
+	}
+
+	var err error
+	if enabled {
+		err = oc.createMulticastAllowPolicy(ns.Name)
+	} else {
+		err = oc.deleteMulticastAllowPolicy(ns.Name)
+	}
+	if err != nil {
+		logrus.Errorf(err.Error())
+		return
+	}
+
+	oc.multicastEnabled[ns.Name] = enabled
+}
+
+// Cleans up the multicast policy for this namespace if multicast was
+// previously allowed.
+func (oc *Controller) multicastDeleteNamespace(ns *kapi.Namespace) {
+	if oc.multicastEnabled[ns.Name] {
+		if err := oc.deleteMulticastAllowPolicy(ns.Name); err != nil {
+			logrus.Errorf(err.Error())
+		}
+	}
+	delete(oc.multicastEnabled, ns.Name)
 }
 
 // AddNamespace creates corresponding addressset in ovn db
@@ -110,7 +171,7 @@ func (oc *Controller) AddNamespace(ns *kapi.Namespace) {
 	defer oc.namespaceMutex[ns.Name].Unlock()
 	oc.namespaceMutexMutex.Unlock()
 
-	oc.namespaceAddressSet[ns.Name] = make(map[string]bool)
+	oc.namespaceAddressSet[ns.Name] = make(map[string]string)
 
 	// Get all the pods in the namespace and append their IP to the
 	// address_set
@@ -120,7 +181,8 @@ func (oc *Controller) AddNamespace(ns *kapi.Namespace) {
 	} else {
 		for _, pod := range existingPods.Items {
 			if pod.Status.PodIP != "" {
-				oc.namespaceAddressSet[ns.Name][pod.Status.PodIP] = true
+				portName := podLogicalPortName(&pod)
+				oc.namespaceAddressSet[ns.Name][pod.Status.PodIP] = portName
 			}
 		}
 	}
@@ -136,6 +198,18 @@ func (oc *Controller) AddNamespace(ns *kapi.Namespace) {
 		addresses)
 
 	oc.namespacePolicies[ns.Name] = make(map[string]*namespacePolicy)
+	oc.multicastUpdateNamespace(ns)
+}
+
+func (oc *Controller) updateNamespace(old, newer *kapi.Namespace) {
+	logrus.Debugf("Updating namespace: old %s new %s", old.Name, newer.Name)
+
+	// A big fat lock per namespace to prevent race conditions
+	// with namespace resources like address sets and deny acls.
+	oc.namespaceMutex[newer.Name].Lock()
+	defer oc.namespaceMutex[newer.Name].Unlock()
+
+	oc.multicastUpdateNamespace(newer)
 }
 
 func (oc *Controller) deleteNamespace(ns *kapi.Namespace) {
@@ -151,6 +225,7 @@ func (oc *Controller) deleteNamespace(ns *kapi.Namespace) {
 	defer mutex.Unlock()
 
 	oc.deleteAddressSet(hashedAddressSet(ns.Name))
+	oc.multicastDeleteNamespace(ns)
 	delete(oc.namespacePolicies, ns.Name)
 	delete(oc.namespaceAddressSet, ns.Name)
 	delete(oc.namespaceMutex, ns.Name)

commit 5955e4fcdbb1af243955b10e51b7e4a733b434e5

Fix multicast Port_Group update at pod creation/deletion.
Whenever a pod is added/removed from a namespace 'ns', if multicast is
enabled in the namespace, also update the associated Port_Group
mcastPortGroup-<ns> otherwise multicast traffic will not be allowed from
the pod.

Whenever a namespace is created with existing pods, add the pods to the
default multicast deny policy.

Also add unit tests for multicast use cases.

Reported-at: https://bugzilla.redhat.com/1772579
Fixes: c3def15ad986 ("Add multicast support.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

go-controller/pkg/ovn/namespace.go
@@ -80,9 +80,13 @@ func (oc *Controller) addPodToNamespace(ns string, ip net.IP,
 
 	setAddressSet(hashedAddressSet(ns), addresses)
 
-	// Enforce the default deny multicast policy
+	// Enforce the default deny multicast policy and, if multicast allowed,
+	// add the pod to the allow policy.
 	if oc.multicastSupport {
 		oc.podAddDefaultDenyMulticastPolicy(logicalPort)
+		if oc.multicastEnabled[ns] {
+			oc.podAddAllowMulticastPolicy(ns, logicalPort)
+		}
 	}
 }
 
@@ -111,9 +115,13 @@ func (oc *Controller) deletePodFromNamespace(ns string, ip net.IP,
 
 	setAddressSet(hashedAddressSet(ns), addresses)
 
-	//Remove the port from the default deny multicast policy
+	// Remove the port from the default deny multicast policy and, if
+	// multicast allowed, remove the pod from the allow policy.
 	if oc.multicastSupport {
 		oc.podDeleteDefaultDenyMulticastPolicy(logicalPort)
+		if oc.multicastEnabled[ns] {
+			oc.podDeleteAllowMulticastPolicy(ns, logicalPort)
+		}
 	}
 }
 
@@ -183,6 +191,11 @@ func (oc *Controller) AddNamespace(ns *kapi.Namespace) {
 			if pod.Status.PodIP != "" {
 				portName := podLogicalPortName(&pod)
 				oc.namespaceAddressSet[ns.Name][pod.Status.PodIP] = portName
+
+				// Enforce the default deny multicast policy.
+				if oc.multicastSupport {
+					oc.podAddDefaultDenyMulticastPolicy(portName)
+				}
 			}
 		}
 	}

go-controller/pkg/ovn/policy.go
@@ -342,6 +342,12 @@ func getMulticastACLMatch(ns string) string {
 	return "ip4.src == $" + nsAddressSet + " && ip4.mcast"
 }
 
+// Returns the multicast port group name and hash for namespace 'ns'.
+func getMulticastPortGroup(ns string) (string, string) {
+	portGroupName := "mcastPortGroup-" + ns
+	return portGroupName, hashedPortGroup(portGroupName)
+}
+
 // Creates a policy to allow multicast traffic within 'ns':
 // - a port group containing all logical ports associated with 'ns'
 // - one "from-lport" ACL allowing egress multicast traffic from the pods
@@ -350,8 +356,7 @@ func getMulticastACLMatch(ns string) string {
 //   This matches only traffic originated by pods in 'ns' (based on the
 //   namespace address set).
 func (oc *Controller) createMulticastAllowPolicy(ns string) error {
-	portGroupName := "mcastPortGroup-" + ns
-	portGroupHash := hashedPortGroup(portGroupName)
+	portGroupName, portGroupHash := getMulticastPortGroup(ns)
 	portGroupUUID, err := createPortGroup(portGroupName, portGroupHash)
 	if err != nil {
 		return fmt.Errorf("Failed to create port_group for %s (%v)",
@@ -376,7 +381,7 @@ func (oc *Controller) createMulticastAllowPolicy(ns string) error {
 
 	// Add all ports from this namespace to the multicast allow group.
 	for _, portName := range oc.namespaceAddressSet[ns] {
-		oc.addToACL(portGroupHash, portName)
+		oc.podAddAllowMulticastPolicy(ns, portName)
 	}
 
 	return nil
@@ -384,8 +389,7 @@ func (oc *Controller) createMulticastAllowPolicy(ns string) error {
 
 // Delete the policy to allow multicast traffic within 'ns'.
 func deleteMulticastAllowPolicy(ns string) error {
-	portGroupName := "mcastPortGroup-" + ns
-	portGroupHash := hashedPortGroup(portGroupName)
+	_, portGroupHash := getMulticastPortGroup(ns)
 
 	err := deleteACLPortGroup(portGroupHash, fromLport,
 		defaultMcastAllowPriority, "ip4.mcast", "allow",
@@ -447,6 +451,16 @@ func (oc *Controller) podAddDefaultDenyMulticastPolicy(logicalPort string) {
 
 func (oc *Controller) podDeleteDefaultDenyMulticastPolicy(logicalPort string) {
 	oc.deleteFromACL("mcastPortGroupDeny", logicalPort)
+}
+
+func (oc *Controller) podAddAllowMulticastPolicy(ns, logicalPort string) {
+	_, portGroupHash := getMulticastPortGroup(ns)
+	oc.addToACL(portGroupHash, logicalPort)
+}
+
+func (oc *Controller) podDeleteAllowMulticastPolicy(ns, logicalPort string) {
+	_, portGroupHash := getMulticastPortGroup(ns)
+	oc.deleteFromACL(portGroupHash, logicalPort)
 }
 
 func (oc *Controller) localPodAddDefaultDeny(

go-controller/pkg/ovn/namespace_test.go
@@ -25,6 +25,7 @@ func newNamespaceMeta(namespace string) metav1.ObjectMeta {
 		Labels: map[string]string{
 			"name": namespace,
 		},
+		Annotations: map[string]string{},
 	}
 }
 
@@ -65,14 +66,54 @@ func (n namespace) delCmds(fexec *ovntest.FakeExec, namespace v1.Namespace) {
 	})
 }
 
+func (n namespace) getLogicalPortUUIDCmds(fexec *ovntest.FakeExec, port, resultUUID string) {
+	fexec.AddFakeCmd(&ovntest.ExpectedCmd{
+		Cmd:    "ovn-nbctl --timeout=15 --if-exists get logical_switch_port " + port + " _uuid",
+		Output: resultUUID,
+	})
+}
+
+func (n namespace) addPodDenyMcast(fexec *ovntest.FakeExec, tP pod, inCache bool, portUUID string) {
+	if !inCache {
+		n.getLogicalPortUUIDCmds(fexec, tP.portName, portUUID)
+	}
+
+	fexec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 " +
+			"--if-exists remove port_group mcastPortGroupDeny ports " + portUUID + " " +
+			"-- add port_group mcastPortGroupDeny ports " + portUUID,
+	})
+}
+
+func (n namespace) delPodDenyMcast(fexec *ovntest.FakeExec, tP pod, inCache bool, portUUID string) {
+	if !inCache {
+		n.getLogicalPortUUIDCmds(fexec, tP.portName, portUUID)
+	}
+
+	fexec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 " +
+			"--if-exists remove port_group mcastPortGroupDeny ports " + portUUID,
+	})
+}
+
+func (n namespace) addPodCmds(fexec *ovntest.FakeExec, tP pod, namespace v1.Namespace) {
+	fexec.AddFakeCmdsNoOutputNoError([]string{
+		fmt.Sprintf(`ovn-nbctl --timeout=15 set address_set %s addresses="%s"`, hashedAddressSet(namespace.Name), tP.podIP),
+	})
+}
+
+func (n namespace) delPodCmds(fexec *ovntest.FakeExec, tP pod, namespace v1.Namespace) {
+	fexec.AddFakeCmdsNoOutputNoError([]string{
+		fmt.Sprintf("ovn-nbctl --timeout=15 clear address_set %s addresses", hashedAddressSet(namespace.Name)),
+	})
+}
+
 func (n namespace) addCmdsWithPods(fexec *ovntest.FakeExec, tP pod, namespace v1.Namespace) {
 	fexec.AddFakeCmd(&ovntest.ExpectedCmd{
 		Cmd:    "ovn-nbctl --timeout=15 --data=bare --no-heading --columns=_uuid find address_set name=" + hashedAddressSet(namespace.Name),
 		Output: fmt.Sprintf("name=%s\n", namespace.Name),
 	})
-	fexec.AddFakeCmdsNoOutputNoError([]string{
-		fmt.Sprintf(`ovn-nbctl --timeout=15 set address_set %s addresses="%s"`, hashedAddressSet(namespace.Name), tP.podIP),
-	})
+	n.addPodCmds(fexec, tP, namespace)
 }
 
 var _ = Describe("OVN Namespace Operations", func() {
@@ -117,6 +158,7 @@ var _ = Describe("OVN Namespace Operations", func() {
 				)
 
 				test.baseCmds(fExec, namespaceT)
+				test.addPodDenyMcast(fExec, tP, false, "fake-uuid")
 				test.addCmdsWithPods(fExec, tP, namespaceT)
 
 				fakeOvn.start(ctx,

go-controller/pkg/ovn/policy_test.go
@@ -196,6 +196,93 @@ func (n networkPolicy) delPodCmds(fexec *ovntest.FakeExec, networkPolicy knet.Ne
 	}
 }
 
+type multicastPolicy struct{}
+
+func (p multicastPolicy) enableCmds(fExec *ovntest.FakeExec, ns string) {
+	pg_name, pg_hash := getMulticastPortGroup(ns)
+
+	fExec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 --data=bare --no-heading --columns=_uuid find port_group name=" + pg_hash,
+	})
+	fExec.AddFakeCmd(&ovntest.ExpectedCmd{
+		Cmd:    "ovn-nbctl --timeout=15 create port_group name=" + pg_hash + " external-ids:name=" + pg_name,
+		Output: "fake_uuid",
+	})
+
+	match := getACLMatch(pg_hash, "ip4.mcast", knet.PolicyTypeEgress)
+	fExec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 --data=bare --no-heading --columns=_uuid find ACL " +
+			match + " action=allow external-ids:default-deny-policy-type=Egress",
+	})
+	fExec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 --id=@acl create acl priority=1012 direction=from-lport " +
+			match + " action=allow external-ids:default-deny-policy-type=Egress " +
+			"-- add port_group fake_uuid acls @acl",
+	})
+
+	match = getMulticastACLMatch(ns)
+	match = getACLMatch(pg_hash, match, knet.PolicyTypeIngress)
+	fExec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 --data=bare --no-heading --columns=_uuid find ACL " +
+			match + " action=allow external-ids:default-deny-policy-type=Ingress",
+	})
+	fExec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 --id=@acl create acl priority=1012 direction=to-lport " +
+			match + " action=allow external-ids:default-deny-policy-type=Ingress " +
+			"-- add port_group fake_uuid acls @acl",
+	})
+}
+
+func (p multicastPolicy) disableCmds(fExec *ovntest.FakeExec, ns string) {
+	_, pg_hash := getMulticastPortGroup(ns)
+
+	match := getACLMatch(pg_hash, "ip4.mcast", knet.PolicyTypeEgress)
+	fExec.AddFakeCmd(&ovntest.ExpectedCmd{
+		Cmd: "ovn-nbctl --timeout=15 --data=bare --no-heading --columns=_uuid find ACL " +
+			match + " " + "action=allow external-ids:default-deny-policy-type=Egress",
+		Output: "fake_uuid",
+	})
+	fExec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 remove port_group " + pg_hash + " acls fake_uuid",
+	})
+
+	match = getMulticastACLMatch(ns)
+	match = getACLMatch(pg_hash, match, knet.PolicyTypeIngress)
+	fExec.AddFakeCmd(&ovntest.ExpectedCmd{
+		Cmd: "ovn-nbctl --timeout=15 --data=bare --no-heading --columns=_uuid find ACL " +
+			match + " " + "action=allow external-ids:default-deny-policy-type=Ingress",
+		Output: "fake_uuid",
+	})
+	fExec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 remove port_group " + pg_hash + " acls fake_uuid",
+	})
+
+	fExec.AddFakeCmd(&ovntest.ExpectedCmd{
+		Cmd:    "ovn-nbctl --timeout=15 --data=bare --no-heading --columns=_uuid find port_group name=" + pg_hash,
+		Output: "fake_uuid",
+	})
+	fExec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 --if-exists destroy port_group fake_uuid",
+	})
+}
+
+func (p multicastPolicy) addPodCmds(fExec *ovntest.FakeExec, ns, portUUID string) {
+	_, pg_hash := getMulticastPortGroup(ns)
+	fExec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 " +
+			"--if-exists remove port_group " + pg_hash + " ports " + portUUID + " " +
+			"-- add port_group " + pg_hash + " ports " + portUUID,
+	})
+}
+
+func (p multicastPolicy) delPodCmds(fExec *ovntest.FakeExec, ns, portUUID string) {
+	_, pg_hash := getMulticastPortGroup(ns)
+	fExec.AddFakeCmdsNoOutputNoError([]string{
+		"ovn-nbctl --timeout=15 " +
+			"--if-exists remove port_group " + pg_hash + " ports " + portUUID,
+	})
+}
+
 var _ = Describe("OVN NetworkPolicy Operations", func() {
 	var (
 		app     *cli.App
@@ -341,6 +428,7 @@ var _ = Describe("OVN NetworkPolicy Operations", func() {
 				nPodTest.addNodeSetupCmds(fExec)
 				nPodTest.addCmdsForNonExistingPod(fExec)
 				nTest.baseCmds(fExec, namespace1)
+				nTest.addPodDenyMcast(fExec, nPodTest, true, "fake_uuid")
 				nTest.addCmdsWithPods(fExec, nPodTest, namespace1)
 				npTest.addPodSelectorCmds(fExec, nPodTest, networkPolicy, true, false)
 
@@ -440,6 +528,7 @@ var _ = Describe("OVN NetworkPolicy Operations", func() {
 				nPodTest.addCmdsForNonExistingPod(fExec)
 				nTest.baseCmds(fExec, namespace1, namespace2)
 				nTest.addCmds(fExec, namespace1)
+				nTest.addPodDenyMcast(fExec, nPodTest, false, "fake_uuid")
 				nTest.addCmdsWithPods(fExec, nPodTest, namespace2)
 				npTest.addPodSelectorCmds(fExec, nPodTest, networkPolicy, false, false)
 
@@ -534,6 +623,7 @@ var _ = Describe("OVN NetworkPolicy Operations", func() {
 				nPodTest.addCmdsForNonExistingPod(fExec)
 				nTest.baseCmds(fExec, namespace1, namespace2)
 				nTest.addCmds(fExec, namespace2)
+				nTest.addPodDenyMcast(fExec, nPodTest, true, "fake_uuid")
 				nTest.addCmdsWithPods(fExec, nPodTest, namespace1)
 				npTest.addNamespaceSelectorCmds(fExec, networkPolicy, true)
 				npTest.addLocalPodCmds(fExec, nPodTest)
@@ -725,6 +815,7 @@ var _ = Describe("OVN NetworkPolicy Operations", func() {
 				nPodTest.addNodeSetupCmds(fExec)
 				nPodTest.addCmdsForNonExistingPod(fExec)
 				nTest.baseCmds(fExec, namespace1)
+				nTest.addPodDenyMcast(fExec, nPodTest, true, "fake_uuid")
 				nTest.addCmdsWithPods(fExec, nPodTest, namespace1)
 				npTest.addPodSelectorCmds(fExec, nPodTest, networkPolicy, true, false)
 
@@ -832,6 +923,7 @@ var _ = Describe("OVN NetworkPolicy Operations", func() {
 				nPodTest.addCmdsForNonExistingPod(fExec)
 				nTest.baseCmds(fExec, namespace1, namespace2)
 				nTest.addCmds(fExec, namespace1)
+				nTest.addPodDenyMcast(fExec, nPodTest, false, "fake_uuid")
 				nTest.addCmdsWithPods(fExec, nPodTest, namespace2)
 				npTest.addPodSelectorCmds(fExec, nPodTest, networkPolicy, false, false)
 
@@ -928,6 +1020,7 @@ var _ = Describe("OVN NetworkPolicy Operations", func() {
 				nPodTest.addNodeSetupCmds(fExec)
 				nPodTest.addCmdsForNonExistingPod(fExec)
 				nTest.baseCmds(fExec, namespace1)
+				nTest.addPodDenyMcast(fExec, nPodTest, true, "fake_uuid")
 				nTest.addCmdsWithPods(fExec, nPodTest, namespace1)
 				npTest.addPodSelectorCmds(fExec, nPodTest, networkPolicy, true, false)
 
@@ -963,6 +1056,184 @@ var _ = Describe("OVN NetworkPolicy Operations", func() {
 				Expect(err).NotTo(HaveOccurred())
 				Eventually(fExec.CalledMatchesExpected).Should(BeTrue(), fExec.ErrorDesc)
 
+				return nil
+			}
+
+			err := app.Run([]string{app.Name})
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("tests enabling/disabling multicast in a namespace", func() {
+			app.Action = func(ctx *cli.Context) error {
+				nTest := namespace{}
+				namespace1 := *newNamespace("namespace1")
+
+				fakeOvn.start(ctx,
+					&v1.NamespaceList{
+						Items: []v1.Namespace{
+							namespace1,
+						},
+					},
+				)
+
+				nTest.baseCmds(fExec, namespace1)
+				nTest.addCmds(fExec, namespace1)
+				fakeOvn.controller.WatchNamespaces()
+				ns, err := fakeOvn.fakeClient.CoreV1().Namespaces().Get(
+					namespace1.Name, metav1.GetOptions{})
+				Expect(err).NotTo(HaveOccurred())
+				Expect(ns).NotTo(BeNil())
+
+				// Multicast is denied by default.
+				_, ok := ns.Annotations[nsMulticastAnnotation]
+				Expect(ok).To(BeFalse())
+
+				// Enable multicast in the namespace.
+				mcastPolicy := multicastPolicy{}
+				mcastPolicy.enableCmds(fExec, namespace1.Name)
+				ns.Annotations[nsMulticastAnnotation] = "true"
+				_, err = fakeOvn.fakeClient.CoreV1().Namespaces().Update(ns)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(fExec.CalledMatchesExpected).Should(BeTrue(), fExec.ErrorDesc)
+
+				// Disable multicast in the namespace.
+				mcastPolicy.disableCmds(fExec, namespace1.Name)
+				ns.Annotations[nsMulticastAnnotation] = "false"
+				_, err = fakeOvn.fakeClient.CoreV1().Namespaces().Update(ns)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(fExec.CalledMatchesExpected).Should(BeTrue(), fExec.ErrorDesc)
+				return nil
+			}
+
+			err := app.Run([]string{app.Name})
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("tests enabling multicast in a namespace with a pod", func() {
+			app.Action = func(ctx *cli.Context) error {
+				nTest := namespace{}
+				namespace1 := *newNamespace("namespace1")
+
+				nPodTest := newTPod(
+					"node1",
+					"10.128.1.0/24",
+					"10.128.1.2",
+					"10.128.1.1",
+					"myPod",
+					"10.128.1.4",
+					"11:22:33:44:55:66",
+					namespace1.Name,
+				)
+
+				nPodTest.baseCmds(fExec)
+				nPodTest.addNodeSetupCmds(fExec)
+				nPodTest.addCmdsForNonExistingPod(fExec)
+				nTest.baseCmds(fExec, namespace1)
+				nTest.addPodDenyMcast(fExec, nPodTest, false, "fake_uuid")
+				nTest.addCmdsWithPods(fExec, nPodTest, namespace1)
+				fakeOvn.start(ctx,
+					&v1.NamespaceList{
+						Items: []v1.Namespace{
+							namespace1,
+						},
+					},
+					&v1.PodList{
+						Items: []v1.Pod{
+							*newPod(nPodTest.namespace, nPodTest.podName, nPodTest.nodeName, nPodTest.podIP),
+						},
+					},
+				)
+
+				fakeOvn.controller.WatchPods()
+				fakeOvn.controller.WatchNamespaces()
+				ns, err := fakeOvn.fakeClient.CoreV1().Namespaces().Get(
+					namespace1.Name, metav1.GetOptions{})
+				Expect(err).NotTo(HaveOccurred())
+				Expect(ns).NotTo(BeNil())
+
+				// Enable multicast in the namespace
+				mcastPolicy := multicastPolicy{}
+				mcastPolicy.enableCmds(fExec, namespace1.Name)
+				// The pod should be added to the multicast allow port group.
+				mcastPolicy.addPodCmds(fExec, namespace1.Name, "fake_uuid")
+				ns.Annotations[nsMulticastAnnotation] = "true"
+				_, err = fakeOvn.fakeClient.CoreV1().Namespaces().Update(ns)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(fExec.CalledMatchesExpected).Should(BeTrue(), fExec.ErrorDesc)
+				return nil
+			}
+
+			err := app.Run([]string{app.Name})
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("tests adding a pod to a multicast enabled namespace", func() {
+			app.Action = func(ctx *cli.Context) error {
+				nTest := namespace{}
+				namespace1 := *newNamespace("namespace1")
+
+				nPodTest := newTPod(
+					"node1",
+					"10.128.1.0/24",
+					"10.128.1.2",
+					"10.128.1.1",
+					"myPod",
+					"10.128.1.4",
+					"11:22:33:44:55:66",
+					namespace1.Name,
+				)
+
+				fakeOvn.start(ctx,
+					&v1.NamespaceList{
+						Items: []v1.Namespace{
+							namespace1,
+						},
+					},
+				)
+
+				nPodTest.baseCmds(fExec)
+				nTest.baseCmds(fExec, namespace1)
+				nTest.addCmds(fExec, namespace1)
+				fakeOvn.controller.WatchNamespaces()
+				fakeOvn.controller.WatchPods()
+				ns, err := fakeOvn.fakeClient.CoreV1().Namespaces().Get(
+					namespace1.Name, metav1.GetOptions{})
+				Expect(err).NotTo(HaveOccurred())
+				Expect(ns).NotTo(BeNil())
+
+				// Enable multicast in the namespace.
+				mcastPolicy := multicastPolicy{}
+				mcastPolicy.enableCmds(fExec, namespace1.Name)
+				ns.Annotations[nsMulticastAnnotation] = "true"
+				_, err = fakeOvn.fakeClient.CoreV1().Namespaces().Update(ns)
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(fExec.CalledMatchesExpected).Should(BeTrue(), fExec.ErrorDesc)
+
+				nPodTest.addNodeSetupCmds(fExec)
+				nPodTest.addCmdsForNonExistingPod(fExec)
+				nTest.addPodDenyMcast(fExec, nPodTest, false, "fake_uuid")
+				nTest.addPodCmds(fExec, nPodTest, namespace1)
+
+				// The pod should be added to the multicast allow group.
+				mcastPolicy.addPodCmds(fExec, namespace1.Name, "fake_uuid")
+
+				_, err = fakeOvn.fakeClient.CoreV1().Pods(nPodTest.namespace).Create(newPod(
+					nPodTest.namespace, nPodTest.podName, nPodTest.nodeName, nPodTest.podIP))
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(fExec.CalledMatchesExpected).Should(BeTrue(), fExec.ErrorDesc)
+
+				// Delete the pod from the namespace.
+				mcastPolicy.delPodCmds(fExec, namespace1.Name, "fake_uuid")
+				// The pod should be removed from the multicasts default deny
+				// group and from the multicast allow group.
+				nTest.delPodDenyMcast(fExec, nPodTest, false, "fake_uuid")
+				nTest.delPodCmds(fExec, nPodTest, namespace1)
+				nPodTest.delCmds(fExec)
+
+				err = fakeOvn.fakeClient.CoreV1().Pods(nPodTest.namespace).Delete(
+					nPodTest.podName, metav1.NewDeleteOptions(0))
+				Expect(err).NotTo(HaveOccurred())
+				Eventually(fExec.CalledMatchesExpected).Should(BeTrue(), fExec.ErrorDesc)
 				return nil
 			}
 