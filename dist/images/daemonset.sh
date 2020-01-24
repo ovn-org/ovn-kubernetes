ovn-org/ovn-kubernetes

commit 3b6d22cad910c0ed57c7d8cdca44bd6f56409c7e

Make KIND specific yaml rendered
Signed-off-by: Tim Rozet <trozet@redhat.com>

dist/images/daemonset.sh
@@ -4,8 +4,11 @@
 #Always exit on errors
 set -e
 
-# This is people that are nut using the ansible install.
-# The script expands the templates into yaml files in ../yaml
+# This is for people that are not using the ansible install.
+# The script renders j2 templates into yaml files in ../templates/
+
+# ensure j2 renderer installed
+pip freeze | grep j2cli || pip install j2cli[yaml] --user
 
 OVN_IMAGE=""
 OVN_IMAGE_PULL_POLICY=""
@@ -19,6 +22,7 @@ OVN_DB_VIP_IMAGE=""
 OVN_DB_VIP=""
 OVN_DB_REPLICAS=""
 OVN_MTU="1400"
+KIND=""
 
 # Parse parameters given as arguments to this script.
 while [ "$1" != "" ]; do
@@ -57,6 +61,9 @@ while [ "$1" != "" ]; do
             ;;
         --mtu)
             OVN_MTU=$VALUE
+            ;;
+        --kind)
+            KIND=true
             ;;
         *)
             echo "WARNING: unknown parameter \"$PARAM\""
@@ -119,10 +126,7 @@ ovn_db_vip_image_repl="{{ ovn_db_vip_image | default('docker.io/ovnkube/ovndb-vi
 ovn_db_replicas_repl="{{ ovn_db_replicas | default(3) }}"
 ovn_db_vip_repl="{{ ovn_db_vip }}"
 
-sed "s,${image_str},${image},
-s,${ovn_gateway_mode_repl},${ovn_gateway_mode},
-s,${ovn_gateway_opts_repl},${ovn_gateway_opts},
-s,${policy_str},${policy}," ../templates/ovnkube-node.yaml.j2 > ../yaml/ovnkube-node.yaml
+ovn_image=${image} ovn_image_pull_policy=${policy} kind=${KIND} ovn_gateway_mode=${ovn_gateway_mode} ovn_gateway_opts=${ovn_gateway_opts} j2 ../templates/ovnkube-node.yaml.j2 -o ../templates/ovnkube-node.yaml
 
 sed "s,${image_str},${image},
 s,${policy_str},${policy}," ../templates/ovnkube-master.yaml.j2 > ../yaml/ovnkube-master.yaml

dist/templates/ovnkube-node.yaml.j2
@@ -165,7 +165,9 @@ spec:
           runAsUser: 0
           capabilities:
             add: ["NET_ADMIN", "SYS_ADMIN", "SYS_PTRACE"]
+          {% if kind is defined and kind -%}
           privileged: true
+          {% endif %}
 
         volumeMounts:
         - mountPath: /var/run/dbus/
@@ -183,9 +185,11 @@ spec:
           name: host-opt-cni-bin
         - mountPath: /etc/cni/net.d
           name: host-etc-cni-netd
+        {% if kind is defined and kind -%}
         - mountPath: /var/run/netns
           name: host-netns
           mountPropagation: Bidirectional
+        {% endif %}
 
         resources:
           requests:
@@ -278,8 +282,10 @@ spec:
       - name: host-config-openvswitch
         hostPath:
           path: /etc/origin/openvswitch
+      {% if kind is defined and kind -%}
       - name: host-netns
         hostPath:
           path: /var/run/netns
+      {% endif %}
       tolerations:
       - operator: "Exists"

commit c96e4e0c0249ac4b78b644d17b488d885976bfa1

daemonset.sh on ubuntu fails since it can't find j2 utility
the j2 utility gets installed in ~/.local/bin, so use that path in
the script. while there, i also removed some unused variables.

Fixes: 3b6d22cad910 (Make KIND specific yaml rendered)
Signed-off-by: Girish Moodalbail <gmoodalbail@nvidia.com>

dist/images/daemonset.sh
@@ -120,13 +120,12 @@ echo "ovn_db_vip: ${ovn_db_vip}"
 # Simplified expansion of template 
 image_str="{{ ovn_image | default('docker.io/ovnkube/ovn-daemonset:latest') }}"
 policy_str="{{ ovn_image_pull_policy | default('IfNotPresent') }}"
-ovn_gateway_opts_repl="{{ ovn_gateway_opts }}"
-ovn_gateway_mode_repl="{{ ovn_gateway_mode }}"
 ovn_db_vip_image_repl="{{ ovn_db_vip_image | default('docker.io/ovnkube/ovndb-vip-u:latest') }}"
 ovn_db_replicas_repl="{{ ovn_db_replicas | default(3) }}"
 ovn_db_vip_repl="{{ ovn_db_vip }}"
 
-ovn_image=${image} ovn_image_pull_policy=${policy} kind=${KIND} ovn_gateway_mode=${ovn_gateway_mode} ovn_gateway_opts=${ovn_gateway_opts} j2 ../templates/ovnkube-node.yaml.j2 -o ../yaml/ovnkube-node.yaml
+ovn_image=${image} ovn_image_pull_policy=${policy} kind=${KIND} ovn_gateway_mode=${ovn_gateway_mode} \
+ ovn_gateway_opts=${ovn_gateway_opts} ~/.local/bin/j2 ../templates/ovnkube-node.yaml.j2 -o ../yaml/ovnkube-node.yaml
 
 sed "s,${image_str},${image},
 s,${policy_str},${policy}," ../templates/ovnkube-master.yaml.j2 > ../yaml/ovnkube-master.yaml