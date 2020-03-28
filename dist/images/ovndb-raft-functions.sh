ovn-org/ovn-kubernetes

commit 5e98a62ab047acca356b67d485aea87471a011a6

use 'ovsdb-client wait' to check the running state of OVN DB servers
From the manpage of `ovsdb-client wait`, we have:

  wait [server] database state
     Waits for database on server to enter a desired state, which may
     be one of:

     connected
       Waits until a database with the given name has been added
       to server.  Then, if database is clustered,  additionally
       waits until it has joined and connected to its cluster.

So, instead of checking `pid` associated with the process is present.
The above command looks more appropriate.

Signed-off-by: Girish Moodalbail <gmoodalbail@nvidia.com>

dist/images/ovndb-raft-functions.sh
@@ -122,7 +122,15 @@ ovsdb-raft () {
       --ovn-${db}-log="${ovn_log_db}" &
   fi
 
-  wait_for_event process_ready ovn${db}_db
+  # Following command waits for the database on server to enter a `connected` state
+  # -- Waits until a database with the given name has been added to server. Then, if database
+  # is clustered, additionally waits until it has joined and connected to its cluster.
+  echo "waiting for ${database} to join and connect to the cluster."
+  /usr/bin/ovsdb-client -t 120 wait unix:${OVN_RUNDIR}/ovn${db}_db.sock ${database} connected
+  if [[ $? != 0 ]]; then
+    echo "the ${database} has not yet joined and connected to its cluster. Exiting..."
+    exit 1
+  fi
   echo "=============== ${db}-ovsdb-raft ========== RUNNING"
 
   if [[ "${POD_NAME}" == "ovnkube-db-0" && "${initialize}" == "true" ]] ; then

commit 642d32d9a713b7f9a454635a1238ab63f7126b7e

define the missing database variable
while cherry picking the changes from my internal repo to upstream repo
I missed adding the database shell variable

Fixes: 5e98a62ab047 (use 'ovsdb-client wait' to check the running state
of OVN DB servers)

Signed-off-by: Girish Moodalbail <gmoodalbail@nvidia.com>

dist/images/ovndb-raft-functions.sh
@@ -94,6 +94,10 @@ ovsdb-raft () {
   ovn_db_pidfile=${OVN_RUNDIR}/ovn${db}_db.pid
   eval ovn_log_db=\$ovn_log_${db}
   ovn_db_file=${OVN_ETCDIR}/ovn${db}_db.db
+  database="OVN_Northbound"
+  if [[ ${db} == "sb" ]]; then
+    database="OVN_Southbound"
+  fi
 
   rm -f ${ovn_db_pidfile}
   verify-ovsdb-raft