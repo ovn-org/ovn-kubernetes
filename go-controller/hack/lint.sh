8f1edff7dd2a284c731dd3b8f379d540b4818dc4
Tim Rozet <trozet@redhat.com>
2020-04-07 10:23:34 -0400

    Fix golang linter timeouts
    Changes-Include:
     - Use verbose print resource usage
     - Use vendor folder to avoid having to download deps
     - Bump Timeout to 15 min
    
    Using vendor for download saves about a minute; we see that the
    entire execution is around 9-10 minutes, while our timeout was 10 min.
    
    Closes #1222
    
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- go-controller/hack/lint.sh
+++ go-controller/hack/lint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-
 GO111MODULE=on ${GOPATH}/bin/golangci-lint run \
     --tests=false --enable gofmt \
-    --timeout=10m0s \
+    --timeout=15m0s --verbose --print-resources-usage --modules-download-mode=vendor \
     && echo "lint OK!"
