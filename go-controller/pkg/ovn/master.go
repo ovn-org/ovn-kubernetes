ovn-org/ovn-kubernetes

commit 3677530d63fef71b9de3fa1261b49ed8e0e5f8f5

ovn: remove gratuitous string/IPNet conversions around subnet allocation

go-controller/pkg/ovn/master.go
@@ -85,14 +85,14 @@ func (oc *Controller) StartClusterMaster(masterNodeName string) error {
 	for _, node := range existingNodes.Items {
 		hostsubnet, _ := parseNodeHostSubnet(&node)
 		if hostsubnet != nil {
-			err := oc.masterSubnetAllocator.MarkAllocatedNetwork(hostsubnet.String())
+			err := oc.masterSubnetAllocator.MarkAllocatedNetwork(hostsubnet)
 			if err != nil {
 				utilruntime.HandleError(err)
 			}
 		}
 		joinsubnet, _ := parseNodeJoinSubnet(&node)
 		if joinsubnet != nil {
-			err := oc.joinSubnetAllocator.MarkAllocatedNetwork(joinsubnet.String())
+			err := oc.joinSubnetAllocator.MarkAllocatedNetwork(joinsubnet)
 			if err != nil {
 				utilruntime.HandleError(err)
 			}
@@ -223,36 +223,37 @@ func (oc *Controller) addNodeJoinSubnetAnnotations(node *kapi.Node, subnet strin
 	return nil
 }
 
-func (oc *Controller) allocateJoinSubnet(node *kapi.Node) (string, error) {
-	joinSubnet, _ := parseNodeJoinSubnet(node)
-	if joinSubnet != nil {
-		return joinSubnet.String(), nil
+func (oc *Controller) allocateJoinSubnet(node *kapi.Node) (*net.IPNet, error) {
+	joinSubnet, err := parseNodeJoinSubnet(node)
+	if err == nil {
+		return joinSubnet, nil
 	}
 
 	// Allocate a new network for the join switch
-	joinSubnetStr, err := oc.joinSubnetAllocator.AllocateNetwork()
+	joinSubnet, err = oc.joinSubnetAllocator.AllocateNetwork()
 	if err != nil {
-		return "", fmt.Errorf("Error allocating subnet for join switch for node  %s: %v", node.Name, err)
+		return nil, fmt.Errorf("Error allocating subnet for join switch for node  %s: %v", node.Name, err)
 	}
 
 	defer func() {
 		// Release the allocation on error
 		if err != nil {
-			_ = oc.joinSubnetAllocator.ReleaseNetwork(joinSubnetStr)
+			_ = oc.joinSubnetAllocator.ReleaseNetwork(joinSubnet)
 		}
 	}()
+
 	// Set annotation on the node
-	err = oc.addNodeJoinSubnetAnnotations(node, joinSubnetStr)
+	err = oc.addNodeJoinSubnetAnnotations(node, joinSubnet.String())
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	klog.Infof("Allocated join subnet %q for node %q", node.Name, joinSubnetStr)
-	return joinSubnetStr, nil
+	klog.Infof("Allocated join subnet %q for node %q", joinSubnet.String(), node.Name)
+	return joinSubnet, nil
 }
 
 func (oc *Controller) deleteNodeJoinSubnet(nodeName string, subnet *net.IPNet) error {
-	err := oc.joinSubnetAllocator.ReleaseNetwork(subnet.String())
+	err := oc.joinSubnetAllocator.ReleaseNetwork(subnet)
 	if err != nil {
 		return fmt.Errorf("Error deleting join subnet %v for node %q: %s", subnet, nodeName, err)
 	}
@@ -451,12 +452,12 @@ func (oc *Controller) syncGatewayLogicalNetwork(node *kapi.Node, l3GatewayConfig
 	}
 
 	// get a subnet for the per-node join switch
-	joinSubnetStr, err := oc.allocateJoinSubnet(node)
+	joinSubnet, err := oc.allocateJoinSubnet(node)
 	if err != nil {
 		return err
 	}
 
-	err = util.GatewayInit(clusterSubnets, joinSubnetStr, systemID, node.Name, ifaceID, ipAddress,
+	err = util.GatewayInit(clusterSubnets, joinSubnet, systemID, node.Name, ifaceID, ipAddress,
 		gwMacAddress, gwNextHop, subnet, nodePortEnable, lspArgs)
 	if err != nil {
 		return fmt.Errorf("failed to init shared interface gateway: %v", err)
@@ -719,21 +720,16 @@ func (oc *Controller) addNode(node *kapi.Node) (hostsubnet *net.IPNet, err error
 	}
 
 	// Node doesn't have a subnet assigned; reserve a new one for it
-	hostsubnetStr, err := oc.masterSubnetAllocator.AllocateNetwork()
+	hostsubnet, err = oc.masterSubnetAllocator.AllocateNetwork()
 	if err != nil {
 		return nil, fmt.Errorf("Error allocating network for node %s: %v", node.Name, err)
 	}
-	klog.Infof("Allocated node %s HostSubnet %s", node.Name, hostsubnetStr)
-
-	_, hostsubnet, err = net.ParseCIDR(hostsubnetStr)
-	if err != nil {
-		return nil, fmt.Errorf("Error in parsing hostsubnet %s - %v", hostsubnetStr, err)
-	}
+	klog.Infof("Allocated node %s HostSubnet %s", node.Name, hostsubnet.String())
 
 	defer func() {
 		// Release the allocation on error
 		if err != nil {
-			_ = oc.masterSubnetAllocator.ReleaseNetwork(hostsubnetStr)
+			_ = oc.masterSubnetAllocator.ReleaseNetwork(hostsubnet)
 		}
 	}()
 
@@ -755,7 +751,7 @@ func (oc *Controller) addNode(node *kapi.Node) (hostsubnet *net.IPNet, err error
 }
 
 func (oc *Controller) deleteNodeHostSubnet(nodeName string, subnet *net.IPNet) error {
-	err := oc.masterSubnetAllocator.ReleaseNetwork(subnet.String())
+	err := oc.masterSubnetAllocator.ReleaseNetwork(subnet)
 	if err != nil {
 		return fmt.Errorf("Error deleting subnet %v for node %q: %s", subnet, nodeName, err)
 	}

go-controller/pkg/ovn/allocator/allocator.go
@@ -34,49 +34,41 @@ func (sna *SubnetAllocator) AddNetworkRange(network string, hostBits uint32) err
 	return nil
 }
 
-func (sna *SubnetAllocator) MarkAllocatedNetwork(subnet string) error {
+func (sna *SubnetAllocator) MarkAllocatedNetwork(subnet *net.IPNet) error {
 	sna.Lock()
 	defer sna.Unlock()
 
-	_, ipnet, err := net.ParseCIDR(subnet)
-	if err != nil {
-		return err
-	}
 	for _, snr := range sna.ranges {
-		if snr.markAllocatedNetwork(ipnet) {
+		if snr.markAllocatedNetwork(subnet) {
 			return nil
 		}
 	}
-	return fmt.Errorf("network %s does not belong to any known range", subnet)
+	return fmt.Errorf("network %s does not belong to any known range", subnet.String())
 }
 
-func (sna *SubnetAllocator) AllocateNetwork() (string, error) {
+func (sna *SubnetAllocator) AllocateNetwork() (*net.IPNet, error) {
 	sna.Lock()
 	defer sna.Unlock()
 
 	for _, snr := range sna.ranges {
 		sn := snr.allocateNetwork()
 		if sn != nil {
-			return sn.String(), nil
+			return sn, nil
 		}
 	}
-	return "", ErrSubnetAllocatorFull
+	return nil, ErrSubnetAllocatorFull
 }
 
-func (sna *SubnetAllocator) ReleaseNetwork(subnet string) error {
+func (sna *SubnetAllocator) ReleaseNetwork(subnet *net.IPNet) error {
 	sna.Lock()
 	defer sna.Unlock()
 
-	_, ipnet, err := net.ParseCIDR(subnet)
-	if err != nil {
-		return err
-	}
 	for _, snr := range sna.ranges {
-		if snr.releaseNetwork(ipnet) {
+		if snr.releaseNetwork(subnet) {
 			return nil
 		}
 	}
-	return fmt.Errorf("network %s does not belong to any known range", subnet)
+	return fmt.Errorf("network %s does not belong to any known range", subnet.String())
 }
 
 // subnetAllocatorRange handles allocating subnets out of a single CIDR

go-controller/pkg/ovn/allocator/allocator_test.go
@@ -29,15 +29,15 @@ func allocateExpected(sna *SubnetAllocator, n int, expected string) error {
 	if err != nil {
 		return fmt.Errorf("failed to allocate %s (%s): %v", networkID(n), expected, err)
 	}
-	if sn != expected {
-		return fmt.Errorf("failed to allocate %s: expected %s, got %s", networkID(n), expected, sn)
+	if sn.String() != expected {
+		return fmt.Errorf("failed to allocate %s: expected %s, got %s", networkID(n), expected, sn.String())
 	}
 	return nil
 }
 
 func allocateNotExpected(sna *SubnetAllocator, n int) error {
 	if sn, err := sna.AllocateNetwork(); err == nil {
-		return fmt.Errorf("unexpectedly succeeded in allocating %s (sn=%s)", networkID(n), sn)
+		return fmt.Errorf("unexpectedly succeeded in allocating %s (sn=%s)", networkID(n), sn.String())
 	} else if err != ErrSubnetAllocatorFull {
 		return fmt.Errorf("returned error was not ErrSubnetAllocatorFull (%v)", err)
 	}
@@ -287,7 +287,7 @@ func TestMarkAllocatedNetwork(t *testing.T) {
 		t.Fatal("Failed to initialize IP allocator: ", err)
 	}
 
-	allocSubnets := make([]string, 4)
+	allocSubnets := make([]*net.IPNet, 4)
 	for i := 0; i < 4; i++ {
 		if allocSubnets[i], err = sna.AllocateNetwork(); err != nil {
 			t.Fatal("Failed to allocate network: ", err)
@@ -295,24 +295,24 @@ func TestMarkAllocatedNetwork(t *testing.T) {
 	}
 
 	if sn, err := sna.AllocateNetwork(); err == nil {
-		t.Fatalf("Unexpectedly succeeded in allocating network (sn=%s)", sn)
+		t.Fatalf("Unexpectedly succeeded in allocating network (sn=%s)", sn.String())
 	}
 	if err := sna.ReleaseNetwork(allocSubnets[2]); err != nil {
-		t.Fatalf("Failed to release the subnet (allocSubnets[2]=%s): %v", allocSubnets[2], err)
+		t.Fatalf("Failed to release the subnet (allocSubnets[2]=%s): %v", allocSubnets[2].String(), err)
 	}
 	for i := 0; i < 2; i++ {
 		if err := sna.MarkAllocatedNetwork(allocSubnets[2]); err != nil {
-			t.Fatalf("Failed to mark allocated subnet (allocSubnets[2]=%s): %v", allocSubnets[2], err)
+			t.Fatalf("Failed to mark allocated subnet (allocSubnets[2]=%s): %v", allocSubnets[2].String(), err)
 		}
 	}
 	if sn, err := sna.AllocateNetwork(); err == nil {
-		t.Fatalf("Unexpectedly succeeded in allocating network (sn=%s)", sn)
+		t.Fatalf("Unexpectedly succeeded in allocating network (sn=%s)", sn.String())
 	}
 
 	// Test subnet that does not belong to network
-	sn := "10.2.3.4/24"
-	if err := sna.MarkAllocatedNetwork(sn); err == nil {
-		t.Fatalf("Unexpectedly succeeded in marking allocated subnet that doesn't belong to network (sn=%s)", sn)
+	_, subnet, _ := net.ParseCIDR("10.2.3.4/24")
+	if err := sna.MarkAllocatedNetwork(subnet); err == nil {
+		t.Fatalf("Unexpectedly succeeded in marking allocated subnet that doesn't belong to network (sn=%s)", subnet.String())
 	}
 }
 
@@ -322,15 +322,15 @@ func TestAllocateReleaseSubnet(t *testing.T) {
 		t.Fatal("Failed to initialize IP allocator: ", err)
 	}
 
-	var releaseSn string
+	var releaseSn *net.IPNet
 
 	for i := 0; i < 4; i++ {
 		sn, err := sna.AllocateNetwork()
 		if err != nil {
 			t.Fatal("Failed to allocate network: ", err)
 		}
-		if sn != fmt.Sprintf("10.1.%d.0/18", i*64) {
-			t.Fatalf("Did not get expected subnet (i=%d, sn=%s)", i, sn)
+		if sn.String() != fmt.Sprintf("10.1.%d.0/18", i*64) {
+			t.Fatalf("Did not get expected subnet (i=%d, sn=%s)", i, sn.String())
 		}
 		if i == 2 {
 			releaseSn = sn
@@ -339,7 +339,7 @@ func TestAllocateReleaseSubnet(t *testing.T) {
 
 	sn, err := sna.AllocateNetwork()
 	if err == nil {
-		t.Fatalf("Unexpectedly succeeded in allocating network (sn=%s)", sn)
+		t.Fatalf("Unexpectedly succeeded in allocating network (sn=%s)", sn.String())
 	}
 
 	if err := sna.ReleaseNetwork(releaseSn); err != nil {
@@ -350,13 +350,13 @@ func TestAllocateReleaseSubnet(t *testing.T) {
 	if err != nil {
 		t.Fatal("Failed to allocate network: ", err)
 	}
-	if sn != releaseSn {
-		t.Fatalf("Did not get expected subnet (sn=%s)", sn)
+	if sn.String() != releaseSn.String() {
+		t.Fatalf("Did not get expected subnet (sn=%s)", sn.String())
 	}
 
 	sn, err = sna.AllocateNetwork()
 	if err == nil {
-		t.Fatalf("Unexpectedly succeeded in allocating network (sn=%s)", sn)
+		t.Fatalf("Unexpectedly succeeded in allocating network (sn=%s)", sn.String())
 	}
 }
 
@@ -386,11 +386,13 @@ func TestMultipleSubnets(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if err := sna.ReleaseNetwork("10.1.128.0/18"); err != nil {
-		t.Fatalf("Failed to release the subnet 10.1.128.0/18: %v", err)
+	_, sn, _ := net.ParseCIDR("10.1.128.0/18")
+	if err := sna.ReleaseNetwork(sn); err != nil {
+		t.Fatalf("Failed to release the subnet %s: %v", sn.String(), err)
 	}
-	if err := sna.ReleaseNetwork("10.2.128.0/18"); err != nil {
-		t.Fatalf("Failed to release the subnet 10.2.128.0/18: %v", err)
+	_, sn, _ = net.ParseCIDR("10.2.128.0/18")
+	if err := sna.ReleaseNetwork(sn); err != nil {
+		t.Fatalf("Failed to release the subnet %s: %v", sn.String(), err)
 	}
 
 	if err := allocateExpected(sna, -1, "10.1.128.0/18"); err != nil {

go-controller/pkg/util/gateway-init.go
@@ -102,7 +102,7 @@ func getGatewayLoadBalancers(gatewayRouter string) (string, string, error) {
 }
 
 // GatewayInit creates a gateway router for the local chassis.
-func GatewayInit(clusterIPSubnet []string, joinSubnetStr, systemID, nodeName, ifaceID, nicIP, nicMacAddress,
+func GatewayInit(clusterIPSubnet []string, joinSubnet *net.IPNet, systemID, nodeName, ifaceID, nicIP, nicMacAddress,
 	defaultGW string, rampoutIPSubnet string, nodePortEnable bool, lspArgs []string) error {
 
 	ip, physicalIPNet, err := net.ParseCIDR(nicIP)
@@ -129,7 +129,6 @@ func GatewayInit(clusterIPSubnet []string, joinSubnetStr, systemID, nodeName, if
 			"stderr: %q, error: %v", gatewayRouter, stdout, stderr, err)
 	}
 
-	_, joinSubnet, _ := net.ParseCIDR(joinSubnetStr)
 	prefixLen, _ := joinSubnet.Mask.Size()
 	gwLRPIp := NextIP(joinSubnet.IP)
 	drLRPIp := NextIP(gwLRPIp)

commit 0522d1a0a30bc72971acc407561c76164ba1ba24

regression: fix nil-pointer reference panic in defer function
Fixes: 3677530d63fe (ovn: remove gratuitous string/IPNet conversions
around subnet allocation)

Signed-off-by: Girish Moodalbail <gmoodalbail@nvidia.com>

go-controller/pkg/ovn/master.go
@@ -599,10 +599,10 @@ func (oc *Controller) addNodeAnnotations(node *kapi.Node, subnet *net.IPNet) err
 	return nil
 }
 
-func (oc *Controller) addNode(node *kapi.Node) (hostsubnet *net.IPNet, err error) {
+func (oc *Controller) addNode(node *kapi.Node) (*net.IPNet, error) {
 	oc.clearInitialNodeNetworkUnavailableCondition(node, nil)
 
-	hostsubnet, _ = util.ParseNodeHostSubnetAnnotation(node)
+	hostsubnet, _ := util.ParseNodeHostSubnetAnnotation(node)
 	if hostsubnet != nil {
 		// Node already has subnet assigned; ensure its logical network is set up
 		return hostsubnet, oc.ensureNodeLogicalNetwork(node.Name, hostsubnet)