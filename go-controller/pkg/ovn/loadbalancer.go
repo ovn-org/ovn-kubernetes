52d643f557082b67c3b80051a59c272ba7d9561f
Dan Winship <danw@redhat.com>
2020-04-21 15:42:18 -0400

    ovn: fix format of IPv6 ACL names
    Signed-off-by: Dan Winship <danw@redhat.com>

--- go-controller/pkg/ovn/loadbalancer.go
+++ go-controller/pkg/ovn/loadbalancer.go
@@ -210,7 +210,8 @@ func (ovn *Controller) createLoadBalancerRejectACL(lb string, sourceIP string, s
 		l3Prefix = "ip4"
 	}
 	vip := util.JoinHostPortInt32(sourceIP, sourcePort)
-	aclName := fmt.Sprintf("%s-%s", lb, vip)
+	// NOTE: doesn't use vip, to avoid having brackets in the name with IPv6
+	aclName := fmt.Sprintf("%s-%s:%d", lb, sourceIP, sourcePort)
 	// If ovn-k8s was restarted, we lost the cache, and an ACL may already exist in OVN. In that case we need to check
 	// using ACL name
 	aclUUID, stderr, err := util.RunOVNNbctl("--data=bare", "--no-heading", "--columns=_uuid", "find", "acl",
