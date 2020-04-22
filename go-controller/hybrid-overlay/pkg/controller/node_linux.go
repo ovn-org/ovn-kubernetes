ovn-org/ovn-kubernetes

commit 0303efa4de5a134a06b6701679fc185b7efc0ed4

Update pod annotation and CNI shim to allow dual-stack
Signed-off-by: Dan Winship <danw@redhat.com>

go-controller/hybrid-overlay/pkg/controller/node_linux.go
@@ -55,17 +55,19 @@ func podToCookie(pod *kapi.Pod) string {
 }
 
 func (n *NodeController) addOrUpdatePod(pod *kapi.Pod) error {
-	podIP, podMAC, err := getPodDetails(pod, n.nodeName)
+	podIPs, podMAC, err := getPodDetails(pod, n.nodeName)
 	if err != nil {
 		klog.V(5).Infof("cleaning up hybrid overlay pod %s/%s because %v", pod.Namespace, pod.Name, err)
 		return n.deletePod(pod)
 	}
 
 	cookie := podToCookie(pod)
-	_, _, err = util.RunOVSOfctl("add-flow", extBridgeName,
-		fmt.Sprintf("table=10,cookie=0x%s,priority=100,ip,nw_dst=%s,actions=set_field:%s->eth_src,set_field:%s->eth_dst,output:ext", cookie, podIP.IP, n.drMAC, podMAC))
-	if err != nil {
-		return fmt.Errorf("failed to add flows for pod %s/%s: %v", pod.Namespace, pod.Name, err)
+	for _, podIP := range podIPs {
+		_, _, err = util.RunOVSOfctl("add-flow", extBridgeName,
+			fmt.Sprintf("table=10,cookie=0x%s,priority=100,ip,nw_dst=%s,actions=set_field:%s->eth_src,set_field:%s->eth_dst,output:ext", cookie, podIP.IP, n.drMAC, podMAC))
+		if err != nil {
+			return fmt.Errorf("failed to add flows for pod %s/%s: %v", pod.Namespace, pod.Name, err)
+		}
 	}
 	return nil
 }
@@ -79,7 +81,7 @@ func (n *NodeController) deletePod(pod *kapi.Pod) error {
 	return nil
 }
 
-func getPodDetails(pod *kapi.Pod, nodeName string) (*net.IPNet, net.HardwareAddr, error) {
+func getPodDetails(pod *kapi.Pod, nodeName string) ([]*net.IPNet, net.HardwareAddr, error) {
 	if pod.Spec.NodeName != nodeName {
 		return nil, nil, fmt.Errorf("not scheduled")
 	}
@@ -88,14 +90,23 @@ func getPodDetails(pod *kapi.Pod, nodeName string) (*net.IPNet, net.HardwareAddr
 	if err != nil {
 		return nil, nil, err
 	}
-	return podInfo.IP, podInfo.MAC, nil
+	return podInfo.IPs, podInfo.MAC, nil
 }
 
 // podChanged returns true if any relevant pod attributes changed
 func podChanged(pod1 *kapi.Pod, pod2 *kapi.Pod, nodeName string) bool {
-	podIP1, mac1, _ := getPodDetails(pod1, nodeName)
-	podIP2, mac2, _ := getPodDetails(pod2, nodeName)
-	return !reflect.DeepEqual(podIP1, podIP2) || !reflect.DeepEqual(mac1, mac2)
+	podIPs1, mac1, _ := getPodDetails(pod1, nodeName)
+	podIPs2, mac2, _ := getPodDetails(pod2, nodeName)
+
+	if len(podIPs1) != len(podIPs2) || !reflect.DeepEqual(mac1, mac2) {
+		return false
+	}
+	for i := range podIPs1 {
+		if podIPs1[i].String() != podIPs2[i].String() {
+			return false
+		}
+	}
+	return true
 }
 
 func (n *NodeController) syncPods(pods []interface{}) {

go-controller/pkg/cni/cni.go
@@ -3,6 +3,7 @@ package cni
 import (
 	"encoding/json"
 	"fmt"
+	"net"
 	"net/http"
 	"time"
 
@@ -174,22 +175,33 @@ func (pr *PodRequest) getCNIResult(podInterfaceInfo *PodInterfaceInfo) (*current
 		return nil, fmt.Errorf("failed to configure pod interface: %v", err)
 	}
 
-	// Build the result structure to pass back to the runtime
-	var ipVersion string
-	if utilnet.IsIPv6(podInterfaceInfo.IP.IP) {
-		ipVersion = "6"
-	} else {
-		ipVersion = "4"
+	gateways := map[string]net.IP{}
+	for _, gw := range podInterfaceInfo.Gateways {
+		if gw.To4() != nil && gateways["4"] == nil {
+			gateways["4"] = gw
+		} else if gw.To4() == nil && gateways["6"] == nil {
+			gateways["6"] = gw
+		}
 	}
+
+	// Build the result structure to pass back to the runtime
+	ips := []*current.IPConfig{}
+	for _, ipcidr := range podInterfaceInfo.IPs {
+		ip := &current.IPConfig{
+			Interface: current.Int(1),
+			Address:   *ipcidr,
+		}
+		if utilnet.IsIPv6CIDR(ipcidr) {
+			ip.Version = "6"
+		} else {
+			ip.Version = "4"
+		}
+		ip.Gateway = gateways[ip.Version]
+		ips = append(ips, ip)
+	}
+
 	return &current.Result{
 		Interfaces: interfacesArray,
-		IPs: []*current.IPConfig{
-			{
-				Version:   ipVersion,
-				Interface: current.Int(1),
-				Address:   *podInterfaceInfo.IP,
-				Gateway:   podInterfaceInfo.GW,
-			},
-		},
+		IPs:        ips,
 	}, nil
 }

go-controller/pkg/cni/helper_linux.go
@@ -60,17 +60,17 @@ func setupNetwork(link netlink.Link, ifInfo *PodInterfaceInfo) error {
 	if err := netlink.LinkSetHardwareAddr(link, ifInfo.MAC); err != nil {
 		return fmt.Errorf("failed to add mac address %s to %s: %v", ifInfo.MAC, link.Attrs().Name, err)
 	}
-	addr := &netlink.Addr{IPNet: ifInfo.IP}
-	if err := netlink.AddrAdd(link, addr); err != nil {
-		return fmt.Errorf("failed to add IP addr %s to %s: %v", ifInfo.IP, link.Attrs().Name, err)
+	for _, ip := range ifInfo.IPs {
+		addr := &netlink.Addr{IPNet: ip}
+		if err := netlink.AddrAdd(link, addr); err != nil {
+			return fmt.Errorf("failed to add IP addr %s to %s: %v", ip, link.Attrs().Name, err)
+		}
 	}
-
-	if ifInfo.GW != nil {
-		if err := ip.AddRoute(nil, ifInfo.GW, link); err != nil {
+	for _, gw := range ifInfo.Gateways {
+		if err := ip.AddRoute(nil, gw, link); err != nil {
 			return fmt.Errorf("failed to add gateway route: %v", err)
 		}
 	}
-
 	for _, route := range ifInfo.Routes {
 		if err := ip.AddRoute(route.Dest, route.NextHop, link); err != nil {
 			return fmt.Errorf("failed to add pod route %v via %v: %v", route.Dest, route.NextHop, err)
@@ -253,13 +253,18 @@ func (pr *PodRequest) ConfigureInterface(namespace string, podName string, ifInf
 		}
 	}
 
+	ipStrs := make([]string, len(ifInfo.IPs))
+	for i, ip := range ifInfo.IPs {
+		ipStrs[i] = ip.String()
+	}
+
 	// Add the new sandbox's OVS port
 	ovsArgs := []string{
 		"add-port", "br-int", hostIface.Name, "--", "set",
 		"interface", hostIface.Name,
 		fmt.Sprintf("external_ids:attached_mac=%s", ifInfo.MAC),
 		fmt.Sprintf("external_ids:iface-id=%s", ifaceID),
-		fmt.Sprintf("external_ids:ip_address=%s", ifInfo.IP),
+		fmt.Sprintf("external_ids:ip_addresses=%s", strings.Join(ipStrs, ",")),
 		fmt.Sprintf("external_ids:sandbox=%s", pr.SandboxID),
 	}
 

go-controller/pkg/cni/helper_windows.go
@@ -23,12 +23,12 @@ import (
 // attach it to the desired network. This function finds the HNS Id of the
 // network based on the gatewayIP. If more than one suitable network it's found,
 // return an error asking to give the HNS Network Id in config.
-func getHNSIdFromConfigOrByGatewayIP(gatewayIP net.IP) (string, error) {
+func getHNSIdFromConfigOrByGatewayIP(gatewayIPs []net.IP) (string, error) {
 	if config.CNI.WinHNSNetworkID != "" {
 		klog.Infof("Using HNS Network Id from config: %v", config.CNI.WinHNSNetworkID)
 		return config.CNI.WinHNSNetworkID, nil
 	}
-	if gatewayIP == nil {
+	if len(gatewayIPs) == 0 {
 		return "", fmt.Errorf("no gateway IP and no HNS Network ID given")
 	}
 	hnsNetworkId := ""
@@ -38,7 +38,7 @@ func getHNSIdFromConfigOrByGatewayIP(gatewayIP net.IP) (string, error) {
 	}
 	for _, hnsNW := range hnsNetworks {
 		for _, hnsNWSubnet := range hnsNW.Subnets {
-			if strings.Compare(gatewayIP.String(), hnsNWSubnet.GatewayAddress) == 0 {
+			if strings.Compare(gatewayIPs[0].String(), hnsNWSubnet.GatewayAddress) == 0 {
 				if len(hnsNetworkId) == 0 {
 					hnsNetworkId = hnsNW.Id
 				} else {
@@ -118,7 +118,11 @@ func (pr *PodRequest) ConfigureInterface(namespace string, podName string, ifInf
 	if conf.DeviceID != "" {
 		return nil, fmt.Errorf("failure OVS-Offload is not supported in Windows")
 	}
-	ipMaskSize, _ := ifInfo.IP.Mask.Size()
+	if len(ifInfo.IPs) != 1 {
+		return nil, fmt.Errorf("dual-stack is not supported in Windows")
+	}
+
+	ipMaskSize, _ := ifInfo.IPs[0].Mask.Size()
 	// NOTE(abalutoiu): The endpoint name should not depend on the container ID.
 	// This is for backwards compatibility in kubernetes which calls the CNI
 	// even for containers that are not the infra container.
@@ -137,7 +141,7 @@ func (pr *PodRequest) ConfigureInterface(namespace string, podName string, ifInf
 	}()
 
 	var hnsNetworkId string
-	hnsNetworkId, err = getHNSIdFromConfigOrByGatewayIP(ifInfo.GW)
+	hnsNetworkId, err = getHNSIdFromConfigOrByGatewayIP(ifInfo.Gateways)
 	if err != nil {
 		klog.Infof("Error when detecting the HNS Network Id: %q", err)
 		return nil, err
@@ -156,7 +160,7 @@ func (pr *PodRequest) ConfigureInterface(namespace string, podName string, ifInf
 		hnsEndpoint := &hcsshim.HNSEndpoint{
 			Name:           endpointName,
 			VirtualNetwork: hnsNetworkId,
-			IPAddress:      ifInfo.IP.IP,
+			IPAddress:      ifInfo.IPs[0].IP,
 			MacAddress:     macAddressIpFormat,
 			PrefixLength:   uint8(ipMaskSize),
 			DNSServerList:  strings.Join(conf.DNS.Nameservers, ","),
@@ -196,7 +200,7 @@ func (pr *PodRequest) ConfigureInterface(namespace string, podName string, ifInf
 		"interface", endpointName,
 		fmt.Sprintf("external_ids:attached_mac=%s", ifInfo.MAC),
 		fmt.Sprintf("external_ids:iface-id=%s", ifaceID),
-		fmt.Sprintf("external_ids:ip_address=%s", ifInfo.IP),
+		fmt.Sprintf("external_ids:ip_address=%s", ifInfo.IPs[0]),
 	}
 	var out []byte
 	out, err = exec.Command("ovs-vsctl", ovsArgs...).CombinedOutput()

go-controller/pkg/ovn/pods.go
@@ -234,7 +234,8 @@ func (oc *Controller) addLogicalPort(pod *kapi.Pod) error {
 	annotation, err := util.UnmarshalPodAnnotation(pod.Annotations)
 	if err == nil {
 		podMac = annotation.MAC
-		podCIDR = annotation.IP
+		// DUAL-STACK FIXME: handle multiple IPs
+		podCIDR = annotation.IPs[0]
 
 		// Check if the pod's logical switch port already exists. If it
 		// does don't re-add the port to OVN as this will change its
@@ -249,7 +250,7 @@ func (oc *Controller) addLogicalPort(pod *kapi.Pod) error {
 		// If the pod already has annotations use the existing static
 		// IP/MAC from the annotation.
 		args = append(args,
-			"--", "lsp-set-addresses", portName, fmt.Sprintf("%s %s", podMac, annotation.IP.IP),
+			"--", "lsp-set-addresses", portName, fmt.Sprintf("%s %s", podMac, podCIDR.IP),
 			"--", "--if-exists", "clear", "logical_switch_port", portName, "dynamic_addresses",
 		)
 	} else {
@@ -331,10 +332,10 @@ func (oc *Controller) addLogicalPort(pod *kapi.Pod) error {
 			return err
 		}
 		marshalledAnnotation, err := util.MarshalPodAnnotation(&util.PodAnnotation{
-			IP:     podCIDR,
-			MAC:    podMac,
-			GW:     gwIP,
-			Routes: routes,
+			IPs:      []*net.IPNet{podCIDR},
+			MAC:      podMac,
+			Gateways: []net.IP{gwIP},
+			Routes:   routes,
 		})
 		if err != nil {
 			return fmt.Errorf("error creating pod network annotation: %v", err)

go-controller/pkg/ovn/pods_test.go
@@ -244,7 +244,7 @@ var _ = Describe("OVN Pod Operations", func() {
 
 				podAnnotation, ok := pod.Annotations[util.OvnPodAnnotationName]
 				Expect(ok).To(BeTrue())
-				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_address":"` + t.podIP + `/24", "mac_address":"` + t.podMAC + `", "gateway_ip": "` + t.nodeGWIP + `"}}`))
+				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_addresses":["` + t.podIP + `/24"], "mac_address":"` + t.podMAC + `", "gateway_ips": ["` + t.nodeGWIP + `"], "ip_address":"` + t.podIP + `/24", "gateway_ip": "` + t.nodeGWIP + `"}}`))
 
 				return nil
 			}
@@ -291,7 +291,7 @@ var _ = Describe("OVN Pod Operations", func() {
 
 				podAnnotation, ok := pod.Annotations[util.OvnPodAnnotationName]
 				Expect(ok).To(BeTrue())
-				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_address":"` + t.podIP + `/24", "mac_address":"` + t.podMAC + `", "gateway_ip": "` + t.nodeGWIP + `"}}`))
+				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_addresses":["` + t.podIP + `/24"], "mac_address":"` + t.podMAC + `", "gateway_ips": ["` + t.nodeGWIP + `"], "ip_address":"` + t.podIP + `/24", "gateway_ip": "` + t.nodeGWIP + `"}}`))
 
 				return nil
 			}
@@ -335,7 +335,7 @@ var _ = Describe("OVN Pod Operations", func() {
 
 				podAnnotation, ok := pod.Annotations[util.OvnPodAnnotationName]
 				Expect(ok).To(BeTrue())
-				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_address":"` + t.podIP + `/24", "mac_address":"` + t.podMAC + `", "gateway_ip": "` + t.nodeGWIP + `"}}`))
+				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_addresses":["` + t.podIP + `/24"], "mac_address":"` + t.podMAC + `", "gateway_ips": ["` + t.nodeGWIP + `"], "ip_address":"` + t.podIP + `/24", "gateway_ip": "` + t.nodeGWIP + `"}}`))
 				Eventually(fExec.CalledMatchesExpected).Should(BeTrue(), fExec.ErrorDesc)
 
 				// Delete it
@@ -399,7 +399,7 @@ var _ = Describe("OVN Pod Operations", func() {
 
 				podAnnotation, ok := pod.Annotations[util.OvnPodAnnotationName]
 				Expect(ok).To(BeTrue())
-				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_address":"` + t.podIP + `/24", "mac_address":"` + t.podMAC + `", "gateway_ip": "` + t.nodeGWIP + `"}}`))
+				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_addresses":["` + t.podIP + `/24"], "mac_address":"` + t.podMAC + `", "gateway_ips": ["` + t.nodeGWIP + `"], "ip_address":"` + t.podIP + `/24", "gateway_ip": "` + t.nodeGWIP + `"}}`))
 
 				return nil
 			}
@@ -456,7 +456,7 @@ var _ = Describe("OVN Pod Operations", func() {
 
 				podAnnotation, ok := pod.Annotations[util.OvnPodAnnotationName]
 				Expect(ok).To(BeTrue())
-				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_address":"` + t.podIP + `/24", "mac_address":"` + t.podMAC + `", "gateway_ip": "` + t.nodeGWIP + `"}}`))
+				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_addresses":["` + t.podIP + `/24"], "mac_address":"` + t.podMAC + `", "gateway_ips": ["` + t.nodeGWIP + `"], "ip_address":"` + t.podIP + `/24", "gateway_ip": "` + t.nodeGWIP + `"}}`))
 
 				return nil
 			}
@@ -533,7 +533,7 @@ var _ = Describe("OVN Pod Operations", func() {
 
 				podAnnotation, ok := pod.Annotations[util.OvnPodAnnotationName]
 				Expect(ok).To(BeTrue())
-				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_address":"` + t.podIP + `/24", "mac_address":"` + t.podMAC + `", "gateway_ip": "` + t.nodeGWIP + `"}}`))
+				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_addresses":["` + t.podIP + `/24"], "mac_address":"` + t.podMAC + `", "gateway_ips": ["` + t.nodeGWIP + `"], "ip_address":"` + t.podIP + `/24", "gateway_ip": "` + t.nodeGWIP + `"}}`))
 
 				return nil
 			}
@@ -580,7 +580,7 @@ var _ = Describe("OVN Pod Operations", func() {
 
 				podAnnotation, ok := pod.Annotations[util.OvnPodAnnotationName]
 				Expect(ok).To(BeTrue())
-				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_address":"` + t.podIP + `/24", "mac_address":"` + t.podMAC + `", "gateway_ip": "` + t.nodeGWIP + `"}}`))
+				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_addresses":["` + t.podIP + `/24"], "mac_address":"` + t.podMAC + `", "gateway_ips": ["` + t.nodeGWIP + `"], "ip_address":"` + t.podIP + `/24", "gateway_ip": "` + t.nodeGWIP + `"}}`))
 
 				// Simulate an OVN restart with a new IP assignment and verify that the pod annotation is updated.
 				fExec.AddFakeCmd(&ovntest.ExpectedCmd{
@@ -606,7 +606,7 @@ var _ = Describe("OVN Pod Operations", func() {
 				// Check that pod annotations have been re-written to correct values
 				podAnnotation, ok = pod.Annotations[util.OvnPodAnnotationName]
 				Expect(ok).To(BeTrue())
-				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_address":"` + t.podIP + `/24", "mac_address":"` + t.podMAC + `", "gateway_ip": "` + t.nodeGWIP + `"}}`))
+				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_addresses":["` + t.podIP + `/24"], "mac_address":"` + t.podMAC + `", "gateway_ips": ["` + t.nodeGWIP + `"], "ip_address":"` + t.podIP + `/24", "gateway_ip": "` + t.nodeGWIP + `"}}`))
 
 				return nil
 			}
@@ -650,7 +650,7 @@ var _ = Describe("OVN Pod Operations", func() {
 
 				podAnnotation, ok := pod.Annotations[util.OvnPodAnnotationName]
 				Expect(ok).To(BeTrue())
-				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_address":"` + t.podIP + `/24", "mac_address":"` + t.podMAC + `", "gateway_ip": "` + t.nodeGWIP + `"}}`))
+				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_addresses":["` + t.podIP + `/24"], "mac_address":"` + t.podMAC + `", "gateway_ips": ["` + t.nodeGWIP + `"], "ip_address":"` + t.podIP + `/24", "gateway_ip": "` + t.nodeGWIP + `"}}`))
 
 				// Simulate an OVN restart with a new IP assignment and verify that the pod annotation is updated.
 				fExec.AddFakeCmd(&ovntest.ExpectedCmd{
@@ -676,7 +676,7 @@ var _ = Describe("OVN Pod Operations", func() {
 				// Check that pod annotations have been re-written to correct values
 				podAnnotation, ok = pod.Annotations[util.OvnPodAnnotationName]
 				Expect(ok).To(BeTrue())
-				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_address":"` + t.podIP + `/24", "mac_address":"` + t.podMAC + `", "gateway_ip": "` + t.nodeGWIP + `"}}`))
+				Expect(podAnnotation).To(MatchJSON(`{"default": {"ip_addresses":["` + t.podIP + `/24"], "mac_address":"` + t.podMAC + `", "gateway_ips": ["` + t.nodeGWIP + `"], "ip_address":"` + t.podIP + `/24", "gateway_ip": "` + t.nodeGWIP + `"}}`))
 
 				return nil
 			}

go-controller/pkg/ovn/policy.go
@@ -1145,7 +1145,8 @@ func (oc *Controller) handlePeerPodSelectorAddUpdate(np *namespacePolicy,
 	if err != nil {
 		return
 	}
-	ipAddress := podAnnotation.IP.IP.String()
+	// DUAL-STACK FIXME: handle multiple IPs
+	ipAddress := podAnnotation.IPs[0].IP.String()
 	if addressMap[ipAddress] {
 		return
 	}
@@ -1181,7 +1182,8 @@ func (oc *Controller) handlePeerPodSelectorDelete(np *namespacePolicy,
 	if err != nil {
 		return
 	}
-	ipAddress := podAnnotation.IP.IP.String()
+	// DUAL-STACK FIXME: handle multiple IPs
+	ipAddress := podAnnotation.IPs[0].IP.String()
 
 	np.Lock()
 	defer np.Unlock()

go-controller/pkg/util/pod_annotation.go
@@ -6,7 +6,37 @@ import (
 	"net"
 
 	"k8s.io/klog"
+	utilnet "k8s.io/utils/net"
 )
+
+// This handles the "k8s.ovn.org/pod-networks" annotation on Pods, used to pass
+// information about networking from the master to the nodes. (The util.PodAnnotation
+// struct is also embedded in the cni.PodInterfaceInfo type that is passed from the
+// cniserver to the CNI shim.)
+//
+// The annotation looks like:
+//
+//   annotations:
+//     k8s.ovn.org/pod-networks: |
+//       {
+//         "default": {
+//           "ip_addresses": ["192.168.0.5/24"],
+//           "mac_address": "0a:58:fd:98:00:01",
+//           "gateway_ips": ["192.168.0.1"]
+//
+//           # for backward compatibility
+//           "ip_address": "192.168.0.5/24",
+//           "gateway_ip": "192.168.0.1"
+//         }
+//       }
+//
+// (With optional additional "routes" also indicated; in particular, if a pod has an
+// additional network attachment that claims the default route, then the "default" network
+// will have explicit routes to the cluster and service subnets.)
+//
+// The "ip_address" and "gateway_ip" fields are deprecated and will eventually go away.
+// (And they are not output when "ip_addresses" or "gateway_ips" contains multiple
+// values.)
 
 const (
 	// OvnPodAnnotationName is the constant string representing the POD annotation key
@@ -15,15 +45,17 @@ const (
 	OvnPodDefaultNetwork = "default"
 )
 
-// PodAnnotation describes the pod's assigned network details
+// PodAnnotation describes the assigned network details for a single pod network. (The
+// actual annotation may include the equivalent of multiple PodAnnotations.)
 type PodAnnotation struct {
-	// IP is the pod's assigned IP address and prefix
-	IP *net.IPNet
+	// IPs are the pod's assigned IP addresses/prefixes
+	IPs []*net.IPNet
 	// MAC is the pod's assigned MAC address
 	MAC net.HardwareAddr
-	// GW is the pod's gateway IP address
-	GW net.IP
-	// Routes are routes to add to the pod's network namespace
+	// Gateways are the pod's gateway IP addresses; note that there may be
+	// fewer Gateways than IPs.
+	Gateways []net.IP
+	// Routes are additional routes to add to the pod's network namespace
 	Routes []PodRoute
 }
 
@@ -35,15 +67,18 @@ type PodRoute struct {
 	NextHop net.IP
 }
 
-// Internal struct used to correctly marshal IPs to JSON
+// Internal struct used to marshal PodAnnotation to the pod annotation
 type podAnnotation struct {
-	IP     string     `json:"ip_address"`
-	MAC    string     `json:"mac_address"`
-	GW     string     `json:"gateway_ip"`
-	Routes []podRoute `json:"routes,omitempty"`
+	IPs      []string   `json:"ip_addresses"`
+	MAC      string     `json:"mac_address"`
+	Gateways []string   `json:"gateway_ips,omitempty"`
+	Routes   []podRoute `json:"routes,omitempty"`
+
+	IP      string `json:"ip_address,omitempty"`
+	Gateway string `json:"gateway_ip,omitempty"`
 }
 
-// Internal struct used to correctly marshal IPs to JSON
+// Internal struct used to marshal PodRoute to the pod annotation
 type podRoute struct {
 	Dest    string `json:"dest"`
 	NextHop string `json:"nextHop"`
@@ -52,15 +87,25 @@ type podRoute struct {
 // MarshalPodAnnotation returns a JSON-formatted annotation describing the pod's
 // network details
 func MarshalPodAnnotation(podInfo *PodAnnotation) (map[string]string, error) {
-	var gw string
-	if podInfo.GW != nil {
-		gw = podInfo.GW.String()
-	}
 	pa := podAnnotation{
-		IP:  podInfo.IP.String(),
 		MAC: podInfo.MAC.String(),
-		GW:  gw,
 	}
+
+	if len(podInfo.IPs) == 1 {
+		pa.IP = podInfo.IPs[0].String()
+		if len(podInfo.Gateways) == 1 {
+			pa.Gateway = podInfo.Gateways[0].String()
+		} else if len(podInfo.Gateways) > 1 {
+			return nil, fmt.Errorf("bad podNetwork data: single-stack network can only have a single gateway")
+		}
+	}
+	for _, ip := range podInfo.IPs {
+		pa.IPs = append(pa.IPs, ip.String())
+	}
+	for _, gw := range podInfo.Gateways {
+		pa.Gateways = append(pa.Gateways, gw.String())
+	}
+
 	for _, r := range podInfo.Routes {
 		if r.Dest.IP.IsUnspecified() {
 			return nil, fmt.Errorf("bad podNetwork data: default route %v should be specified as gateway", r)
@@ -88,7 +133,7 @@ func MarshalPodAnnotation(podInfo *PodAnnotation) (map[string]string, error) {
 	}, nil
 }
 
-// UnmarshalPodAnnotation returns a the unmarshalled pod annotation
+// UnmarshalPodAnnotation returns the default network info from pod.Annotations
 func UnmarshalPodAnnotation(annotations map[string]string) (*PodAnnotation, error) {
 	ovnAnnotation, ok := annotations[OvnPodAnnotationName]
 	if !ok {
@@ -104,24 +149,43 @@ func UnmarshalPodAnnotation(annotations map[string]string) (*PodAnnotation, erro
 	a := &tempA
 
 	podAnnotation := &PodAnnotation{}
-	// Minimal validation
-	ip, ipnet, err := net.ParseCIDR(a.IP)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse pod IP %q: %v", a.IP, err)
-	}
-	ipnet.IP = ip
-	podAnnotation.IP = ipnet
+	var err error
 
 	podAnnotation.MAC, err = net.ParseMAC(a.MAC)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse pod MAC %q: %v", a.MAC, err)
 	}
 
-	if a.GW != "" {
-		podAnnotation.GW = net.ParseIP(a.GW)
-		if podAnnotation.GW == nil {
-			return nil, fmt.Errorf("failed to parse pod gateway %q", a.GW)
+	if len(a.IPs) == 0 {
+		if a.IP == "" {
+			return nil, fmt.Errorf("bad annotation data (neither ip_address nor ip_addresses is set)")
 		}
+		a.IPs = append(a.IPs, a.IP)
+	} else if a.IP != "" && a.IP != a.IPs[0] {
+		return nil, fmt.Errorf("bad annotation data (ip_address and ip_addresses conflict)")
+	}
+	for _, ipstr := range a.IPs {
+		ip, ipnet, err := net.ParseCIDR(ipstr)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse pod IP %q: %v", ipstr, err)
+		}
+		ipnet.IP = ip
+		podAnnotation.IPs = append(podAnnotation.IPs, ipnet)
+	}
+
+	if len(a.Gateways) == 0 {
+		if a.Gateway != "" {
+			a.Gateways = append(a.Gateways, a.Gateway)
+		}
+	} else if a.Gateway != "" && a.Gateway != a.Gateways[0] {
+		return nil, fmt.Errorf("bad annotation data (gateway_ip and gateway_ips conflict)")
+	}
+	for _, gwstr := range a.Gateways {
+		gw := net.ParseIP(gwstr)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse pod gateway %q", gwstr)
+		}
+		podAnnotation.Gateways = append(podAnnotation.Gateways, gw)
 	}
 
 	for _, r := range a.Routes {
@@ -137,6 +201,8 @@ func UnmarshalPodAnnotation(annotations map[string]string) (*PodAnnotation, erro
 			route.NextHop = net.ParseIP(r.NextHop)
 			if route.NextHop == nil {
 				return nil, fmt.Errorf("failed to parse pod route next hop %q", r.NextHop)
+			} else if utilnet.IsIPv6(route.NextHop) != utilnet.IsIPv6CIDR(route.Dest) {
+				return nil, fmt.Errorf("pod route %s has next hop %s of different family", r.Dest, r.NextHop)
 			}
 		}
 		podAnnotation.Routes = append(podAnnotation.Routes, route)

commit a39e555c1da3be95cf245f95eb658071b12ef24b

hybrid-overlay: fix podChanged() return values
It was returning exactly the opposite of what it was supposed to.

Fixes: 0303efa4de5a13

Signed-off-by: Dan Williams <dcbw@redhat.com>

go-controller/hybrid-overlay/pkg/controller/node_linux.go
@@ -98,14 +98,14 @@ func podChanged(pod1 *kapi.Pod, pod2 *kapi.Pod, nodeName string) bool {
 	podIPs2, mac2, _ := getPodDetails(pod2, nodeName)
 
 	if len(podIPs1) != len(podIPs2) || !reflect.DeepEqual(mac1, mac2) {
-		return false
+		return true
 	}
 	for i := range podIPs1 {
 		if podIPs1[i].String() != podIPs2[i].String() {
-			return false
+			return true
 		}
 	}
-	return true
+	return false
 }
 
 func (n *NodeController) syncPods(pods []interface{}) {