f25d8212c00dc4510960b383aad29dbd2d91ce06
Alexander Constantinescu <aconstan@redhat.com>
2020-05-04 17:52:57 +0200

    Updating update-codegen to accomodate multiple CRDs
    Signed-off-by: Alexander Constantinescu <aconstan@redhat.com>

--- go-controller/hack/lint.sh
+++ go-controller/hack/lint.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 GO111MODULE=on ${GOPATH}/bin/golangci-lint run \
-    --skip-dirs=pkg/crd/egressfirewall/v1/apis/ \
     --timeout=15m0s --verbose --print-resources-usage --modules-download-mode=vendor \
     && echo "lint OK!"
--- go-controller/hack/update-codegen.sh
+++ go-controller/hack/update-codegen.sh
@@ -4,6 +4,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+crds=$(ls pkg/crd 2> /dev/null)
+if [ -z "${crds}" ]; then
+  exit
+fi
+
 if  ! ( command -v controller-gen > /dev/null ); then
   # Need to take an unreleased version of controller-tools
   # because of controller-tools bug 302
@@ -18,34 +23,36 @@ if  ! ( command -v controller-gen > /dev/null ); then
   fi
 fi
 
-echo "Generating deepcopy funcs"
-deepcopy-gen \
-  --input-dirs github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/egressfirewall/v1 \
-  -O zz_generated.deepcopy \
-  --bounding-dirs github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd
+for crd in ${crds}; do
+  echo "Generating deepcopy funcs for $crd"
+  deepcopy-gen \
+    --input-dirs github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/$crd/v1 \
+    -O zz_generated.deepcopy \
+    --bounding-dirs github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd
 
 
-echo "Generating clientset"
-client-gen \
-  --clientset-name "${CLIENTSET_NAME_VERSIONED:-versioned}" \
-  --input-base "" \
-  --input github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/egressfirewall/v1 \
-  --output-package github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/egressfirewall/v1/apis/clientset \
-  "$@"
+  echo "Generating clientset for $crd"
+  client-gen \
+    --clientset-name "${CLIENTSET_NAME_VERSIONED:-versioned}" \
+    --input-base "" \
+    --input github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/$crd/v1 \
+    --output-package github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/$crd/v1/apis/clientset \
+    "$@"
 
-echo "Generating listers"
-lister-gen \
-  --input-dirs github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/egressfirewall/v1 \
-  --output-package github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/egressfirewall/v1/apis/listers \
-  "$@"
+  echo "Generating listers for $crd"
+  lister-gen \
+    --input-dirs github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/$crd/v1 \
+    --output-package github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/$crd/v1/apis/listers \
+    "$@"
 
-echo "Generating informers"
-informer-gen \
-  --input-dirs github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/egressfirewall/v1 \
-  --versioned-clientset-package github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/egressfirewall/v1/apis/clientset/versioned \
-  --listers-package  github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/egressfirewall/v1/apis/listers \
-  --output-package github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/egressfirewall/v1/apis/informers \
-  "$@"
+  echo "Generating informers for $crd"
+  informer-gen \
+    --input-dirs github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/$crd/v1 \
+    --versioned-clientset-package github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/$crd/v1/apis/clientset/versioned \
+    --listers-package  github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/$crd/v1/apis/listers \
+    --output-package github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/$crd/v1/apis/informers \
+    "$@"
+done
 
 echo "Generating CRDs"
 mkdir -p _output/crds
