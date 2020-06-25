5b1b684bad7918b9b0b50c9bbad398209761cbc1
Dave Tucker <dave@dtucker.co.uk>
2020-06-25 15:43:29 +0100

    test: Add Junit Reports for e2e test suites
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- test/scripts/e2e-cp.sh
+++ test/scripts/e2e-cp.sh
@@ -11,6 +11,8 @@ export KUBECONFIG=${HOME}/admin.conf
 export MASTER_NAME=${KIND_CLUSTER_NAME}-control-plane
 export NODE_NAMES=${MASTER_NAME}
 export KIND_INSTALL_INGRESS=${KIND_INSTALL_INGRESS}
+export E2E_REPORT_DIR=${E2E_REPORT_DIR}
+export E2E_REPORT_PREFIX=${E2E_REPORT_PREFIX}
 
 sed -E -i 's/"\$\{ginkgo\}" "\$\{ginkgo_args\[\@\]\:\+\$\{ginkgo_args\[\@\]\}\}" "\$\{e2e_test\}"/pushd \$GITHUB_WORKSPACE\/test\/e2e\nGO111MODULE=on "\$\{ginkgo\}" "\$\{ginkgo_args\[\@\]\:\+\$\{ginkgo_args\[\@\]\}\}"/' ${GOPATH}/src/k8s.io/kubernetes/hack/ginkgo-e2e.sh
 
