1fde3fd86054f4f21510649e93389840310e36bc
Vishwanath Jayaraman <vjayaram@redhat.com>
2020-07-16 07:55:16 -0500

    unit test for GetPortAddresses() reworked to support go-ovn bindings
    Signed-off-by: Vishwanath Jayaraman <vjayaram@redhat.com>

--- go-controller/pkg/util/net_unit_test.go
+++ go-controller/pkg/util/net_unit_test.go
@@ -7,6 +7,7 @@ import (
 
 	goovn "github.com/ebay/go-ovn"
 	ovntest "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/testing"
+	goovn_mocks "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/testing/mocks/ebay/go-ovn"
 	mock_k8s_io_utils_exec "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/testing/mocks/k8s.io/utils/exec"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util/mocks"
 	"github.com/stretchr/testify/assert"
@@ -61,93 +62,81 @@ func TestNextIP(t *testing.T) {
 }
 
 func TestGetPortAddresses(t *testing.T) {
-	mockKexecIface := new(mock_k8s_io_utils_exec.Interface)
-	mockExecRunner := new(mocks.ExecRunner)
-	mockCmd := new(mock_k8s_io_utils_exec.Cmd)
-	// below is defined in ovs.go
-	runCmdExecRunner = mockExecRunner
-	// note runner is defined in ovs.go file
-	runner = &execHelper{exec: mockKexecIface}
-	mockOVNNBClient := ovntest.NewMockOVNClient(goovn.DBNB)
-
+	mockOvnNBClient := new(goovn_mocks.Client)
 	tests := []struct {
-		desc                    string
-		input                   string
-		errExpected             bool
-		onRetArgsExecUtilsIface *onCallReturnArgs
-		onRetArgsKexecIface     *onCallReturnArgs
+		desc                 string
+		inpPort              string
+		errAssert            bool
+		errMatch             error
+		onRetArgsOvnNBClient *onCallReturnArgs
 	}{
-		// NOTE: May need a test to validate portName parameter that is passed to function
 		{
-			desc:                    "tests code path when RunOVNNbctl returns error",
-			input:                   "TEST_PORT",
-			errExpected:             false,
-			onRetArgsExecUtilsIface: &onCallReturnArgs{"RunCmd", []string{"*mocks.Cmd", "string", "[]string", "string", "string", "string", "string", "string", "string", "string"}, []interface{}{nil, nil, fmt.Errorf("executable file not found in $PATH")}},
-			onRetArgsKexecIface:     &onCallReturnArgs{"Command", []string{"string", "string", "string", "string", "string", "string", "string", "string"}, []interface{}{mockCmd}},
+			desc:                 "test path where LSPGet() returns goovn.ErrorSchema/goovn.ErrorNotFound",
+			inpPort:              "TEST_PORT",
+			onRetArgsOvnNBClient: &onCallReturnArgs{"LSPGet", []string{"string"}, []interface{}{nil, goovn.ErrorSchema}},
 		},
 		{
-			desc:                    "test code path when RunOVNNbctl returns valid output",
-			input:                   "TEST_PORT",
-			onRetArgsExecUtilsIface: &onCallReturnArgs{"RunCmd", []string{"*mocks.Cmd", "string", "[]string", "string", "string", "string", "string", "string", "string", "string"}, []interface{}{bytes.NewBuffer([]byte("22:e9:ac:f4:00:04 10.244.0.3\n[dynamic]")), nil, nil}},
-			onRetArgsKexecIface:     &onCallReturnArgs{"Command", []string{"string", "string", "string", "string", "string", "string", "string", "string"}, []interface{}{mockCmd}},
+			desc:                 "test path where LSPGet() returns an error other than goovn.ErrorSchema/goovn.ErrorNotFound",
+			inpPort:              "TEST_PORT",
+			errAssert:            true,
+			onRetArgsOvnNBClient: &onCallReturnArgs{"LSPGet", []string{"string"}, []interface{}{nil, goovn.ErrorOption}},
 		},
 		{
-			desc:                    "test code path where IP address parsing fails",
-			input:                   "TEST_PORT",
-			errExpected:             false,
-			onRetArgsExecUtilsIface: &onCallReturnArgs{"RunCmd", []string{"*mocks.Cmd", "string", "[]string", "string", "string", "string", "string", "string", "string", "string"}, []interface{}{bytes.NewBuffer([]byte("22:e9:ac:f4:00:04 10.244.0.\n[dynamic]")), nil, nil}},
-			onRetArgsKexecIface:     &onCallReturnArgs{"Command", []string{"string", "string", "string", "string", "string", "string", "string", "string"}, []interface{}{mockCmd}},
+			desc:                 "test path where LSPGet() returns nil",
+			inpPort:              "TEST_PORT",
+			onRetArgsOvnNBClient: &onCallReturnArgs{"LSPGet", []string{"string"}, []interface{}{nil, nil}},
 		},
 		{
-			desc:                    "test code path where addresses list count is less than 2",
-			input:                   "TEST_PORT",
-			errExpected:             false,
-			onRetArgsExecUtilsIface: &onCallReturnArgs{"RunCmd", []string{"*mocks.Cmd", "string", "[]string", "string", "string", "string", "string", "string", "string", "string"}, []interface{}{nil, nil, nil}},
-			onRetArgsKexecIface:     &onCallReturnArgs{"Command", []string{"string", "string", "string", "string", "string", "string", "string", "string"}, []interface{}{mockCmd}},
+			desc:                 "test path where lsp.DynamicAddresses is a zero length string and len(addresses)==0",
+			inpPort:              "TEST_PORT",
+			onRetArgsOvnNBClient: &onCallReturnArgs{"LSPGet", []string{"string"}, []interface{}{&goovn.LogicalSwitchPort{DynamicAddresses: ""}, nil}},
 		},
-		// {
-		// 	desc:                    "test code path when RunOVNNbctl returns only static address",
-		// 	input:                   "TEST_PORT",
-		// 	onRetArgsExecUtilsIface: &onCallReturnArgs{"RunCmd", []string{"*mocks.Cmd", "string", "[]string", "string", "string", "string", "string", "string", "string", "string"}, []interface{}{bytes.NewBuffer([]byte("[]\n[\"06:c6:d4:fb:fb:ba 10.244.2.2\"]")), nil, nil}},
-		// 	onRetArgsKexecIface:     &onCallReturnArgs{"Command", []string{"string", "string", "string", "string", "string", "string", "string", "string"}, []interface{}{mockCmd}},
-		// },
-		// {
-		// 	desc:                    "test the code path when RunOVNNbctl returns `[]\\n[dynamic]`",
-		// 	input:                   "TEST_PORT",
-		// 	onRetArgsExecUtilsIface: &onCallReturnArgs{"RunCmd", []string{"*mocks.Cmd", "string", "[]string", "string", "string", "string", "string", "string", "string", "string"}, []interface{}{bytes.NewBuffer([]byte("[]\n[dynamic]")), nil, nil}},
-		// 	onRetArgsKexecIface:     &onCallReturnArgs{"Command", []string{"string", "string", "string", "string", "string", "string", "string", "string"}, []interface{}{mockCmd}},
-		// },
 		{
-			desc:                    "test the code path where ParseMAC fails",
-			input:                   "TEST_PORT",
-			errExpected:             false,
-			onRetArgsExecUtilsIface: &onCallReturnArgs{"RunCmd", []string{"*mocks.Cmd", "string", "[]string", "string", "string", "string", "string", "string", "string", "string"}, []interface{}{bytes.NewBuffer([]byte("\"22:e9:ac::00:04 10.244.0.3\\n[dynamic]\"")), nil, nil}},
-			onRetArgsKexecIface:     &onCallReturnArgs{"Command", []string{"string", "string", "string", "string", "string", "string", "string", "string"}, []interface{}{mockCmd}},
+			desc:                 "test path where lsp.DynamicAddresses is non-zero length string and value of first address in addresses list is set to dynamic",
+			inpPort:              "TEST_PORT",
+			onRetArgsOvnNBClient: &onCallReturnArgs{"LSPGet", []string{"string"}, []interface{}{&goovn.LogicalSwitchPort{DynamicAddresses: "06:c6:d4:fb:fb:ba 10.244.2.2"}, nil}},
+		},
+		{
+			desc:                 "test code path where addresses list count is less than 2",
+			inpPort:              "TEST_PORT",
+			errMatch:             fmt.Errorf("error while obtaining addresses for"),
+			onRetArgsOvnNBClient: &onCallReturnArgs{"LSPGet", []string{"string"}, []interface{}{&goovn.LogicalSwitchPort{DynamicAddresses: "06:c6:d4:fb:fb:ba"}, nil}},
+		},
+		{
+			desc:                 "test the code path where ParseMAC fails",
+			inpPort:              "TEST_PORT",
+			errMatch:             fmt.Errorf("failed to parse logical switch port"),
+			onRetArgsOvnNBClient: &onCallReturnArgs{"LSPGet", []string{"string"}, []interface{}{&goovn.LogicalSwitchPort{Addresses: []string{"192.168.1.3", "0a:00:00:00:00:01"}}, nil}},
+		},
+		{
+			desc:                 "test code path where IP address parsing fails",
+			inpPort:              "TEST_PORT",
+			errMatch:             fmt.Errorf("failed to parse logical switch port"),
+			onRetArgsOvnNBClient: &onCallReturnArgs{"LSPGet", []string{"string"}, []interface{}{&goovn.LogicalSwitchPort{Addresses: []string{"192.168.1.3", "0a:00:00:00:00:01"}}, nil}},
+		},
+		{
+			desc:                 "test success path where MAC, IPs are returned",
+			inpPort:              "TEST_PORT",
+			onRetArgsOvnNBClient: &onCallReturnArgs{"LSPGet", []string{"string"}, []interface{}{&goovn.LogicalSwitchPort{Addresses: []string{"0a:00:00:00:00:01", "192.168.1.3"}}, nil}},
 		},
 	}
 	for i, tc := range tests {
 		t.Run(fmt.Sprintf("%d:%s", i, tc.desc), func(t *testing.T) {
-			call := mockExecRunner.On(tc.onRetArgsExecUtilsIface.onCallMethodName)
-			for _, arg := range tc.onRetArgsExecUtilsIface.onCallMethodArgType {
+			call := mockOvnNBClient.On(tc.onRetArgsOvnNBClient.onCallMethodName)
+			for _, arg := range tc.onRetArgsOvnNBClient.onCallMethodArgType {
 				call.Arguments = append(call.Arguments, mock.AnythingOfType(arg))
 			}
-			for _, ret := range tc.onRetArgsExecUtilsIface.retArgList {
+			for _, ret := range tc.onRetArgsOvnNBClient.retArgList {
 				call.ReturnArguments = append(call.ReturnArguments, ret)
 			}
 			call.Once()
-			ifaceCall := mockKexecIface.On(tc.onRetArgsKexecIface.onCallMethodName)
-			for _, arg := range tc.onRetArgsKexecIface.onCallMethodArgType {
-				ifaceCall.Arguments = append(ifaceCall.Arguments, mock.AnythingOfType(arg))
-			}
-			for _, ret := range tc.onRetArgsKexecIface.retArgList {
-				ifaceCall.ReturnArguments = append(ifaceCall.ReturnArguments, ret)
-			}
-			ifaceCall.Once()
 
-			hwAddr, ipList, err := GetPortAddresses(tc.input, mockOVNNBClient)
+			hwAddr, ipList, err := GetPortAddresses(tc.inpPort, mockOvnNBClient)
 			t.Log(hwAddr.String(), ipList, err)
-			if tc.errExpected {
+			if tc.errAssert {
 				assert.Error(t, err)
+			} else if tc.errMatch != nil {
+				assert.Contains(t, err.Error(), tc.errMatch.Error())
 			}
 		})
 	}
