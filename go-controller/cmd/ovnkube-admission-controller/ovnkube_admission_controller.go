9e9fe3b837f7baef3cb087d930d734d412e3eaf8
Alexander Constantinescu <aconstan@redhat.com>
2020-08-20 15:34:30 +0200

    Admission control webhook implementation
    Signed-off-by: Alexander Constantinescu <aconstan@redhat.com>

---
+++ go-controller/cmd/ovnkube-admission-controller/ovnkube_admission_controller.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+
+	v1 "k8s.io/api/admission/v1"
+)
+
+const (
+	tlsDir      = `/run/secrets/tls`
+	tlsCertFile = `tls.crt`
+	tlsKeyFile  = `tls.key`
+)
+
+func dummyValidate(req *v1.AdmissionRequest) error {
+	return nil
+}
+
+func main() {
+	certPath := filepath.Join(tlsDir, tlsCertFile)
+	keyPath := filepath.Join(tlsDir, tlsKeyFile)
+
+	mux := http.NewServeMux()
+	mux.Handle("/", admitFuncHandler(dummyValidate))
+	server := &http.Server{
+		Addr:    ":8443",
+		Handler: mux,
+	}
+	log.Fatal(server.ListenAndServeTLS(certPath, keyPath))
+}
