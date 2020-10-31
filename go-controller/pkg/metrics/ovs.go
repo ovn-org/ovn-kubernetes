189c0725ec552f43d690000fce060cc70bdf0e75
Girish Moodalbail <gmoodalbail@nvidia.com>
2020-10-30 21:45:48 -0700

    metrics: fix typo in OVS metric name
    Authored-by: Dmitry Yusupov <dyusupov@nvidia.com>
    Signed-off-by: Girish Moodalbail <gmoodalbail@nvidia.com>

--- go-controller/pkg/metrics/ovs.go
+++ go-controller/pkg/metrics/ovs.go
@@ -226,7 +226,7 @@ var metricOvsTcPolicy = prometheus.NewGauge(prometheus.GaugeOpts{
 var metricInterafceDriverName = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 	Namespace: MetricOvsNamespace,
 	Subsystem: MetricOvsSubsystemVswitchd,
-	Name:      "interafce_driver_name",
+	Name:      "interface_driver_name",
 	Help: "A metric with a constant '1' value labeled by driver name that " +
 		"specifies the name of the device driver controlling the network interface"},
 	[]string{
