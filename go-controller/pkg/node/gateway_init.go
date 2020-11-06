15b663ffef9c7c908bd1050d1c87ddf4a98b6782
Thomas F Herbert <therbert@redhat.com>
2020-11-06 14:29:52 -0500

    Fix build error after merge.
    Signed-off-by: Thomas F Herbert <therbert@redhat.com>

--- go-controller/pkg/node/gateway_init.go
+++ go-controller/pkg/node/gateway_init.go
@@ -7,14 +7,12 @@ import (
 
 	kapi "k8s.io/api/core/v1"
 	"k8s.io/client-go/tools/cache"
-	"k8s.io/klog"
+	"k8s.io/klog/v2"
 	utilnet "k8s.io/utils/net"
 
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/kube"
 	util "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
-	"k8s.io/klog/v2"
-	utilnet "k8s.io/utils/net"
 )
 
 // bridgedGatewayNodeSetup makes the bridge's MAC address permanent (if needed), sets up
