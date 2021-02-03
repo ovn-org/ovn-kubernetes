8136a5e2bd745abe74d357793c271f2837fe5db5
Adrian Chiris <adrianc@nvidia.com>
2021-02-03 15:03:58 +0000

    Move sriovnet to its own file under util
    Similarly to netlink library wrapper, move sriovnet
    under util package.
    
    Generate mocks and adjust unit-tests
    
    Signed-off-by: Adrian Chiris <adrianc@nvidia.com>

--- go-controller/pkg/cni/helper_linux_test.go
+++ go-controller/pkg/cni/helper_linux_test.go
@@ -425,15 +425,15 @@ func TestSetupInterface(t *testing.T) {
 func TestSetupSriovInterface(t *testing.T) {
 	mockNetLinkOps := new(util_mocks.NetLinkOps)
 	mockCNIPlugin := new(mocks.CNIPluginLibOps)
-	mockSriovNetLibOps := new(mocks.SriovNetLibOps)
+	mockSriovnetOps := new(util_mocks.SriovnetOps)
 	mockNS := new(cni_ns_mocks.NetNS)
 	mockLink := new(netlink_mocks.Link)
 	// below sets the `netLinkOps` in util/net_linux.go to a mock instance for purpose of unit tests execution
 	util.SetNetLinkOpMockInst(mockNetLinkOps)
 	// `cniPluginLibOps` is defined in helper_linux.go
 	cniPluginLibOps = mockCNIPlugin
-	// `sriovLibOps` is defined in helper_linux.go
-	sriovLibOps = mockSriovNetLibOps
+	// set `sriovnetOps` in util/sriovnet_linux.go to a mock instance for unit tests execution
+	util.SetSriovnetOpsInst(mockSriovnetOps)
 
 	res, err := sriovnet.GetUplinkRepresentor("0000:01:00.0")
 	t.Log(res, err)
@@ -705,7 +705,7 @@ func TestSetupSriovInterface(t *testing.T) {
 			ovntest.ProcessMockFnList(&mockNetLinkOps.Mock, tc.netLinkOpsMockHelper)
 			ovntest.ProcessMockFnList(&mockCNIPlugin.Mock, tc.cniPluginMockHelper)
 			ovntest.ProcessMockFnList(&mockNS.Mock, tc.nsMockHelper)
-			ovntest.ProcessMockFnList(&mockSriovNetLibOps.Mock, tc.sriovOpsMockHelper)
+			ovntest.ProcessMockFnList(&mockSriovnetOps.Mock, tc.sriovOpsMockHelper)
 			ovntest.ProcessMockFnList(&mockLink.Mock, tc.linkMockHelper)
 
 			hostIface, contIface, err := setupSriovInterface(tc.inpNetNS, tc.inpContID, tc.inpIfaceName, tc.inpPodIfaceInfo, tc.inpPCIAddrs)
@@ -720,7 +720,7 @@ func TestSetupSriovInterface(t *testing.T) {
 			mockNetLinkOps.AssertExpectations(t)
 			mockCNIPlugin.AssertExpectations(t)
 			mockNS.AssertExpectations(t)
-			mockSriovNetLibOps.AssertExpectations(t)
+			mockSriovnetOps.AssertExpectations(t)
 			mockLink.AssertExpectations(t)
 		})
 	}
