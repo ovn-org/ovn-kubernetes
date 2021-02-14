af0e243c64952b3fcaf5ef07c9dc3bc68ccbaaf7
Adrian Chiris <adrianc@nvidia.com>
2021-02-14 15:38:49 +0000

    Add utility function: RunPeriodicallyUntilStop
    Executes a callable in a periodic manner to allow code reuse
    by ovnkube node to call healthcheck and monitoring methods:
     - node.checkForStaleOVSInterfaces()
     - ManagementPort.CheckManagementPortHealth()
    
    Signed-off-by: Adrian Chiris <adrianc@nvidia.com>

--- go-controller/pkg/node/node.go
+++ go-controller/pkg/node/node.go
@@ -465,10 +465,15 @@ func (n *OvnNode) Start(wg *sync.WaitGroup) error {
 
 	if config.OvnKubeNode.Mode != types.NodeModeSmartNICHost {
 		// start health check to ensure there are no stale OVS internal ports
-		go checkForStaleOVSInterfaces(n.stopChan, n.name, n.watchFactory.(*factory.WatchFactory))
+		go util.RunPeriodicallyUntilStop(func() {
+			checkForStaleOVSInterfaces(n.name, n.watchFactory.(*factory.WatchFactory))
+		}, time.Minute, n.stopChan)
 
 		// start management port health check
-		go checkManagementPortHealth(mgmtPortConfig, n.stopChan)
+		go util.RunPeriodicallyUntilStop(func() {
+			checkManagementPortHealth(mgmtPortConfig)
+		}, 30*time.Second, n.stopChan)
+
 		n.WatchEndpoints()
 	}
 
