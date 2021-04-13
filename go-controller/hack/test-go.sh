ed9ce1881dafafabc6976db00dfe6015f180557e
Casey Callendrello <cdc@redhat.com>
2021-04-13 16:44:59 +0200

    DNM coveralls hacking
    Signed-off-by: Casey Callendrello <cdc@redhat.com>

--- go-controller/hack/test-go.sh
+++ go-controller/hack/test-go.sh
@@ -8,48 +8,49 @@ setup_env
 
 cd "${OVN_KUBE_ROOT}"
 
-PKGS=$(go list -mod vendor -f '{{if len .TestGoFiles}} {{.ImportPath}} {{end}}' ${PKGS:-./cmd/... ./pkg/... ./hybrid-overlay/...} | xargs)
-
 if [[ "$1" == "focus" && "$2" != "" ]]; then
     gingko_focus="-ginkgo.focus="${2}""
+    shift; shift
 fi
+
+PKGS="$(go list -mod vendor -f '{{if len .TestGoFiles}} {{.ImportPath}} {{end}}' ${PKGS:-./cmd/... ./pkg/... ./hybrid-overlay/...} | xargs)"
 
 TEST_REPORT_DIR=${TEST_REPORT_DIR:="./_artifacts"}
 function testrun {
     local idx="${1}"
     local pkg="${2}"
     local go_test="go test -mod=vendor"
-    local otherargs="${@:3} "
     local args=""
     local ginkgoargs=
+    echo pkg=$pkg
     # enable go race detector
     # FIXME race detector fails with hybrid-overlay tests
-    if [[ ! -z "${RACE:-}" && "${pkg}" != "github.com/ovn-org/ovn-kubernetes/go-controller/hybrid-overlay/pkg/controller" ]]; then
+    if [[ -n "${RACE}" && "${pkg}" != "github.com/ovn-org/ovn-kubernetes/go-controller/hybrid-overlay/pkg/controller" ]]; then
         args="-race "
     fi
     if [[ "$USER" != root && " ${root_pkgs[@]} " =~ " $pkg " && -z "${DOCKER_TEST:-}" ]]; then
         testfile=$(mktemp --tmpdir ovn-test.XXXXXXXX)
         echo "sudo required for ${pkg}, compiling test to ${testfile}"
         if [[ ! -z "${RACE:-}" && "${pkg}" != "github.com/ovn-org/ovn-kubernetes/go-controller/hybrid-overlay/pkg/controller" ]]; then
-            go test -mod=vendor -race -covermode atomic -c "${pkg}" -o "${testfile}"
+            go test -mod=vendor -race  -c "${pkg}" -o "${testfile}"
         else
-            go test -mod=vendor -covermode set -c "${pkg}" -o "${testfile}"
+            go test -mod=vendor -c "${pkg}" -o "${testfile}"
         fi
         args=""
         go_test="sudo ${testfile}"
     fi
+
     # set gomaxprocs to 1 in CI, because actions are run in containers and we don't know what cpu
     # limits are being imposed
     if [ -n "${CI}" ]; then
         args="${args}-test.cpu 1 "
     fi
     if [[ -n "$gingko_focus" ]]; then
-        local ginkgoargs=${ginkgo_focus:-}
+        ginkgoargs=${ginkgo_focus:-}
     fi
     local path=${pkg#github.com/ovn-org/ovn-kubernetes/go-controller}
-    # coverage is incompatible with the race detector
     if [ ! -z "${COVERALLS:-}" ]; then
-        args="-test.coverprofile=${idx}.coverprofile "
+        args="$args -test.coverprofile=${idx}.coverprofile "
     fi
     if grep -q -r "ginkgo" ."${path}"; then
 	    prefix=$(echo "${path}" | cut -c 2- | sed 's,/,_,g')
@@ -58,12 +59,11 @@ function testrun {
             ginkgoargs="-ginkgo.v ${gingko_focus} -ginkgo.reportFile ${TEST_REPORT_DIR}/junit-${prefix}.xml"
         fi
     fi
-    args="${args}${otherargs}"
     if [ "$go_test" == "go test -mod=vendor" ]; then
-        args=${args}${pkg}
+        args="${args}${pkg}"
     fi
-    echo "${go_test} -test.v ${args} ${ginkgoargs}"
-    ${go_test} -test.v ${args} ${ginkgoargs} 2>&1
+    echo "${go_test}  ${args} ${ginkgoargs}"
+    ${go_test}  ${args} ${ginkgoargs} 2>&1
 }
 
 # These packages requires root for network namespace maniuplation in unit tests
