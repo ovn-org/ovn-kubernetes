68742496672726ad7030f5d9193f48e1e3e1c42a
Federico Paolinelli <fpaoline@redhat.com>
2021-07-14 17:16:09 +0200

    E2E control plane: un-skip ipv6 ex gw tests
    With the new ovn version they are supposed to pass
    
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- test/scripts/e2e-cp.sh
+++ test/scripts/e2e-cp.sh
@@ -12,15 +12,9 @@ should provide connection to external host by DNS name from a pod|\
 should provide Internet connection continuously when master is killed|\
 should provide Internet connection continuously when ovn-k8s pod is killed|\
 Should validate connectivity from a pod to a non-node host address on same node|\
-Should validate connectivity to an external gateway\'s loopback address via a pod with external gateway annotations enabled|\
-Should validate connectivity to multiple external gateways for an ECMP scenario|\
 Should validate connectivity without vxlan before and after updating the namespace annotation to a new external gateway|\
-Should validate ICMP connectivity to an external gateway\'s loopback address via a pod with external gateway annotations enabled|\
-Should validate ICMP connectivity to multiple external gateways for an ECMP scenario|\
 Should validate ingress connectivity from an external gateway|\
 Should validate NetFlow data of br-int is sent to an external gateway|\
-Should validate TCP/UDP connectivity to an external gateway\'s loopback address via a pod with external gateway annotations enabled|\
-Should validate TCP/UDP connectivity to multiple external gateways for a UDP / TCP scenario|\
 Should validate the egress firewall policy functionality against remote hosts|\
 Should validate the egress IP functionality against remote hosts|\
 recovering from deleting db files while maintain connectivity"
