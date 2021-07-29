a0cd15464dd4b74a6dd28e894dc9bc589ec5f6c5
Ori Braunshtein <obraunsh@redhat.com>
2021-07-29 16:27:55 +0300

    Output address resolution requests to LOCAL port
    Currently address resolution requests (ARP/Neighbor solicitation) for
    LoadBalancer/External IPs are answered by all of the gateway routers in the cluster.
    By forwarding these requests to the local port, a network load balancer implementation
    like MetalLB is able to be the only one replying to them - thus enabling it
    to be the only one announcing a specific LoadBalancer service IP.
    
    Signed-off-by: Ori Braunshtein <obraunsh@redhat.com>

--- go-controller/pkg/node/gateway_shared_intf.go
+++ go-controller/pkg/node/gateway_shared_intf.go
@@ -159,10 +159,14 @@ func (npw *nodePortWatcher) updateServiceFlowCache(service *kapi.Service, add bo
 			flowProtocol := protocol
 			nwDst := "nw_dst"
 			nwSrc := "nw_src"
+			addrResDst := nwDst
+			addrResProto := "arp"
 			if utilnet.IsIPv6String(ing.IP) {
 				flowProtocol = protocol + "6"
 				nwDst = "ipv6_dst"
 				nwSrc = "ipv6_src"
+				addrResDst = "nd_target"
+				addrResProto = "icmp6, icmp_type=135, icmp_code=0"
 			}
 			key = strings.Join([]string{"Ingress", service.Namespace, service.Name, ingIP.String(), fmt.Sprintf("%d", svcPort.Port)}, "_")
 			if !add {
@@ -174,7 +178,10 @@ func (npw *nodePortWatcher) updateServiceFlowCache(service *kapi.Service, add bo
 						cookie, npw.ofportPhys, flowProtocol, nwDst, ing.IP, svcPort.Port, actions),
 					fmt.Sprintf("cookie=%s, priority=110, in_port=%s, %s, %s=%s, tp_src=%d, "+
 						"actions=output:%s",
-						cookie, npw.ofportPatch, flowProtocol, nwSrc, ing.IP, svcPort.Port, npw.ofportPhys)})
+						cookie, npw.ofportPatch, flowProtocol, nwSrc, ing.IP, svcPort.Port, npw.ofportPhys),
+					fmt.Sprintf("cookie=%s, priority=110, in_port=%s, %s, %s=%s, "+
+						"actions=output:%s",
+						cookie, npw.ofportPhys, addrResProto, addrResDst, ing.IP, ovsLocalPort)})
 			}
 		}
 
@@ -182,10 +189,14 @@ func (npw *nodePortWatcher) updateServiceFlowCache(service *kapi.Service, add bo
 			flowProtocol := protocol
 			nwDst := "nw_dst"
 			nwSrc := "nw_src"
+			addrResDst := nwDst
+			addrResProto := "arp"
 			if utilnet.IsIPv6String(externalIP) {
 				flowProtocol = protocol + "6"
 				nwDst = "ipv6_dst"
 				nwSrc = "ipv6_src"
+				addrResDst = "nd_target"
+				addrResProto = "icmp6, icmp_type=135, icmp_code=0"
 			}
 			cookie, err = svcToCookie(service.Namespace, service.Name, externalIP, svcPort.Port)
 			if err != nil {
@@ -203,7 +214,10 @@ func (npw *nodePortWatcher) updateServiceFlowCache(service *kapi.Service, add bo
 						cookie, npw.ofportPhys, flowProtocol, nwDst, externalIP, svcPort.Port, actions),
 					fmt.Sprintf("cookie=%s, priority=110, in_port=%s, %s, %s=%s, tp_src=%d, "+
 						"actions=output:%s",
-						cookie, npw.ofportPatch, flowProtocol, nwSrc, externalIP, svcPort.Port, npw.ofportPhys)})
+						cookie, npw.ofportPatch, flowProtocol, nwSrc, externalIP, svcPort.Port, npw.ofportPhys),
+					fmt.Sprintf("cookie=%s, priority=110, in_port=%s, %s, %s=%s, "+
+						"actions=output:%s",
+						cookie, npw.ofportPhys, addrResProto, addrResDst, externalIP, ovsLocalPort)})
 			}
 		}
 	}
