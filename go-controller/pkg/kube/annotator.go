ef706912fd31d970807d7b9a25159c03ee8a9b1b
Patryk Diak <pdiak@redhat.com>
2021-09-23 12:42:40 +0200

    Remove namespace(*kapi.Namespace) dependency from namespace annotator
    Namespace annotator used the namespace object to compare its annotations with the changes provided.
    Because the object isn't synchronized with the API server, the comparison used potentially outdated values.
    
    This change removes the logic of comparing existing annotations and only caches provided changes to send them with Run().
    
    Signed-off-by: Patryk Diak <pdiak@redhat.com>

--- go-controller/pkg/kube/annotator.go
+++ go-controller/pkg/kube/annotator.go
@@ -5,8 +5,6 @@ import (
 	"fmt"
 	"reflect"
 	"sync"
-
-	kapi "k8s.io/api/core/v1"
 )
 
 // Annotator represents the exported methods for handling node annotations
@@ -15,12 +13,6 @@ type Annotator interface {
 	Set(key string, value interface{}) error
 	Delete(key string)
 	Run() error
-}
-
-type action struct {
-	key     string
-	val     string
-	origVal interface{}
 }
 
 type nodeAnnotator struct {
@@ -141,71 +133,58 @@ func (pa *podAnnotator) Run() error {
 }
 
 // NewNamespaceAnnotator returns a new annotator for Namespace objects
-func NewNamespaceAnnotator(kube Interface, namespace *kapi.Namespace) Annotator {
+func NewNamespaceAnnotator(kube Interface, namespaceName string) Annotator {
 	return &namespaceAnnotator{
-		kube:      kube,
-		namespace: namespace,
-		changes:   make(map[string]*action),
+		kube:          kube,
+		namespaceName: namespaceName,
+		changes:       make(map[string]interface{}),
 	}
 }
 
 type namespaceAnnotator struct {
-	kube      Interface
-	namespace *kapi.Namespace
+	kube          Interface
+	namespaceName string
 
-	changes map[string]*action
+	changes map[string]interface{}
 	sync.Mutex
 }
 
 func (na *namespaceAnnotator) Set(key string, val interface{}) error {
-	act := &action{
-		key:     key,
-		origVal: val,
-	}
-	if val != nil {
-		// Annotations must be either a valid string value or nil; coerce
-		// any non-empty values to string
-		if reflect.TypeOf(val).Kind() == reflect.String {
-			act.val = val.(string)
-		} else {
-			bytes, err := json.Marshal(val)
-			if err != nil {
-				return fmt.Errorf("failed to marshal %q value %v to string: %v", key, val, err)
-			}
-			act.val = string(bytes)
-		}
-	}
 	na.Lock()
 	defer na.Unlock()
-	na.changes[key] = act
+
+	if val == nil {
+		na.changes[key] = nil
+		return nil
+	}
+
+	// Annotations must be either a valid string value or nil; coerce
+	// any non-empty values to string
+	if reflect.TypeOf(val).Kind() == reflect.String {
+		na.changes[key] = val.(string)
+	} else {
+		bytes, err := json.Marshal(val)
+		if err != nil {
+			return fmt.Errorf("failed to marshal %q value %v to string: %v", key, val, err)
+		}
+		na.changes[key] = string(bytes)
+	}
+
 	return nil
 }
 
 func (na *namespaceAnnotator) Delete(key string) {
 	na.Lock()
 	defer na.Unlock()
-	na.changes[key] = &action{key: key}
+	na.changes[key] = nil
 }
 
 func (na *namespaceAnnotator) Run() error {
-	annotations := make(map[string]interface{})
 	na.Lock()
 	defer na.Unlock()
-	for k, act := range na.changes {
-		// Ignore annotations that already exist with the same value
-		if existing, ok := na.namespace.Annotations[k]; existing != act.val || !ok {
-			if act.origVal != nil {
-				// Annotation should be updated to new value
-				annotations[k] = act.val
-			} else {
-				// Annotation should be deleted
-				annotations[k] = ""
-			}
-		}
-	}
-	if len(annotations) == 0 {
+	if len(na.changes) == 0 {
 		return nil
 	}
 
-	return na.kube.SetAnnotationsOnNamespace(na.namespace.Name, annotations)
+	return na.kube.SetAnnotationsOnNamespace(na.namespaceName, na.changes)
 }
