ovn-org/ovn-kubernetes

commit 35dd1ae238ce7cbfce0ba17b133325a3fd5b7063

CARRY: run all pod deletion OVN commands as a batch

go-controller/pkg/ovn/pods.go
@@ -123,15 +123,26 @@ func (oc *Controller) deleteLogicalPort(pod *kapi.Pod) {
 
 	// FIXME: if any of these steps fails we need to stop and try again later...
 
-	if err := oc.deletePodFromNamespace(pod.Namespace, portInfo.name, portInfo.uuid, portInfo.ips); err != nil {
-		klog.Errorf(err.Error())
-	}
-
-	start1 := time.Now()
-	err = util.OvnNBLSPDel(oc.ovnNBClient, logicalPort)
-	ovnExecuteTime = time.Since(start1)
+	var cmds []*goovn.OvnCommand
+	addrSetCmds, err := oc.deletePodFromNamespace(pod.Namespace, portInfo.name, portInfo.uuid, portInfo.ips)
 	if err != nil {
 		klog.Errorf(err.Error())
+	} else {
+		cmds = append(cmds, addrSetCmds...)
+	}
+
+	cmd, err := oc.ovnNBClient.LSPDel(logicalPort)
+	if err != nil {
+		klog.Errorf(err.Error())
+	} else {
+		cmds = append(cmds, cmd)
+	}
+	start1 := time.Now()
+	// execute all the commands together.
+	err = oc.ovnNBClient.Execute(cmds...)
+	ovnExecuteTime = time.Since(start1)
+	if err != nil {
+		klog.Errorf("Error deleting logical port %s: %v", portInfo.name, err)
 	}
 
 	if err := oc.lsManager.ReleaseIPs(portInfo.logicalSwitch, portInfo.ips); err != nil {
@@ -352,8 +363,13 @@ func (oc *Controller) addLogicalPort(pod *kapi.Pod) (err error) {
 			} else {
 				klog.Infof("Released IPs: %s for node: %s", util.JoinIPNetIPs(podIfAddrs, " "), logicalSwitch)
 			}
-			if nsErr := oc.deletePodFromNamespace(pod.Namespace, portName, "", podIfAddrs); nsErr != nil {
+			if addrSetCmds, nsErr := oc.deletePodFromNamespace(pod.Namespace, portName, "", podIfAddrs); nsErr != nil {
 				klog.Errorf("Error when deleting pod: %s from namespace: %v", pod.Name, err)
+			} else {
+				err = oc.ovnNBClient.Execute(addrSetCmds...)
+				if err != nil {
+					klog.Errorf("Error removing pod %s IPs from namespace address set: %v", portName, err)
+				}
 			}
 		}
 	}()

go-controller/pkg/ovn/address_set/address_set.go
@@ -56,6 +56,7 @@ type AddressSet interface {
 	DeleteIPs(ip []net.IP) error
 	Destroy() error
 	PrepareAddIPsCmds(ip []net.IP) ([]*goovn.OvnCommand, error)
+	PrepareDeleteIPsCmds(ip []net.IP) ([]*goovn.OvnCommand, error)
 }
 
 type ovnAddressSetFactory struct {
@@ -374,6 +375,31 @@ func (as *ovnAddressSets) DeleteIPs(ips []net.IP) error {
 	return nil
 }
 
+func (as *ovnAddressSets) PrepareDeleteIPsCmds(ips []net.IP) ([]*goovn.OvnCommand, error) {
+	if len(ips) == 0 {
+		return nil, nil
+	}
+
+	v4ips, v6ips := splitIPsByFamily(ips)
+	var cmds []*goovn.OvnCommand
+	if as.ipv6 != nil {
+		if cmd, err := as.ipv6.deleteIPsCmd(v6ips); err != nil {
+			return nil, fmt.Errorf("failed to DeleteIPs to the v6 set: %w", err)
+		} else {
+			cmds = append(cmds, cmd)
+		}
+	}
+	if as.ipv4 != nil {
+		if cmd, err := as.ipv4.deleteIPsCmd(v4ips); err != nil {
+			return nil, fmt.Errorf("failed to DeleteIPs to the v4 set: %w", err)
+		} else {
+			cmds = append(cmds, cmd)
+		}
+	}
+
+	return cmds, nil
+}
+
 func (as *ovnAddressSets) Destroy() error {
 
 	if as.ipv4 != nil {
@@ -461,7 +487,7 @@ func (as *ovnAddressSet) deleteIPsCmd(ips []net.IP) (*goovn.OvnCommand, error) {
 	}
 	ipStr := joinIPs(ips)
 
-	cmd, err := as.nb.ASDelIPs(as.hashName, uniqIPs)
+	cmd, err := as.nb.ASDelIPs(as.hashName, as.uuid, uniqIPs)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create add ips cmd for address set %q, ips: %s: %v", asDetail(as), ipStr, err)
 	}

go-controller/pkg/ovn/address_set/fake_address_set.go
@@ -263,6 +263,10 @@ func (as *fakeAddressSets) SetIPs(ips []net.IP) error {
 	return nil
 }
 
+func (as *fakeAddressSets) PrepareDeleteIPsCmds(ips []net.IP) ([]*goovn.OvnCommand, error) {
+	return nil, nil
+}
+
 func (as *fakeAddressSets) DeleteIPs(ips []net.IP) error {
 	var err error
 	as.Lock()

go-controller/pkg/ovn/namespace.go
@@ -95,27 +95,30 @@ func (oc *Controller) addPodToNamespace(ns string, ips []*net.IPNet) (*gatewayIn
 	return oc.getRoutingExternalGWs(nsInfo), oc.getRoutingPodGWs(nsInfo), nsInfo.hybridOverlayExternalGW, cmds, nil
 }
 
-func (oc *Controller) deletePodFromNamespace(ns, name, uuid string, ips []*net.IPNet) error {
+func (oc *Controller) deletePodFromNamespace(ns, name, uuid string, ips []*net.IPNet) ([]*goovn.OvnCommand, error) {
 	nsInfo, nsUnlock := oc.getNamespaceLocked(ns, true)
 	if nsInfo == nil {
-		return nil
+		return nil, nil
 	}
 	defer nsUnlock()
 
+	var cmds []*goovn.OvnCommand
+	var err error
 	if nsInfo.addressSet != nil && len(ips) > 0 {
-		if err := nsInfo.addressSet.DeleteIPs(createIPAddressSlice(ips)); err != nil {
-			return err
+		cmds, err = nsInfo.addressSet.PrepareDeleteIPsCmds(createIPAddressSlice(ips))
+		if err != nil {
+			return nil, err
 		}
 	}
 
 	// Remove the port from the multicast allow policy.
 	if oc.multicastSupport && nsInfo.multicastEnabled && uuid != "" {
 		if err := podDeleteAllowMulticastPolicy(oc.ovnNBClient, ns, name, uuid); err != nil {
-			return err
+			return nil, err
 		}
 	}
 
-	return nil
+	return cmds, nil
 }
 
 func createIPAddressSlice(ips []*net.IPNet) []net.IP {

go-controller/vendor/github.com/ebay/go-ovn/address_set.go
@@ -78,13 +78,16 @@ func (odbi *ovndb) asAddIPImp(name, uuid string, addrs []string) (*OvnCommand, e
 	return &OvnCommand{operations, odbi, make([][]map[string]interface{}, len(operations))}, nil
 }
 
-func (odbi *ovndb) asDelIPImp(name string, addrs []string) (*OvnCommand, error) {
+func (odbi *ovndb) asDelIPImp(name, uuid string, addrs []string) (*OvnCommand, error) {
 	addresses, err := libovsdb.NewOvsSet(addrs)
 	if err != nil {
 		return nil, err
 	}
 	mutation := libovsdb.NewMutation("addresses", "delete", addresses)
 	condition := libovsdb.NewCondition("name", "==", name)
+	if uuid != "" {
+		condition = libovsdb.NewCondition("_uuid", "==", stringToGoUUID(uuid))
+	}
 	updateOp := libovsdb.Operation{
 		Op:    opMutate,
 		Table: TableAddressSet,

go-controller/vendor/github.com/ebay/go-ovn/client.go
@@ -103,7 +103,7 @@ type Client interface {
 	// Add addressset
 	ASAdd(name string, addrs []string, external_ids map[string]string) (*OvnCommand, error)
 	ASAddIPs(name, uuid string, addrs []string) (*OvnCommand, error)
-	ASDelIPs(name string, addrs []string) (*OvnCommand, error)
+	ASDelIPs(name, uuid string, addrs []string) (*OvnCommand, error)
 	// Delete addressset
 	ASDel(name string) (*OvnCommand, error)
 	// Get all AS
@@ -878,8 +878,8 @@ func (c *ovndb) ASAddIPs(name, uuid string, addrs []string) (*OvnCommand, error)
 	return c.asAddIPImp(name, uuid, addrs)
 }
 
-func (c *ovndb) ASDelIPs(name string, addrs []string) (*OvnCommand, error) {
-	return c. asDelIPImp(name, addrs)
+func (c *ovndb) ASDelIPs(name, uuid string, addrs []string) (*OvnCommand, error) {
+	return c.asDelIPImp(name, uuid, addrs)
 }
 
 func (c *ovndb) ASDel(name string) (*OvnCommand, error) {

commit 0505367538f6610bff4f821a11ef84f3b0bf3e9e

CARRY: pods: fix overwriting returned error from defer()
Using the same error variable name in the defer() overwrote
the error itself, and caused the function to return nil even
though an error had occurred.

Fixes: 35dd1ae238 CARRY: run all pod deletion OVN commands as a batch

go-controller/pkg/ovn/pods.go
@@ -372,9 +372,8 @@ func (oc *Controller) addLogicalPort(pod *kapi.Pod) (err error) {
 			if addrSetCmds, nsErr := oc.deletePodFromNamespace(pod.Namespace, portName, "", podIfAddrs); nsErr != nil {
 				klog.Errorf("Error when deleting pod: %s from namespace: %v", pod.Name, err)
 			} else {
-				err = oc.ovnNBClient.Execute(addrSetCmds...)
-				if err != nil {
-					klog.Errorf("Error removing pod %s IPs from namespace address set: %v", portName, err)
+				if addrErr := oc.ovnNBClient.Execute(addrSetCmds...); addrErr != nil {
+					klog.Errorf("Error removing pod %s IPs from namespace address set: %v", portName, addrErr)
 				}
 			}
 		}