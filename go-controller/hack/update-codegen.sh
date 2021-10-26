1866d13f2e069e1ed8a0456c19a4158e85b089d9
Dan Williams <dcbw@redhat.com>
2021-10-26 11:58:38 -0500

    codegen: build and install missing *gen tools
    Signed-off-by: Dan Williams <dcbw@redhat.com>

--- go-controller/hack/update-codegen.sh
+++ go-controller/hack/update-codegen.sh
@@ -4,18 +4,35 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# generate ovsdb bindings
-if  ! ( command -v modelgen > /dev/null ); then
-  echo "modelgen not found, installing github.com/ovn-org/libovsdb/cmd/modelgen"
-  olddir="${PWD}"
-  builddir="$(mktemp -d)"
-  cd "${builddir}"
-  GO111MODULE=on go install github.com/ovn-org/libovsdb/cmd/modelgen@8b93f8d269af
-  cd "${olddir}"
-  if [[ "${builddir}" == /tmp/* ]]; then #paranoia
+source "$(dirname "${BASH_SOURCE}")/init.sh"
+
+# Check for `go` binary and set ${GOPATH}.
+setup_env
+
+function check_or_install() {
+  cmd=$1
+  url=$2
+
+  if ! ( command -v ${1} > /dev/null ); then
+    echo "${1} not found, installing ${2}"
+    olddir="${PWD}"
+    builddir="$(mktemp -d)"
+    cd "${builddir}"
+    GO111MODULE=on go install ${2}
+    cd "${olddir}"
+    if [[ "${builddir}" == /tmp/* ]]; then #paranoia
       rm -rf "${builddir}"
+    fi
   fi
-fi
+}
+readonly -f check_or_install
+
+check_or_install modelgen "github.com/ovn-org/libovsdb/cmd/modelgen@8b93f8d269af"
+check_or_install controller-gen "sigs.k8s.io/controller-tools/cmd/controller-gen@v0.7.0"
+check_or_install deepcopy-gen "k8s.io/code-generator/cmd/deepcopy-gen@v0.22.2"
+check_or_install client-gen "k8s.io/code-generator/cmd/client-gen@v0.22.2"
+check_or_install lister-gen "k8s.io/code-generator/cmd/lister-gen@v0.22.2"
+check_or_install informer-gen "k8s.io/code-generator/cmd/informer-gen@v0.22.2"
 
 go generate ./pkg/nbdb
 go generate ./pkg/sbdb
@@ -25,28 +42,17 @@ if [ -z "${crds}" ]; then
   exit
 fi
 
-if  ! ( command -v controller-gen > /dev/null ); then
-  echo "controller-gen not found, installing sigs.k8s.io/controller-tools"
-  olddir="${PWD}"
-  builddir="$(mktemp -d)"
-  cd "${builddir}"
-  GO111MODULE=on go get -u sigs.k8s.io/controller-tools/cmd/controller-gen
-  cd "${olddir}"
-  if [[ "${builddir}" == /tmp/* ]]; then #paranoia
-      rm -rf "${builddir}"
-  fi
-fi
-
 for crd in ${crds}; do
   echo "Generating deepcopy funcs for $crd"
   deepcopy-gen \
+    --go-header-file /dev/null \
     --input-dirs github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/$crd/v1 \
     -O zz_generated.deepcopy \
     --bounding-dirs github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd
 
-
   echo "Generating clientset for $crd"
   client-gen \
+    --go-header-file /dev/null \
     --clientset-name "${CLIENTSET_NAME_VERSIONED:-versioned}" \
     --input-base "" \
     --input github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/$crd/v1 \
@@ -55,12 +61,14 @@ for crd in ${crds}; do
 
   echo "Generating listers for $crd"
   lister-gen \
+    --go-header-file /dev/null \
     --input-dirs github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/$crd/v1 \
     --output-package github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/$crd/v1/apis/listers \
     "$@"
 
   echo "Generating informers for $crd"
   informer-gen \
+    --go-header-file /dev/null \
     --input-dirs github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/$crd/v1 \
     --versioned-clientset-package github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/$crd/v1/apis/clientset/versioned \
     --listers-package  github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/$crd/v1/apis/listers \
