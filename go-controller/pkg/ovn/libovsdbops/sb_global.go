68b59a89758c8ee75f4a9398a0053f40eafc3028
Andrew Stoycos <astoycos@redhat.com>
2021-11-30 10:55:16 -0500

    Fixup FindSBGlobal and monitor sb_global
    Some minor fixups, and ensure we return it's error if any
    in RegisterMasterMetrics
    
    Ensure we monitor the sbdb's sb_global table since
    it is fetched for metrics purposes
    
    Signed-off-by: Andrew Stoycos <astoycos@redhat.com>

--- go-controller/pkg/ovn/libovsdbops/sb_global.go
+++ go-controller/pkg/ovn/libovsdbops/sb_global.go
@@ -10,22 +10,22 @@ import (
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
 )
 
-// findSBGlobal returns the SBGlobal table entry
+// FindSBGlobal returns the SBGlobal table entry
 func FindSBGlobal(sbClient libovsdbclient.Client) (*sbdb.SBGlobal, error) {
 	sbGlobal := []sbdb.SBGlobal{}
 	ctx, cancel := context.WithTimeout(context.Background(), types.OVSDBTimeout)
 	defer cancel()
 	err := sbClient.List(ctx, &sbGlobal)
 	if err != nil {
-		return nil, fmt.Errorf("failed listing nbGlobal table entires err: %v", err)
+		return nil, fmt.Errorf("failed listing sbGlobal table entry err: %v", err)
 	}
-	// We should error if the nbGlobal table entry does not exist
+	// We should error if the sbGlobal table entry does not exist
 	if len(sbGlobal) == 0 {
 		return nil, libovsdbclient.ErrNotFound
 	}
-	// The nbGlobal table should only have one row
+	// The sbGlobal table should only have one row
 	if len(sbGlobal) != 1 {
-		return nil, fmt.Errorf("multible NBGlobal rows found")
+		return nil, fmt.Errorf("multible SBGlobal rows found")
 	}
 
 	return &sbGlobal[0], nil
