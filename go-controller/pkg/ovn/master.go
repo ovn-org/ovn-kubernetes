ovn-org/ovn-kubernetes

commit 79652ddc21c0ca37b3c00e855b8ac23403ee2d28

delete port connecting the distributed router to node's logical switch
when node is deleted, we need to deleted the node's logical switch and
the logical router port connecting the distributed router to the switch.

Signed-off-by: Yun Zhou <yunz@nvidia.com>

go-controller/pkg/ovn/master.go
@@ -1143,7 +1143,7 @@ func (oc *Controller) deleteNodeHostSubnet(nodeName string, subnet *net.IPNet) e
 	return nil
 }
 
-// deleteNodeLogicalNetwork removes the logical switch and router associated with the node
+// deleteNodeLogicalNetwork removes the logical switch and logical router port associated with the node
 func (oc *Controller) deleteNodeLogicalNetwork(nodeName string) error {
 	// Remove switch to lb associations from the LBCache before removing the switch
 	lbCache, err := ovnlb.GetLBCache(oc.nbClient)
@@ -1152,19 +1152,19 @@ func (oc *Controller) deleteNodeLogicalNetwork(nodeName string) error {
 	}
 	lbCache.RemoveSwitch(nodeName)
 	// Remove the logical switch associated with the node
-	logicalRouterName := types.RouterToSwitchPrefix + nodeName
+	logicalRouterPortName := types.RouterToSwitchPrefix + nodeName
 	opModels := []libovsdbops.OperationModel{
 		{
 			Model:          &nbdb.LogicalSwitch{},
 			ModelPredicate: func(ls *nbdb.LogicalSwitch) bool { return ls.Name == nodeName },
 		},
 		{
-			Model:          &nbdb.LogicalRouter{},
-			ModelPredicate: func(lr *nbdb.LogicalRouter) bool { return lr.Name == logicalRouterName },
+			Model:          &nbdb.LogicalRouterPort{},
+			ModelPredicate: func(lrp *nbdb.LogicalRouterPort) bool { return lrp.Name == logicalRouterPortName },
 		},
 	}
 	if err := oc.modelClient.Delete(opModels...); err != nil {
-		return fmt.Errorf("failed to delete logical switch and router associated with node: %s, error: %v", nodeName, err)
+		return fmt.Errorf("failed to delete logical switch and logical router port associated with node: %s, error: %v", nodeName, err)
 	}
 	return nil
 }

commit ac8fc97c9558066f4319bf5af9218380334094a0

Fix the node deletion cleanup.
When a node is deleted, the node logical switch and logical router
port connecting to the cluster router were not cleaned up in Northbound
db.  Fix this issue.

Issue 2711
https://github.com/ovn-org/ovn-kubernetes/issues/2711

Fixes: 79652ddc21c("delete port connecting the distributed router to node's logical switch")
Signed-off-by: Numan Siddique <numans@ovn.org>

go-controller/pkg/ovn/master.go
@@ -1190,14 +1190,29 @@ func (oc *Controller) deleteNodeLogicalNetwork(nodeName string) error {
 	lbCache.RemoveSwitch(nodeName)
 	// Remove the logical switch associated with the node
 	logicalRouterPortName := types.RouterToSwitchPrefix + nodeName
+	clusterRouterModel := nbdb.LogicalRouter{}
+	nodeLogicalRouterPort := nbdb.LogicalRouterPort{
+		Name: logicalRouterPortName,
+	}
 	opModels := []libovsdbops.OperationModel{
 		{
 			Model:          &nbdb.LogicalSwitch{},
 			ModelPredicate: func(ls *nbdb.LogicalSwitch) bool { return ls.Name == nodeName },
 		},
 		{
-			Model:          &nbdb.LogicalRouterPort{},
-			ModelPredicate: func(lrp *nbdb.LogicalRouterPort) bool { return lrp.Name == logicalRouterPortName },
+			Model: &nodeLogicalRouterPort,
+			DoAfter: func() {
+				if nodeLogicalRouterPort.UUID != "" {
+					clusterRouterModel.Ports = []string{nodeLogicalRouterPort.UUID}
+				}
+			},
+		},
+		{
+			Model:          &clusterRouterModel,
+			ModelPredicate: func(lr *nbdb.LogicalRouter) bool { return lr.Name == types.OVNClusterRouter },
+			OnModelMutations: []interface{}{
+				&clusterRouterModel.Ports,
+			},
 		},
 	}
 	if err := oc.modelClient.Delete(opModels...); err != nil {