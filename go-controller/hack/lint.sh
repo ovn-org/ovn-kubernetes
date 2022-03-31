155b794d05d78e8191610705d006056885f6b217
Dan Williams <dcbw@redhat.com>
2022-03-31 15:39:44 -0500

    hack: bump golangci-lint to 1.45.2
    Fixes:
    
    level=warning msg="[runner] Can't run linter unused: buildir: failed to load package encoding: could not load export data: cannot import \"encoding\" (unknown iexport format version 2), export data is newer version - update tool"
    
    Signed-off-by: Dan Williams <dcbw@redhat.com>

--- go-controller/hack/lint.sh
+++ go-controller/hack/lint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# pin golangci-lint version to 1.44.0
-VERSION=v1.44.0
+# pin golangci-lint version to 1.45.2
+VERSION=v1.45.2
 if [ "$#" -ne 1 ]; then
     echo "Expected command line argument - container runtime (docker/podman) got $# arguments: $@"
     exit 1
