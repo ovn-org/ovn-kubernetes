07b3de6cbc7a94103ad5c176764abddd65dd8979
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2022-04-27 13:29:16 +0000

    Bump to ovn-org/libovsdb#305
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

--- go-controller/vendor/github.com/ovn-org/libovsdb/mapper/mapper.go
+++ go-controller/vendor/github.com/ovn-org/libovsdb/mapper/mapper.go
@@ -319,16 +319,10 @@ func (m Mapper) equalIndexes(one, other *Info, indexes ...string) (bool, error) 
 // NewMonitorRequest returns a monitor request for the provided tableName
 // If fields is provided, the request will be constrained to the provided columns
 // If no fields are provided, all columns will be used
-func (m *Mapper) NewMonitorRequest(data *Info, fields []interface{}) (*ovsdb.MonitorRequest, error) {
+func (m *Mapper) NewMonitorRequest(data *Info, fields []string) (*ovsdb.MonitorRequest, error) {
 	var columns []string
 	if len(fields) > 0 {
-		for _, f := range fields {
-			column, err := data.ColumnByPtr(f)
-			if err != nil {
-				return nil, err
-			}
-			columns = append(columns, column)
-		}
+		columns = append(columns, fields...)
 	} else {
 		for c := range data.Metadata.TableSchema.Columns {
 			columns = append(columns, c)
