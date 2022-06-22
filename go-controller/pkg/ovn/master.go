d47644268e5037c7ed3a46b098adb73323941e5c
Tim Rozet <trozet@redhat.com>
2022-06-22 17:20:37 -0400

    Conditionally request pod retries during node add
    We were always requesting pods on a node to be retried immediately, even
    though they would end up failing if disable snat gw was true and the
    node did not yet have an l3 gateway config set. If this case is true,
    the pod add will fail, so there is no point requesting them to be added.
    
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- go-controller/pkg/ovn/master.go
+++ go-controller/pkg/ovn/master.go
@@ -1387,21 +1387,24 @@ func (oc *Controller) addUpdateNodeEvent(node *kapi.Node, nSyncs *nodeSyncs) err
 	}
 
 	// ensure pods that already exist on this node have their logical ports created
-	options := metav1.ListOptions{FieldSelector: fields.OneTermEqualSelector("spec.nodeName", node.Name).String()}
-	pods, err := oc.client.CoreV1().Pods(metav1.NamespaceAll).List(context.TODO(), options)
-	if err != nil {
-		klog.Errorf("Unable to list existing pods on node: %s, existing pods on this node may not function")
-	} else if nSyncs.syncNode { // do this only if its a new node add
-		klog.V(5).Infof("When adding node %s, found %d pods to add to retryPods", node.Name, len(pods.Items))
-		for _, pod := range pods.Items {
-			pod := pod
-			if util.PodCompleted(&pod) {
-				continue
+	// if per pod SNAT is being used, then l3 gateway config is required to be able to add pods
+	if _, gwFailed := oc.gatewaysFailed.Load(node.Name); !gwFailed || !config.Gateway.DisableSNATMultipleGWs {
+		options := metav1.ListOptions{FieldSelector: fields.OneTermEqualSelector("spec.nodeName", node.Name).String()}
+		pods, err := oc.client.CoreV1().Pods(metav1.NamespaceAll).List(context.TODO(), options)
+		if err != nil {
+			klog.Errorf("Unable to list existing pods on node: %s, existing pods on this node may not function")
+		} else if nSyncs.syncNode || nSyncs.syncGw { // do this only if it is a new node add or a gateway sync happened
+			klog.V(5).Infof("When adding node %s, found %d pods to add to retryPods", node.Name, len(pods.Items))
+			for _, pod := range pods.Items {
+				pod := pod
+				if util.PodCompleted(&pod) {
+					continue
+				}
+				klog.V(5).Infof("Adding pod %s/%s to retryPods", pod.Namespace, pod.Name)
+				oc.retryPods.addRetryObjWithAdd(&pod)
 			}
-			klog.V(5).Infof("Adding pod %s/%s to retryPods", pod.Namespace, pod.Name)
-			oc.retryPods.addRetryObjWithAdd(&pod)
+			oc.retryPods.requestRetryObjs()
 		}
-		oc.retryPods.requestRetryObjs()
 	}
 
 	if len(errs) == 0 {
