1bbdc048677ba2a97274d52a9cc96f97b006f6c1
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2022-09-14 15:54:27 +0000

    Don't assume policy type when evaluating isolation
    PolicyTypes field of a policy should always be set to an appropriate
    value before it reaches the network plugin. The network plugin should
    not make any assumptions about what the policy type is outside of what
    PolicyTypes indicates towards evaluating the isolation status of a
    selected pod.
    
    Not doing any re-synchronization of possibly affected policies because:
    - If the user does not provide PolicyTypes, api server will fill those
      appropriately.
    - It is unlikely that a user provides PolicyTypes that do not include a
      type for which rules are provided as well. And if it does, it's
      probably better not to change the behavior of those policies anyway.
    
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

--- go-controller/pkg/ovn/policy.go
+++ go-controller/pkg/ovn/policy.go
@@ -705,6 +705,22 @@ func podDeleteAllowMulticastPolicy(nbClient libovsdbclient.Client, ns string, po
 	return libovsdbops.DeletePortsFromPortGroup(nbClient, hashedPortGroup(ns), portUUID)
 }
 
+// policyType returns whether the policy is of type ingress and/or egress
+func policyType(policy *knet.NetworkPolicy) (bool, bool) {
+	var policyTypeIngress bool
+	var policyTypeEgress bool
+
+	for _, policyType := range policy.Spec.PolicyTypes {
+		if policyType == knet.PolicyTypeIngress {
+			policyTypeIngress = true
+		} else if policyType == knet.PolicyTypeEgress {
+			policyTypeEgress = true
+		}
+	}
+
+	return policyTypeIngress, policyTypeEgress
+}
+
 // localPodAddDefaultDeny ensures ports (i.e. pods) are in the correct
 // default-deny portgroups. Whether or not pods are in default-deny depends
 // on whether or not any policies select this pod, so there is a reference
@@ -713,22 +729,12 @@ func (oc *Controller) localPodAddDefaultDeny(policy *knet.NetworkPolicy,
 	ports ...*lpInfo) (ingressDenyPorts, egressDenyPorts []string) {
 	oc.lspMutex.Lock()
 
-	// Default deny rule.
-	// 1. Any pod that matches a network policy should get a default
-	// ingress deny rule.  This is irrespective of whether there
-	// is a ingress section in the network policy. But, if
-	// PolicyTypes in the policy has only "egress" in it, then
-	// it is a 'egress' only network policy and we should not
-	// add any default deny rule for ingress.
-	// 2. If there is any "egress" section in the policy or
-	// the PolicyTypes has 'egress' in it, we add a default
-	// egress deny rule.
-
 	ingressDenyPorts = []string{}
 	egressDenyPorts = []string{}
 
-	// Handle condition 1 above.
-	if !(len(policy.Spec.PolicyTypes) == 1 && policy.Spec.PolicyTypes[0] == knet.PolicyTypeEgress) {
+	policyTypeIngress, policyTypeEgress := policyType(policy)
+
+	if policyTypeIngress {
 		for _, portInfo := range ports {
 			// if this is the first NP referencing this pod, then we
 			// need to add it to the port group.
@@ -742,8 +748,7 @@ func (oc *Controller) localPodAddDefaultDeny(policy *knet.NetworkPolicy,
 	}
 
 	// Handle condition 2 above.
-	if (len(policy.Spec.PolicyTypes) == 1 && policy.Spec.PolicyTypes[0] == knet.PolicyTypeEgress) ||
-		len(policy.Spec.Egress) > 0 || len(policy.Spec.PolicyTypes) == 2 {
+	if policyTypeEgress {
 		for _, portInfo := range ports {
 			if oc.lspEgressDenyCache[portInfo.name] == 0 {
 				// again, reference count is 0, so add to port
@@ -768,9 +773,11 @@ func (oc *Controller) localPodDelDefaultDeny(
 	ingressDenyPorts = []string{}
 	egressDenyPorts = []string{}
 
+	policyTypeIngress, policyTypeEgress := policyType(np.policy)
+
 	// Remove port from ingress deny port-group for [Ingress] and [ingress,egress] PolicyTypes
 	// If NOT [egress] PolicyType
-	if !(len(np.policy.Spec.PolicyTypes) == 1 && np.policy.Spec.PolicyTypes[0] == knet.PolicyTypeEgress) {
+	if policyTypeIngress {
 		for _, portInfo := range ports {
 			if oc.lspIngressDenyCache[portInfo.name] > 0 {
 				oc.lspIngressDenyCache[portInfo.name]--
@@ -784,8 +791,7 @@ func (oc *Controller) localPodDelDefaultDeny(
 
 	// Remove port from egress deny port group for [egress] and [ingress,egress] PolicyTypes
 	// if [egress] PolicyType OR there are any egress rules OR [ingress,egress] PolicyType
-	if (len(np.policy.Spec.PolicyTypes) == 1 && np.policy.Spec.PolicyTypes[0] == knet.PolicyTypeEgress) ||
-		len(np.egressPolicies) > 0 || len(np.policy.Spec.PolicyTypes) == 2 {
+	if policyTypeEgress {
 		for _, portInfo := range ports {
 			if oc.lspEgressDenyCache[portInfo.name] > 0 {
 				oc.lspEgressDenyCache[portInfo.name]--
@@ -1071,6 +1077,15 @@ func (oc *Controller) createNetworkPolicy(np *networkPolicy, policy *knet.Networ
 		podSelector       *metav1.LabelSelector
 	}
 	var policyHandlers []policyHandler
+
+	// Consider both ingress and egress rules of the policy regardless of this
+	// policy type. A pod is isolated as long as as it is selected by any
+	// namespace policy. Since we don't process all namespace policies on a
+	// given policy update that might change the isolation status of a selected
+	// pod, we have create the allow ACLs derived from the policy rules in case
+	// the selected pods become isolated in the future even if that is not their
+	// current status.
+
 	// Go through each ingress rule.  For each ingress rule, create an
 	// addressSet for the peer pods.
 	for i, ingressJSON := range policy.Spec.Ingress {
@@ -1145,6 +1160,7 @@ func (oc *Controller) createNetworkPolicy(np *networkPolicy, policy *knet.Networ
 		}
 		np.egressPolicies = append(np.egressPolicies, egress)
 	}
+
 	np.Unlock()
 
 	for _, handler := range policyHandlers {
