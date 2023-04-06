ovn-org/ovn-kubernetes

commit 4b3475abb4a2f0c5d5976992e5365dc2549d5dc7

services: Use OVN template load balancers for NodePort services.
For now we only use templates for NodePort services that don't have
ETP=local set or affinity timeout.  The latter is not supported in
core OVN but the former can probably be implemented as a follow up
change.

With load balancer templates we're able to reduce the size of the NB and
amount of work ovn-northd has to perform by a factor of N (where N is
the number of nodes).  In particular, for a cluster with N nodes and S
NodePort services:
- before this change we would configure O(N * S) load balancers (two per
  node for every service)
- with this change this gets reduced to O(S) load balancers (four per
  service).  We do have to configure N template variables per service
  but the impact of those on the amount of work ovn-northd has to
  perform is minimal as it will only have to propagate those records
  to the Southbound.

Instead of generating a load balancer per service per node we now
generate at most four (template) load balancers (IPv4/IPv6 applicable to
gateway routers or applicable to node switches).

Each template load balancer uses as VIP a template variable denoting
the node IP, e.g., NODEIP_IPv4 and as backend another template variable,
e.g., SVC_node_port_svc__UDP__30957__node__router__template__IPv4.

These template variables have potentially different values on different
chassis. For example, NODEIP_IPv4 will expand to chassis-X's IP on
chassis-X and to chassis-Y's IP on chassis-Y.

Similarly, SVC_node_port_svc__UDP__30957__node__router__template__IPv4
will expand on chassis-X to the set of backends that are required by
the NodePort service on chassis-X and that might be different than the
set of backends required on chassis-Y.

The node IP template variables are managed by the services controller.
Together with the nodeInfo structs provided by the node tracker they
represent the node-specific information.  A RWMutex, nodeInfoRWLock,

go-controller/pkg/ovn/controller/services/repair.go
@@ -67,13 +67,30 @@ func (r *repair) runBeforeSync() {
 		r.unsyncedServices.Insert(key)
 	}
 
+	// Find all templates.
+	allTemplates, err := listSvcTemplates(r.nbClient)
+	if err != nil {
+		klog.Errorf("Unable to get templates for repair: %v", err)
+	}
+
 	// Find all load-balancers associated with Services
-	existingLBs, err := getLBs(r.nbClient)
+	existingLBs, err := getLBs(r.nbClient, allTemplates)
 	if err != nil {
 		klog.Errorf("Unable to get service lbs for repair: %v", err)
 	}
 
 	// Look for any load balancers whose Service no longer exists in the apiserver
+	// and for any chassis template vars whose Service no longer exists.
+	staleTemplateNames := sets.Set[string]{}
+	for templateName := range allTemplates {
+		// NodeIP templates are always valid, skip those.
+		if isLBNodeIPTemplateName(templateName) {
+			continue
+		}
+
+		// All others are potentially stale.
+		staleTemplateNames.Insert(templateName)
+	}
 	staleLBs := []string{}
 	for _, lb := range existingLBs {
 		// Extract namespace + name, look to see if it exists
@@ -82,12 +99,19 @@ func (r *repair) runBeforeSync() {
 		if err != nil || namespace == "" {
 			klog.Warningf("Service LB %#v has unreadable owner, deleting", lb)
 			staleLBs = append(staleLBs, lb.UUID)
+			continue
 		}
 
 		_, err = r.serviceLister.Services(namespace).Get(name)
 		if apierrors.IsNotFound(err) {
 			klog.V(5).Infof("Found stale service LB %#v", lb)
 			staleLBs = append(staleLBs, lb.UUID)
+			continue
+		}
+
+		// All of the LB's template vars are still useful.
+		for _, t := range lb.Templates {
+			staleTemplateNames.Delete(t.Name)
 		}
 	}
 
@@ -96,6 +120,12 @@ func (r *repair) runBeforeSync() {
 		klog.Errorf("Failed to delete stale LBs: %v", err)
 	}
 	klog.V(2).Infof("Deleted %d stale service LBs", len(staleLBs))
+
+	// Delete those stale template vars
+	if err := libovsdbops.DeleteAllChassisTemplateVarVariables(r.nbClient, staleTemplateNames.UnsortedList()); err != nil {
+		klog.Errorf("Failed to delete stale Chassis Template Vars: %v", err)
+	}
+	klog.V(2).Infof("Deleted %d stale Chassis Template Vars", len(staleTemplateNames))
 
 	// Remove existing reject rules. They are not used anymore
 	// given the introduction of idling loadbalancers

go-controller/pkg/ovn/controller/services/services_controller.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	libovsdbclient "github.com/ovn-org/libovsdb/client"
+	globalconfig "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/factory"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/metrics"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
@@ -62,6 +63,8 @@ func NewController(client clientset.Interface,
 		queue:            workqueue.NewNamedRateLimitingQueue(newRatelimiter(100), controllerName),
 		workerLoopPeriod: time.Second,
 		alreadyApplied:   map[string][]LB{},
+		nodeIPv4Template: makeTemplate(makeLBNodeIPTemplateName(v1.IPv4Protocol)),
+		nodeIPv6Template: makeTemplate(makeLBNodeIPTemplateName(v1.IPv6Protocol)),
 	}
 
 	// services
@@ -147,18 +150,34 @@ type Controller struct {
 	// nodeTracker
 	nodeTracker *nodeTracker
 
+	// Per node information and template variables.  The latter expand to each
+	// chassis' node IP (v4 and v6).
+	// Must be accessed only with the nodeInfo mutex taken.
+	// These are written in RequestFullSync().
+	nodeInfos        []nodeInfo
+	nodeIPv4Template *Template
+	nodeIPv6Template *Template
+	nodeInfoRWLock   sync.RWMutex
+
 	// alreadyApplied is a map of service key -> already applied configuration, so we can short-circuit
 	// if a service's config hasn't changed
-	alreadyApplied     map[string][]LB
-	alreadyAppliedLock sync.Mutex
+	alreadyApplied       map[string][]LB
+	alreadyAppliedRWLock sync.RWMutex
+
+	// Lock order considerations: if both nodeInfoRWLock and alreadyAppliedRWLock
+	// need to be taken for some reason then the order in which they're taken is
+	// always: first nodeInfoRWLock and then alreadyAppliedRWLock.
 
 	// 'true' if Load_Balancer_Group is supported.
 	useLBGroups bool
+
+	// 'true' if Chassis_Template_Var is supported.
+	useTemplates bool
 }
 
 // Run will not return until stopCh is closed. workers determines how many
 // endpoints will be handled in parallel.
-func (c *Controller) Run(workers int, stopCh <-chan struct{}, runRepair, useLBGroups bool) error {
+func (c *Controller) Run(workers int, stopCh <-chan struct{}, runRepair, useLBGroups, useTemplates bool) error {
 	defer utilruntime.HandleCrash()
 	defer c.queue.ShutDown()
 
@@ -166,6 +185,7 @@ func (c *Controller) Run(workers int, stopCh <-chan struct{}, runRepair, useLBGr
 	defer klog.Infof("Shutting down controller %s", controllerName)
 
 	c.useLBGroups = useLBGroups
+	c.useTemplates = useTemplates
 
 	// Wait for the caches to be synced
 	klog.Info("Waiting for informer caches to sync")
@@ -246,11 +266,19 @@ func (c *Controller) handleErr(err error, key interface{}) {
 // That is because such work will be performed in syncService, so all that is needed here is the ability
 // to distinguish what is present in ovn database and this 'dirty' initial value.
 func (c *Controller) initTopLevelCache() error {
-	c.alreadyAppliedLock.Lock()
-	defer c.alreadyAppliedLock.Unlock()
+	var err error
 
-	// first, list all load balancers and their respective services
-	services, lbs, err := getServiceLBs(c.nbClient)
+	c.alreadyAppliedRWLock.Lock()
+	defer c.alreadyAppliedRWLock.Unlock()
+
+	// First list all the templates.
+	allTemplates, err := listSvcTemplates(c.nbClient)
+	if err != nil {
+		return fmt.Errorf("failed to load templates: %w", err)
+	}
+
+	// Then list all load balancers and their respective services.
+	services, lbs, err := getServiceLBs(c.nbClient, allTemplates)
 	if err != nil {
 		return fmt.Errorf("failed to load balancers: %w", err)
 	}
@@ -288,6 +316,11 @@ func (c *Controller) syncService(key string) error {
 		metrics.MetricSyncServiceLatency.Observe(time.Since(startTime).Seconds())
 	}()
 
+	// Shared node information (c.nodeInfos, c.nodeIPv4Template, c.nodeIPv6Template)
+	// needs to be accessed with the nodeInfoRWLock taken for read.
+	c.nodeInfoRWLock.RLock()
+	defer c.nodeInfoRWLock.RUnlock()
+
 	// Get current Service from the cache
 	service, err := c.serviceLister.Services(namespace).Get(name)
 	// It´s unlikely that we have an error different that "Not Found Object"
@@ -307,14 +340,14 @@ func (c *Controller) syncService(key string) error {
 			},
 		}
 
-		c.alreadyAppliedLock.Lock()
+		c.alreadyAppliedRWLock.RLock()
 		alreadyAppliedLbs, alreadyAppliedKeyExists := c.alreadyApplied[key]
 		var existingLBs []LB
 		if alreadyAppliedKeyExists {
 			existingLBs = make([]LB, len(alreadyAppliedLbs))
 			copy(existingLBs, alreadyAppliedLbs)
 		}
-		c.alreadyAppliedLock.Unlock()
+		c.alreadyAppliedRWLock.RUnlock()
 
 		if alreadyAppliedKeyExists {
 			//
@@ -328,9 +361,9 @@ func (c *Controller) syncService(key string) error {
 					namespace, name, err)
 			}
 
-			c.alreadyAppliedLock.Lock()
+			c.alreadyAppliedRWLock.Lock()
 			delete(c.alreadyApplied, key)
-			c.alreadyAppliedLock.Unlock()
+			c.alreadyAppliedRWLock.Unlock()
 		}
 
 		c.repair.serviceSynced(key)
@@ -355,29 +388,35 @@ func (c *Controller) syncService(key string) error {
 	}
 
 	// Build the abstract LB configs for this service
-	perNodeConfigs, clusterConfigs := buildServiceLBConfigs(service, endpointSlices)
+	perNodeConfigs, templateConfigs, clusterConfigs := buildServiceLBConfigs(service, endpointSlices,
+		c.useLBGroups, c.useTemplates)
 	klog.V(5).Infof("Built service %s LB cluster-wide configs %#v", key, clusterConfigs)
 	klog.V(5).Infof("Built service %s LB per-node configs %#v", key, perNodeConfigs)
+	klog.V(5).Infof("Built service %s LB template configs %#v", key, templateConfigs)
 
 	// Convert the LB configs in to load-balancer objects
-	nodeInfos := c.nodeTracker.allNodes()
-	clusterLBs := buildClusterLBs(service, clusterConfigs, nodeInfos, c.useLBGroups)
-	perNodeLBs := buildPerNodeLBs(service, perNodeConfigs, nodeInfos)
+	clusterLBs := buildClusterLBs(service, clusterConfigs, c.nodeInfos, c.useLBGroups)
+	templateLBs := buildTemplateLBs(service, templateConfigs, c.nodeInfos,
+		c.nodeIPv4Template, c.nodeIPv6Template)
+	perNodeLBs := buildPerNodeLBs(service, perNodeConfigs, c.nodeInfos)
 	klog.V(5).Infof("Built service %s cluster-wide LB %#v", key, clusterLBs)
 	klog.V(5).Infof("Built service %s per-node LB %#v", key, perNodeLBs)
-	klog.V(3).Infof("Service %s has %d cluster-wide and %d per-node configs, making %d and %d load balancers",
-		key, len(clusterConfigs), len(perNodeConfigs), len(clusterLBs), len(perNodeLBs))
-	lbs := append(clusterLBs, perNodeLBs...)
+	klog.V(5).Infof("Built service %s template LB %#v", key, templateLBs)
+	klog.V(3).Infof("Service %s has %d cluster-wide, %d per-node configs, %d template configs, making %d (cluster) %d (per node) and %d (template) load balancers",
+		key, len(clusterConfigs), len(perNodeConfigs), len(templateConfigs),
+		len(clusterLBs), len(perNodeLBs), len(templateLBs))
+	lbs := append(clusterLBs, templateLBs...)
+	lbs = append(lbs, perNodeLBs...)
 
 	// Short-circuit if nothing has changed
-	c.alreadyAppliedLock.Lock()
+	c.alreadyAppliedRWLock.RLock()
 	alreadyAppliedLbs, alreadyAppliedKeyExists := c.alreadyApplied[key]
 	var existingLBs []LB
 	if alreadyAppliedKeyExists {
 		existingLBs = make([]LB, len(alreadyAppliedLbs))
 		copy(existingLBs, alreadyAppliedLbs)
 	}
-	c.alreadyAppliedLock.Unlock()
+	c.alreadyAppliedRWLock.RUnlock()
 
 	if alreadyAppliedKeyExists && LoadBalancersEqualNoUUID(existingLBs, lbs) {
 		klog.V(3).Infof("Skipping no-op change for service %s", key)
@@ -391,23 +430,78 @@ func (c *Controller) syncService(key string) error {
 			return fmt.Errorf("failed to ensure service %s load balancers: %w", key, err)
 		}
 
-		c.alreadyAppliedLock.Lock()
+		c.alreadyAppliedRWLock.Lock()
 		c.alreadyApplied[key] = lbs
-		c.alreadyAppliedLock.Unlock()
+		c.alreadyAppliedRWLock.Unlock()
 	}
 
 	c.repair.serviceSynced(key)
 	return nil
 }
 
+func (c *Controller) syncNodeInfos(nodeInfos []nodeInfo) {
+	c.nodeInfoRWLock.Lock()
+	defer c.nodeInfoRWLock.Unlock()
+
+	c.nodeInfos = nodeInfos
+	if !c.useTemplates {
+		return
+	}
+
+	// Compute the nodeIP template values.
+	c.nodeIPv4Template = makeTemplate(makeLBNodeIPTemplateName(v1.IPv4Protocol))
+	c.nodeIPv6Template = makeTemplate(makeLBNodeIPTemplateName(v1.IPv6Protocol))
+
+	for _, node := range c.nodeInfos {
+		if node.chassisID == "" {
+			continue
+		}
+		// Services are currently supported only on the node's first IP.
+		// Extract that one and populate the node's IP template value.
+		if globalconfig.IPv4Mode {
+			if ipv4, err := util.MatchFirstIPFamily(false, node.nodeIPs); err == nil {
+				c.nodeIPv4Template.Value[node.chassisID] = ipv4.String()
+			}
+		}
+		if globalconfig.IPv6Mode {
+			if ipv6, err := util.MatchFirstIPFamily(true, node.nodeIPs); err == nil {
+				c.nodeIPv6Template.Value[node.chassisID] = ipv6.String()
+			}
+		}
+	}
+
+	// Sync the nodeIP template values to the DB.
+	nodeIPTemplateMap := TemplateMap{}
+	if c.nodeIPv4Template.len() > 0 {
+		nodeIPTemplateMap[c.nodeIPv4Template.Name] = c.nodeIPv4Template
+	}
+	if c.nodeIPv6Template.len() > 0 {
+		nodeIPTemplateMap[c.nodeIPv6Template.Name] = c.nodeIPv6Template
+	}
+
+	nodeIPTemplates := []TemplateMap{
+		nodeIPTemplateMap,
+	}
+	if err := svcCreateOrUpdateTemplateVar(c.nbClient, nodeIPTemplates); err != nil {
+		klog.Errorf("Could not sync node IP templates")
+		return
+	}
+}
+
 // RequestFullSync re-syncs every service that currently exists
-func (c *Controller) RequestFullSync() {
+func (c *Controller) RequestFullSync(nodeInfos []nodeInfo) {
 	klog.Info("Full service sync requested")
+
+	// Resync node infos and node IP templates.
+	c.syncNodeInfos(nodeInfos)
+
+	// Resync services.
 	services, err := c.serviceLister.List(labels.Everything())
 	if err != nil {
 		klog.Errorf("Cached lister failed!? %v", err)
 		return
 	}
+
 	for _, service := range services {
 		c.onServiceAdd(service)
 	}

go-controller/pkg/ovn/master.go
@@ -386,7 +386,7 @@ func (oc *DefaultNetworkController) ensureNodeLogicalNetwork(node *kapi.Node, ho
 		return err
 	}
 
-	return oc.createNodeLogicalSwitch(node.Name, hostSubnets, oc.loadBalancerGroupUUID)
+	return oc.createNodeLogicalSwitch(node.Name, hostSubnets, oc.clusterLoadBalancerGroupUUID, oc.switchLoadBalancerGroupUUID)
 }
 
 func (oc *DefaultNetworkController) addNode(node *kapi.Node) ([]*net.IPNet, error) {
@@ -480,6 +480,13 @@ func (oc *DefaultNetworkController) deleteStaleNodeChassis(node *kapi.Node) erro
 		p := func(item *sbdb.Chassis) bool {
 			return item.Name == staleChassis
 		}
+		if err = libovsdbops.DeleteChassisTemplateVar(oc.nbClient, &nbdb.ChassisTemplateVar{Chassis: staleChassis}); err != nil {
+			// Send an event and Log on failure
+			oc.recorder.Eventf(node, kapi.EventTypeWarning, "ErrorMismatchChassis",
+				"Node %s is now with a new chassis ID. Its stale chassis template vars are still in the NBDB",
+				node.Name)
+			return fmt.Errorf("node %s is now with a new chassis ID. Its stale chassis template vars are still in the NBDB", node.Name)
+		}
 		if err = libovsdbops.DeleteChassisWithPredicate(oc.sbClient, p); err != nil {
 			// Send an event and Log on failure
 			oc.recorder.Eventf(node, kapi.EventTypeWarning, "ErrorMismatchChassis",
@@ -504,11 +511,19 @@ func (oc *DefaultNetworkController) deleteNode(nodeName string) error {
 		return fmt.Errorf("failed to clean up GR LRP IPs for node %s: %v", nodeName, err)
 	}
 
+	chassisTemplateVars := make([]*nbdb.ChassisTemplateVar, 0)
 	p := func(item *sbdb.Chassis) bool {
-		return item.Hostname == nodeName
+		if item.Hostname == nodeName {
+			chassisTemplateVars = append(chassisTemplateVars, &nbdb.ChassisTemplateVar{Chassis: item.Name})
+			return true
+		}
+		return false
 	}
 	if err := libovsdbops.DeleteChassisWithPredicate(oc.sbClient, p); err != nil {
 		return fmt.Errorf("failed to remove the chassis associated with node %s in the OVN SB Chassis table: %v", nodeName, err)
+	}
+	if err := libovsdbops.DeleteChassisTemplateVar(oc.nbClient, chassisTemplateVars...); err != nil {
+		return fmt.Errorf("failed deleting chassis template variables for %s: %v", nodeName, err)
 	}
 	return nil
 }
@@ -565,42 +580,19 @@ func (oc *DefaultNetworkController) clearInitialNodeNetworkUnavailableCondition(
 // and sbdb and deletes chassis that are stale
 func (oc *DefaultNetworkController) syncNodesPeriodic() {
 	//node names is a slice of all node names
-	nodes, err := oc.kube.GetNodes()
+	kNodes, err := oc.kube.GetNodes()
 	if err != nil {
 		klog.Errorf("Error getting existing nodes from kube API: %v", err)
 		return
 	}
 
-	nodeNames := make([]string, 0, len(nodes.Items))
-
-	for _, node := range nodes.Items {
-		nodeNames = append(nodeNames, node.Name)
+	nodes := make([]*kapi.Node, 0, len(kNodes.Items))
+	for i := range kNodes.Items {
+		nodes = append(nodes, &kNodes.Items[i])
 	}
 
-	chassisList, err := libovsdbops.ListChassis(oc.sbClient)
-	if err != nil {
-		klog.Errorf("Failed to get chassis list: error: %v", err)
-		return
-	}
-
-	chassisHostNameMap := map[string]*sbdb.Chassis{}
-	for _, chassis := range chassisList {
-		chassisHostNameMap[chassis.Hostname] = chassis
-	}
-
-	//delete existing nodes from the chassis map.
-	for _, nodeName := range nodeNames {
-		delete(chassisHostNameMap, nodeName)
-	}
-
-	staleChassis := []*sbdb.Chassis{}
-	for _, v := range chassisHostNameMap {
-		staleChassis = append(staleChassis, v)
-	}
-
-	if err = libovsdbops.DeleteChassis(oc.sbClient, staleChassis...); err != nil {
-		klog.Errorf("Failed Deleting chassis %v error: %v", chassisHostNameMap, err)
-		return
+	if err := oc.syncChassis(nodes); err != nil {
+		klog.Errorf("Failed to sync chassis: error: %v", err)
 	}
 }
 
@@ -608,9 +600,10 @@ func (oc *DefaultNetworkController) syncNodesPeriodic() {
 // watchNodes() will be called for all existing nodes at startup anyway.
 // Note that this list will include the 'join' cluster switch, which we
 // do not want to delete.
-func (oc *DefaultNetworkController) syncNodes(nodes []interface{}) error {
+func (oc *DefaultNetworkController) syncNodes(kNodes []interface{}) error {
 	foundNodes := sets.New[string]()
-	for _, tmp := range nodes {
+	nodes := make([]*kapi.Node, 0, len(kNodes))
+	for _, tmp := range kNodes {
 		node, ok := tmp.(*kapi.Node)
 		if !ok {
 			return fmt.Errorf("spurious object in syncNodes: %v", tmp)
@@ -620,6 +613,7 @@ func (oc *DefaultNetworkController) syncNodes(nodes []interface{}) error {
 			continue
 		}
 		foundNodes.Insert(node.Name)
+		nodes = append(nodes, node)
 
 		// For each existing node, reserve its joinSwitch LRP IPs if they already exist.
 		if _, err := oc.joinSwIPManager.EnsureJoinLRPIPs(node.Name); err != nil {
@@ -644,44 +638,82 @@ func (oc *DefaultNetworkController) syncNodes(nodes []interface{}) error {
 		}
 	}
 
-	// cleanup stale chassis with no corresponding nodes
+	if err := oc.syncChassis(nodes); err != nil {
+		return fmt.Errorf("failed to sync chassis: error: %v", err)
+	}
+	return nil
+}
+
+// Cleanup stale chassis and chassis template variables with no
+// corresponding nodes.
+func (oc *DefaultNetworkController) syncChassis(nodes []*kapi.Node) error {
 	chassisList, err := libovsdbops.ListChassis(oc.sbClient)
 	if err != nil {
-		return fmt.Errorf("failed to get chassis list: %v", err)
+		return fmt.Errorf("failed to get chassis list: error: %v", err)
 	}
 
-	knownChassisNames := sets.NewString()
-	chassisDeleteList := []*sbdb.Chassis{}
-	for _, chassis := range chassisList {
-		knownChassisNames.Insert(chassis.Name)
-		// skip chassis that have a corresponding node
-		if foundNodes.Has(chassis.Hostname) {
-			continue
-		}
-		chassisDeleteList = append(chassisDeleteList, chassis)
-	}
-
-	// cleanup stale chassis private with no corresponding chassis
+	// Cleanup stale chassis private with no corresponding chassis
 	chassisPrivateList, err := libovsdbops.ListChassisPrivate(oc.sbClient)
 	if err != nil {
 		return fmt.Errorf("failed to get chassis private list: %v", err)
 	}
 
-	for _, chassis := range chassisPrivateList {
-		// skip chassis private that have a corresponding chassis
-		if knownChassisNames.Has(chassis.Name) {
-			continue
-		}
-		// we add to the list what would be the corresponding Chassis. Even if
-		// the Chassis does not exist in SBDB, DeleteChassis will remove the
-		// ChassisPrivate.
-		chassisDeleteList = append(chassisDeleteList, &sbdb.Chassis{Name: chassis.Name})
+	templateVarList, err := libovsdbops.ListTemplateVar(oc.nbClient)
+	if err != nil {
+		return fmt.Errorf("failed to get template var list: error: %v", err)
 	}
 
-	// Delete stale chassis and associated chassis private
-	if err := libovsdbops.DeleteChassis(oc.sbClient, chassisDeleteList...); err != nil {
-		return fmt.Errorf("failed deleting chassis %v: %v", chassisDeleteList, err)
+	chassisHostNameMap := map[string]*sbdb.Chassis{}
+	chassisNameMap := map[string]*sbdb.Chassis{}
+	for _, chassis := range chassisList {
+		chassisHostNameMap[chassis.Hostname] = chassis
+		chassisNameMap[chassis.Name] = chassis
 	}
+
+	for _, chassisPrivate := range chassisPrivateList {
+		// Skip chassis private that have a corresponding chassis
+		if _, ok := chassisNameMap[chassisPrivate.Name]; ok {
+			continue
+		}
+		// We add to the map what would be the corresponding Chassis. Even if
+		// the Chassis does not exist in SBDB, DeleteChassis will remove the
+		// ChassisPrivate.
+		chassisNameMap[chassisPrivate.Name] = &sbdb.Chassis{Name: chassisPrivate.Name}
+	}
+
+	templateChassisMap := map[string]*nbdb.ChassisTemplateVar{}
+	for _, templateVar := range templateVarList {
+		templateChassisMap[templateVar.Chassis] = templateVar
+	}
+
+	// Delete existing nodes from the chassis map.
+	// Also delete existing templateVars from the template map.
+	for _, node := range nodes {
+		if chassis, ok := chassisHostNameMap[node.Name]; ok {
+			delete(chassisNameMap, chassis.Name)
+			delete(chassisHostNameMap, chassis.Hostname)
+			delete(templateChassisMap, chassis.Name)
+		}
+	}
+
+	staleChassis := make([]*sbdb.Chassis, 0, len(chassisHostNameMap))
+	for _, chassis := range chassisNameMap {
+		staleChassis = append(staleChassis, chassis)
+	}
+
+	staleChassisTemplateVars := make([]*nbdb.ChassisTemplateVar, 0, len(templateChassisMap))
+	for _, template := range templateChassisMap {
+		staleChassisTemplateVars = append(staleChassisTemplateVars, template)
+	}
+
+	if err := libovsdbops.DeleteChassis(oc.sbClient, staleChassis...); err != nil {
+		return fmt.Errorf("failed Deleting chassis %v error: %v", chassisHostNameMap, err)
+	}
+
+	if err := libovsdbops.DeleteChassisTemplateVar(oc.nbClient, staleChassisTemplateVars...); err != nil {
+		return fmt.Errorf("failed Deleting chassis template vars %v error: %v", chassisHostNameMap, err)
+	}
+
 	return nil
 }
 
@@ -831,6 +863,7 @@ func (oc *DefaultNetworkController) deleteNodeEvent(node *kapi.Node) error {
 	if err := oc.deleteNode(node.Name); err != nil {
 		return err
 	}
+
 	oc.lsManager.DeleteSwitch(node.Name)
 	oc.addNodeFailed.Delete(node.Name)
 	oc.mgmtPortFailed.Delete(node.Name)

go-controller/pkg/libovsdbops/model.go
@@ -62,6 +62,8 @@ func getUUID(model model.Model) string {
 		return t.UUID
 	case *nbdb.QoS:
 		return t.UUID
+	case *nbdb.ChassisTemplateVar:
+		return t.UUID
 	default:
 		panic(fmt.Sprintf("getUUID: unknown model %T", t))
 	}
@@ -116,6 +118,8 @@ func setUUID(model model.Model, uuid string) {
 	case *sbdb.SBGlobal:
 		t.UUID = uuid
 	case *nbdb.QoS:
+		t.UUID = uuid
+	case *nbdb.ChassisTemplateVar:
 		t.UUID = uuid
 	default:
 		panic(fmt.Sprintf("setUUID: unknown model %T", t))
@@ -234,6 +238,11 @@ func copyIndexes(model model.Model) model.Model {
 		return &nbdb.QoS{
 			UUID: t.UUID,
 		}
+	case *nbdb.ChassisTemplateVar:
+		return &nbdb.ChassisTemplateVar{
+			UUID:    t.UUID,
+			Chassis: t.Chassis,
+		}
 	default:
 		panic(fmt.Sprintf("copyIndexes: unknown model %T", t))
 	}
@@ -287,6 +296,8 @@ func getListFromModel(model model.Model) interface{} {
 		return &[]*sbdb.MACBinding{}
 	case *nbdb.QoS:
 		return &[]nbdb.QoS{}
+	case *nbdb.ChassisTemplateVar:
+		return &[]*nbdb.ChassisTemplateVar{}
 	default:
 		panic(fmt.Sprintf("getModelList: unknown model %T", t))
 	}

commit 2a89deff5f4deed9145ccf6499194d290177b0e2

services: Don't try to list/cleanup templates when OVN doesn't support that.
Spotted in upstream ovn-org/ovn CI when running against ovn versions
<=22.09 which don't support component templates:

https://github.com/ovn-org/ovn/actions/runs/4628617882

Reported error:
  failed to sync chassis: error: failed to get template var list: error:
  Wrong parameter type (*nbdb.ChassisTemplateVar): Model not found in
  Database Model

Fixes: 4b3475abb4a2 ("services: Use OVN template load balancers for NodePort services.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

go-controller/pkg/ovn/controller/services/repair.go
@@ -50,7 +50,7 @@ func newRepair(serviceLister corelisters.ServiceLister, nbClient libovsdbclient.
 }
 
 // runBeforeSync performs some cleanup of stale LBs and other miscellaneous setup.
-func (r *repair) runBeforeSync() {
+func (r *repair) runBeforeSync(useTemplates bool) {
 	// no need to lock, single-threaded.
 
 	startTime := time.Now()
@@ -68,9 +68,15 @@ func (r *repair) runBeforeSync() {
 	}
 
 	// Find all templates.
-	allTemplates, err := listSvcTemplates(r.nbClient)
-	if err != nil {
-		klog.Errorf("Unable to get templates for repair: %v", err)
+	allTemplates := TemplateMap{}
+
+	if useTemplates {
+		var err error
+
+		allTemplates, err = listSvcTemplates(r.nbClient)
+		if err != nil {
+			klog.Errorf("Unable to get templates for repair: %v", err)
+		}
 	}
 
 	// Find all load-balancers associated with Services

go-controller/pkg/ovn/controller/services/services_controller.go
@@ -197,7 +197,7 @@ func (c *Controller) Run(workers int, stopCh <-chan struct{}, runRepair, useLBGr
 		// Run the repair controller only once
 		// it keeps in sync Kubernetes and OVN
 		// and handles removal of stale data on upgrades
-		c.repair.runBeforeSync()
+		c.repair.runBeforeSync(c.useTemplates)
 	}
 
 	if err := c.initTopLevelCache(); err != nil {
@@ -272,9 +272,13 @@ func (c *Controller) initTopLevelCache() error {
 	defer c.alreadyAppliedRWLock.Unlock()
 
 	// First list all the templates.
-	allTemplates, err := listSvcTemplates(c.nbClient)
-	if err != nil {
-		return fmt.Errorf("failed to load templates: %w", err)
+	allTemplates := TemplateMap{}
+
+	if c.useTemplates {
+		allTemplates, err = listSvcTemplates(c.nbClient)
+		if err != nil {
+			return fmt.Errorf("failed to load templates: %w", err)
+		}
 	}
 
 	// Then list all load balancers and their respective services.

go-controller/pkg/ovn/master.go
@@ -662,9 +662,13 @@ func (oc *DefaultNetworkController) syncChassis(nodes []*kapi.Node) error {
 		return fmt.Errorf("failed to get chassis private list: %v", err)
 	}
 
-	templateVarList, err := libovsdbops.ListTemplateVar(oc.nbClient)
-	if err != nil {
-		return fmt.Errorf("failed to get template var list: error: %v", err)
+	templateVarList := []*nbdb.ChassisTemplateVar{}
+
+	if oc.svcTemplateSupport {
+		templateVarList, err = libovsdbops.ListTemplateVar(oc.nbClient)
+		if err != nil {
+			return fmt.Errorf("failed to get template var list: error: %w", err)
+		}
 	}
 
 	chassisHostNameMap := map[string]*sbdb.Chassis{}