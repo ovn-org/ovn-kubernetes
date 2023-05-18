dcd500354c3ed7a11a5ef36dbc014eead7a06291
Surya Seetharaman <suryaseetharaman.9@gmail.com>
2023-05-18 14:20:14 +0200

    Update OCPHACKs to use nodeipt pkg
    Due to the changes made in https://github.com/ovn-org/ovn-kubernetes/pull/3389
    this is necessary
    
    Signed-off-by: Surya Seetharaman <suryaseetharaman.9@gmail.com>

--- go-controller/pkg/node/OCP_HACKS.go
+++ go-controller/pkg/node/OCP_HACKS.go
@@ -6,40 +6,40 @@ package node
 import (
 	"fmt"
 
-	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
-
 	"github.com/coreos/go-iptables/iptables"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
+	nodeipt "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/node/iptables"
 )
 
 // Block MCS Access. https://github.com/openshift/ovn-kubernetes/pull/170
-func generateBlockMCSRules(rules *[]iptRule, protocol iptables.Protocol) {
-	var delRules []iptRule
+func generateBlockMCSRules(rules *[]nodeipt.Rule, protocol iptables.Protocol) {
+	var delRules []nodeipt.Rule
 
 	for _, chain := range []string{"FORWARD", "OUTPUT"} {
 		for _, port := range []string{"22623", "22624"} {
-			*rules = append(*rules, iptRule{
-				table:    "filter",
-				chain:    chain,
-				args:     []string{"-p", "tcp", "-m", "tcp", "--dport", port, "--syn", "-j", "REJECT"},
-				protocol: protocol,
+			*rules = append(*rules, nodeipt.Rule{
+				Table:    "filter",
+				Chain:    chain,
+				Args:     []string{"-p", "tcp", "-m", "tcp", "--dport", port, "--syn", "-j", "REJECT"},
+				Protocol: protocol,
 			})
 			// Delete the old "--syn"-less rules on upgrade
-			delRules = append(delRules, iptRule{
-				table:    "filter",
-				chain:    chain,
-				args:     []string{"-p", "tcp", "-m", "tcp", "--dport", port, "-j", "REJECT"},
-				protocol: protocol,
+			delRules = append(delRules, nodeipt.Rule{
+				Table:    "filter",
+				Chain:    chain,
+				Args:     []string{"-p", "tcp", "-m", "tcp", "--dport", port, "-j", "REJECT"},
+				Protocol: protocol,
 			})
 		}
 	}
 
-	_ = delIptRules(delRules)
+	_ = nodeipt.DelRules(delRules)
 }
 
 // insertMCSBlockIptRules inserts iptables rules to block local Machine Config Service
 // ports. See https://github.com/openshift/ovn-kubernetes/pull/170
 func insertMCSBlockIptRules() error {
-	rules := []iptRule{}
+	rules := []nodeipt.Rule{}
 	if config.IPv4Mode {
 		generateBlockMCSRules(&rules, iptables.ProtocolIPv4)
 	}
