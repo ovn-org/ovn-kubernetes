d01fa2ab55aaf0c718bf2e0f9e5ee2c6ae431f69
Enrique Llorente <ellorent@redhat.com>
2023-06-05 08:32:12 +0200

    go, lint: Pin CI go and lint to project's
    Project golang and linter version was changed, this commit change CI to read
    those from project.
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- go-controller/hack/lint.sh
+++ go-controller/hack/lint.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-VERSION=v1.52.2
+source .golangci.env
+
 if [ "$#" -ne 1 ]; then
     echo "Expected command line argument - container runtime (docker/podman) got $# arguments: $@"
     exit 1
 fi
 
-$1 run --security-opt label=disable --rm -v $(pwd):/app -w /app -e GO111MODULE=on golangci/golangci-lint:${VERSION} \
+mkdir -p ~/.cache/golangci-lint/$GOLANGCI_LINT_VERSION
+
+$1 run --security-opt label=disable --rm -v $(pwd):/app -v ~/.cache/golangci-lint/$GOLANGCI_LINT_VERSION:/root/.cache -w /app -e GO111MODULE=on golangci/golangci-lint:${GOLANGCI_LINT_VERSION} \
 	golangci-lint run --verbose --print-resources-usage \
 	--modules-download-mode=vendor --timeout=15m0s && \
 	echo "lint OK!"
