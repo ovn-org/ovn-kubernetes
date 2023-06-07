1a0954af1f9b5391bc64b2af38cfae3125d1c087
Surya Seetharaman <suryaseetharaman.9@gmail.com>
2023-06-07 09:18:07 +0200

    Install CRDs on the KIND cluster
    This PR adds the plumbing required to curl the CRDs
    from a released upstream version of ANP and BANP.
    It also installs these into the KIND cluster.
    We also provide the necessary permissions that are
    required to access the new CRDs in ovn-setup.yaml.j2.
    
    NOTE: We curl the CRDs into the dist/templates
    directory and maintain a copy of the version OVNK
    uses so that it is consistent with what the other
    features do. It will also help us see the version
    OVNK is using versus the upstream version
    
    Signed-off-by: Surya Seetharaman <suryaseetharaman.9@gmail.com>

--- contrib/kind.sh
+++ contrib/kind.sh
@@ -955,6 +955,8 @@ install_ovn() {
   run_kubectl apply -f k8s.ovn.org_egressqoses.yaml
   run_kubectl apply -f k8s.ovn.org_egressservices.yaml
   run_kubectl apply -f k8s.ovn.org_adminpolicybasedexternalroutes.yaml
+  run_kubectl apply -f policy.networking.k8s.io_adminnetworkpolicies.yaml
+  run_kubectl apply -f policy.networking.k8s.io_baselineadminnetworkpolicies.yaml
   run_kubectl apply -f ovn-setup.yaml
   MASTER_NODES=$(kind get nodes --name "${KIND_CLUSTER_NAME}" | sort | head -n "${KIND_NUM_MASTER}")
   # We want OVN HA not Kubernetes HA
--- go-controller/hack/update-codegen.sh
+++ go-controller/hack/update-codegen.sh
@@ -81,3 +81,8 @@ echo "Copying egressIP CRD"
 cp _output/crds/k8s.ovn.org_egressips.yaml ../dist/templates/k8s.ovn.org_egressips.yaml.j2
 echo "Copying egressQoS CRD"
 cp _output/crds/k8s.ovn.org_egressqoses.yaml ../dist/templates/k8s.ovn.org_egressqoses.yaml.j2
+# NOTE: When you update vendoring versions for the ANP & BANP APIs, we must update the version of the CRD we pull from in the below URL
+echo "Copying Admin Network Policy CRD"
+curl -sSL https://raw.githubusercontent.com/kubernetes-sigs/network-policy-api/v0.1.0/config/crd/policy.networking.k8s.io_adminnetworkpolicies.yaml -o ../dist/templates/policy.networking.k8s.io_adminnetworkpolicies.yaml
+echo "Copying Baseline Admin Network Policy CRD"
+curl -sSL https://raw.githubusercontent.com/kubernetes-sigs/network-policy-api/v0.1.0/config/crd/policy.networking.k8s.io_baselineadminnetworkpolicies.yaml -o ../dist/templates/policy.networking.k8s.io_baselineadminnetworkpolicies.yaml
