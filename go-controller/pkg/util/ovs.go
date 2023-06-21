3c6a56c9450177aad6afdcfaaa11fd36dfef7f8f
Nadia Pinaeva <npinaeva@redhat.com>
2023-06-21 09:21:58 +0200

    delete dead code from util/ovs, init() had impact on performance without being used by anyone
    Signed-off-by: Nadia Pinaeva <npinaeva@redhat.com>

--- go-controller/pkg/util/ovs.go
+++ go-controller/pkg/util/ovs.go
@@ -4,14 +4,11 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"os/exec"
 	"regexp"
 	"runtime"
 	"strings"
 	"sync/atomic"
 	"time"
-
-	"github.com/pkg/errors"
 
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
@@ -46,8 +43,6 @@ const (
 	rhel               = "RHEL"
 	ubuntu             = "Ubuntu"
 	windowsOS          = "windows"
-	defaultOSMaxArgs   = 262144
-	minOSArgs          = 1000
 )
 
 const (
@@ -71,8 +66,6 @@ var (
 var ovnCmdRetryCount = 200
 var AppFs = afero.NewOsFs()
 
-var MaxArgsError = errors.New("requested transaction exceeds maximum arguments")
-
 // PrepareTestConfig restores default config values. Used by testcases to
 // provide a pristine environment between tests.
 func PrepareTestConfig() {
@@ -83,36 +76,6 @@ func PrepareTestConfig() {
 // this metric is set only for the ovnkube in master mode since 99.9% of
 // all the ovn-nbctl/ovn-sbctl calls occur on the master
 var MetricOvnCliLatency *prometheus.HistogramVec
-
-var maxArgs int
-
-func init() {
-	maxArgs = findMaxArgsUsable(defaultOSMaxArgs)
-	klog.Infof("Maximum command line arguments set to: %d", maxArgs)
-}
-
-// findMaxArgsUsable finds the maximum amount of usable args on the system, which may be
-// different than what the kernel returns for ARG_MAX
-func findMaxArgsUsable(estimatedMax int) int {
-	backoff := .9
-	args := make([]string, estimatedMax)
-	for i := range args {
-		args[i] = "a"
-	}
-
-	for estimatedMax > minOSArgs {
-		if _, err := exec.Command("/bin/true", args...).Output(); err == nil {
-			break
-		}
-		estimatedMax = int(float64(estimatedMax) * backoff)
-		args = args[:estimatedMax]
-	}
-
-	if estimatedMax < minOSArgs {
-		estimatedMax = minOSArgs
-	}
-	return estimatedMax
-}
 
 func runningPlatform() (string, error) {
 	if runtime.GOOS == windowsOS {
@@ -804,82 +767,6 @@ func DetectSCTPSupport() (bool, error) {
 		}
 	}
 	return false, nil
-}
-
-// NBTxn hold parts of an ovn-nbctl transaction request
-type NBTxn struct {
-	args    []string
-	txnArgs []string
-	env     []string
-}
-
-// NewNBTxn returns a new ovn-nbctl transaction request object
-func NewNBTxn() *NBTxn {
-	args, env := getNbctlArgsAndEnv(ovsCommandTimeout, []string{}...)
-	return &NBTxn{
-		args: args,
-		env:  env,
-	}
-}
-
-// Add adds a new request to the transaction
-func (t *NBTxn) add(args ...string) {
-	if len(t.txnArgs) > 0 {
-		t.txnArgs = append(t.txnArgs, "--")
-	}
-	t.txnArgs = append(t.txnArgs, args...)
-}
-
-// Commit commits all parts of the transaction and returns output and errors
-func (t *NBTxn) Commit() (string, string, error) {
-	if len(t.txnArgs) == 0 {
-		return "", "", nil
-	}
-	allArgs := append(t.args, t.txnArgs...)
-	stdout, stderr, err := runOVNretry(runner.nbctlPath, t.env, allArgs...)
-	return strings.Trim(strings.TrimSpace(stdout.String()), "\""), stderr.String(), err
-}
-
-// AddOrCommit adds a slice of requests to a transaction
-// If the incoming slice to be added would be greater than the maximum
-// number of arguments for a transaction; the transaction is committed
-// and the current transactions arguments are reset to the slice
-// Note: This method should be called once with a slice of dependent args
-// For example, using create --id=@acl with dependent add to switch cmds
-// should all be added in a single AddOrCommit call
-// The caller should take care not to overload the call with a too large
-// slice exceeding max args, or the command can never be committed
-func (t *NBTxn) AddOrCommit(args []string) (string, string, error) {
-	if len(args) > maxArgs {
-		return "", "", MaxArgsError
-	}
-	// assume a 10 argument buffer for other arguments by default added to the nbctl call
-	buffer := 10
-	incomingLength := len(args)
-	if len(t.txnArgs) > 0 {
-		// increment for --
-		incomingLength += 1
-	}
-
-	klog.V(5).Infof("Number of args: %d, txnArgs: %d, incomingLen: %d, buffer: %d", len(t.args),
-		len(t.txnArgs), incomingLength, buffer)
-
-	// case where we are going to exceed max arguments
-	// also check entire line length is going be over 100k
-	// maximum bash command seems to be a combination of max args and length of each argument
-	// maximum length is PAGE_SIZE * 32 which we can assume to be 4k page, and equals 131072
-	if len(t.args)+len(t.txnArgs)+incomingLength+buffer > maxArgs || len(strings.Join(t.args, " "))+
-		len(strings.Join(t.txnArgs, " "))+len(strings.Join(args, " ")) > 100000 {
-		klog.Info("Requested transaction add is too large, committing...")
-		if stdout, stderr, err := t.Commit(); err != nil {
-			return stdout, stderr, err
-		}
-		// reset txnArgs
-		t.txnArgs = []string{}
-	}
-
-	t.add(args...)
-	return "", "", nil
 }
 
 // DetectCheckPktLengthSupport checks if OVN supports check packet length action in OVS kernel datapath
