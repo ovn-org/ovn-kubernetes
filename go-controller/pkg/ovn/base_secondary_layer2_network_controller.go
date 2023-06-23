9fbc90948c98a6590a43cec6333632702fcc19fa
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2023-06-23 09:33:39 +0000

    Rename base layer2 event handler
    We will introduce a specific layer2 event handler and the current name
    for the base layer 2 event handler (shared between layer2 and localnet
    controllers) would collide, so rename it.
    
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

--- go-controller/pkg/ovn/base_secondary_layer2_network_controller.go
+++ go-controller/pkg/ovn/base_secondary_layer2_network_controller.go
@@ -23,7 +23,7 @@ import (
 
 // method/structure shared by all layer 2 network controller, including localnet and layer2 network controllres.
 
-type secondaryLayer2NetworkControllerEventHandler struct {
+type baseSecondaryLayer2NetworkControllerEventHandler struct {
 	baseHandler  baseNetworkControllerEventHandler
 	watchFactory *factory.WatchFactory
 	objType      reflect.Type
@@ -35,24 +35,24 @@ type secondaryLayer2NetworkControllerEventHandler struct {
 // type considers them equal and therefore no update is needed. It returns false when the two objects are not considered
 // equal and an update needs be executed. This is regardless of how the update is carried out (whether with a dedicated update
 // function or with a delete on the old obj followed by an add on the new obj).
-func (h *secondaryLayer2NetworkControllerEventHandler) AreResourcesEqual(obj1, obj2 interface{}) (bool, error) {
+func (h *baseSecondaryLayer2NetworkControllerEventHandler) AreResourcesEqual(obj1, obj2 interface{}) (bool, error) {
 	return h.baseHandler.areResourcesEqual(h.objType, obj1, obj2)
 }
 
 // GetInternalCacheEntry returns the internal cache entry for this object, given an object and its type.
 // This is now used only for pods, which will get their the logical port cache entry.
-func (h *secondaryLayer2NetworkControllerEventHandler) GetInternalCacheEntry(obj interface{}) interface{} {
+func (h *baseSecondaryLayer2NetworkControllerEventHandler) GetInternalCacheEntry(obj interface{}) interface{} {
 	return h.oc.GetInternalCacheEntryForSecondaryNetwork(h.objType, obj)
 }
 
 // GetResourceFromInformerCache returns the latest state of the object, given an object key and its type.
 // from the informers cache.
-func (h *secondaryLayer2NetworkControllerEventHandler) GetResourceFromInformerCache(key string) (interface{}, error) {
+func (h *baseSecondaryLayer2NetworkControllerEventHandler) GetResourceFromInformerCache(key string) (interface{}, error) {
 	return h.baseHandler.getResourceFromInformerCache(h.objType, h.watchFactory, key)
 }
 
 // RecordAddEvent records the add event on this given object.
-func (h *secondaryLayer2NetworkControllerEventHandler) RecordAddEvent(obj interface{}) {
+func (h *baseSecondaryLayer2NetworkControllerEventHandler) RecordAddEvent(obj interface{}) {
 	switch h.objType {
 	case factory.MultiNetworkPolicyType:
 		mnp := obj.(*mnpapi.MultiNetworkPolicy)
@@ -62,34 +62,34 @@ func (h *secondaryLayer2NetworkControllerEventHandler) RecordAddEvent(obj interf
 }
 
 // RecordUpdateEvent records the udpate event on this given object.
-func (h *secondaryLayer2NetworkControllerEventHandler) RecordUpdateEvent(obj interface{}) {
+func (h *baseSecondaryLayer2NetworkControllerEventHandler) RecordUpdateEvent(obj interface{}) {
 	h.baseHandler.recordAddEvent(h.objType, obj)
 }
 
 // RecordDeleteEvent records the delete event on this given object.
-func (h *secondaryLayer2NetworkControllerEventHandler) RecordDeleteEvent(obj interface{}) {
+func (h *baseSecondaryLayer2NetworkControllerEventHandler) RecordDeleteEvent(obj interface{}) {
 	h.baseHandler.recordAddEvent(h.objType, obj)
 }
 
 // RecordSuccessEvent records the success event on this given object.
-func (h *secondaryLayer2NetworkControllerEventHandler) RecordSuccessEvent(obj interface{}) {
+func (h *baseSecondaryLayer2NetworkControllerEventHandler) RecordSuccessEvent(obj interface{}) {
 	h.baseHandler.recordAddEvent(h.objType, obj)
 }
 
 // RecordErrorEvent records the error event on this given object.
-func (h *secondaryLayer2NetworkControllerEventHandler) RecordErrorEvent(obj interface{}, reason string, err error) {
+func (h *baseSecondaryLayer2NetworkControllerEventHandler) RecordErrorEvent(obj interface{}, reason string, err error) {
 }
 
 // IsResourceScheduled returns true if the given object has been scheduled.
 // Only applied to pods for now. Returns true for all other types.
-func (h *secondaryLayer2NetworkControllerEventHandler) IsResourceScheduled(obj interface{}) bool {
+func (h *baseSecondaryLayer2NetworkControllerEventHandler) IsResourceScheduled(obj interface{}) bool {
 	return h.baseHandler.isResourceScheduled(h.objType, obj)
 }
 
 // AddResource adds the specified object to the cluster according to its type and returns the error,
 // if any, yielded during object creation.
 // Given an object to add and a boolean specifying if the function was executed from iterateRetryResources
-func (h *secondaryLayer2NetworkControllerEventHandler) AddResource(obj interface{}, fromRetryLoop bool) error {
+func (h *baseSecondaryLayer2NetworkControllerEventHandler) AddResource(obj interface{}, fromRetryLoop bool) error {
 	return h.oc.AddSecondaryNetworkResourceCommon(h.objType, obj)
 }
 
@@ -97,18 +97,18 @@ func (h *secondaryLayer2NetworkControllerEventHandler) AddResource(obj interface
 // type and returns the error, if any, yielded during the object update.
 // Given an old and a new object; The inRetryCache boolean argument is to indicate if the given resource
 // is in the retryCache or not.
-func (h *secondaryLayer2NetworkControllerEventHandler) UpdateResource(oldObj, newObj interface{}, inRetryCache bool) error {
+func (h *baseSecondaryLayer2NetworkControllerEventHandler) UpdateResource(oldObj, newObj interface{}, inRetryCache bool) error {
 	return h.oc.UpdateSecondaryNetworkResourceCommon(h.objType, oldObj, newObj, inRetryCache)
 }
 
 // DeleteResource deletes the object from the cluster according to the delete logic of its resource type.
 // Given an object and optionally a cachedObj; cachedObj is the internal cache entry for this object,
 // used for now for pods and network policies.
-func (h *secondaryLayer2NetworkControllerEventHandler) DeleteResource(obj, cachedObj interface{}) error {
+func (h *baseSecondaryLayer2NetworkControllerEventHandler) DeleteResource(obj, cachedObj interface{}) error {
 	return h.oc.DeleteSecondaryNetworkResourceCommon(h.objType, obj, cachedObj)
 }
 
-func (h *secondaryLayer2NetworkControllerEventHandler) SyncFunc(objs []interface{}) error {
+func (h *baseSecondaryLayer2NetworkControllerEventHandler) SyncFunc(objs []interface{}) error {
 	var syncFunc func([]interface{}) error
 
 	if h.syncFunc != nil {
@@ -137,7 +137,7 @@ func (h *secondaryLayer2NetworkControllerEventHandler) SyncFunc(objs []interface
 
 // IsObjectInTerminalState returns true if the given object is a in terminal state.
 // This is used now for pods that are either in a PodSucceeded or in a PodFailed state.
-func (h *secondaryLayer2NetworkControllerEventHandler) IsObjectInTerminalState(obj interface{}) bool {
+func (h *baseSecondaryLayer2NetworkControllerEventHandler) IsObjectInTerminalState(obj interface{}) bool {
 	return h.baseHandler.isObjectInTerminalState(h.objType, obj)
 }
 
@@ -162,7 +162,7 @@ func (oc *BaseSecondaryLayer2NetworkController) initRetryFramework() {
 // newRetryFramework builds and returns a retry framework for the input resource type;
 func (oc *BaseSecondaryLayer2NetworkController) newRetryFramework(
 	objectType reflect.Type) *retry.RetryFramework {
-	eventHandler := &secondaryLayer2NetworkControllerEventHandler{
+	eventHandler := &baseSecondaryLayer2NetworkControllerEventHandler{
 		baseHandler:  baseNetworkControllerEventHandler{},
 		objType:      objectType,
 		watchFactory: oc.watchFactory,
