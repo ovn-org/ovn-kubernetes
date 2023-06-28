ovn-org/ovn-kubernetes

commit 25d892cc9e318d19738d9519e0e20fbab28f7eae

Fixes invalid error while getting router NATs
We list all of the NATs on a router via libovsdb, then we iterate
through each NAT uuid and try to fetch it via libovsdb. During the gap
in time where the NATs are listed on a router, and when they are
individually fetched, the NAT may have already been deleted. In this
case we were breaking the loop and returning an error.

There is no guarantee that the NATs we initially fetch are the ones
currently on the router. As such we should not consider this an error.
Instead we should just return the NATs that still exist.

Also, make the error messages more clear so that if a generic libovsdb
error is propagated we know if it was from the get router or the get NAT
operations.

Signed-off-by: Tim Rozet <trozet@redhat.com>

go-controller/pkg/libovsdbops/router.go
@@ -997,16 +997,16 @@ func FindNATsWithPredicate(nbClient libovsdbclient.Client, predicate natPredicat
 // GetRouterNATs looks up NATs associated to the provided logical router from
 // the cache
 func GetRouterNATs(nbClient libovsdbclient.Client, router *nbdb.LogicalRouter) ([]*nbdb.NAT, error) {
-	router, err := GetLogicalRouter(nbClient, router)
+	r, err := GetLogicalRouter(nbClient, router)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to get router: %s, error: %w", router.Name, err)
 	}
 
 	nats := []*nbdb.NAT{}
-	for _, uuid := range router.Nat {
+	for _, uuid := range r.Nat {
 		nat, err := GetNAT(nbClient, &nbdb.NAT{UUID: uuid})
-		if err != nil {
-			return nil, err
+		if err != nil && err != libovsdbclient.ErrNotFound {
+			return nil, fmt.Errorf("failed to lookup NAT entry with uuid: %s, error: %w", uuid, err)
 		}
 		nats = append(nats, nat)
 	}

commit 1541b58332198325cc1fa4304d9627abab516474

Fixes panic with delete NAT operations
Regression introduced by:
25d892cc9e318d19738d9519e0e20fbab28f7eae

Would end up appending a nil NAT and causing an NPE when trying to
execute EquivalentNAT:

panic: runtime error: invalid memory address or nil pointer dereference
[signal SIGSEGV: segmentation violation code=0x1 addr=0x78 pc=0x178ea7b]

goroutine 140 [running]:
github.com/ovn-org/ovn-kubernetes/go-controller/pkg/libovsdbops.isEquivalentNAT(0x0, 0xc002e6cea0)
	/go/src/github.com/openshift/ovn-kubernetes/go-controller/pkg/libovsdbops/router.go:935 +0x9b
github.com/ovn-org/ovn-kubernetes/go-controller/pkg/libovsdbops.DeleteNATsOps({0x24aa068, 0xc0008c0fc0}, {0x0, 0x0, 0x0}, 0xc002e1a4e0, {0xc000130c48, 0x1, 0xc00106f6f8?})
	/go/src/github.com/openshift/ovn-kubernetes/go-controller/pkg/libovsdbops/router.go:1087 +0x728

Signed-off-by: Tim Rozet <trozet@redhat.com>

go-controller/pkg/libovsdbops/router.go
@@ -1005,7 +1005,10 @@ func GetRouterNATs(nbClient libovsdbclient.Client, router *nbdb.LogicalRouter) (
 	nats := []*nbdb.NAT{}
 	for _, uuid := range r.Nat {
 		nat, err := GetNAT(nbClient, &nbdb.NAT{UUID: uuid})
-		if err != nil && err != libovsdbclient.ErrNotFound {
+		if err == libovsdbclient.ErrNotFound {
+			continue
+		}
+		if err != nil {
 			return nil, fmt.Errorf("failed to lookup NAT entry with uuid: %s, error: %w", uuid, err)
 		}
 		nats = append(nats, nat)