a9d1995e0d881e07e78758ccdd18f4ae0d8b774b
Patryk Diak <pdiak@redhat.com>
2023-08-03 14:54:39 +0200

    Refactor RBAC
    This commit refactors the RBAC rules and serviceAccount ovn-kubernetes uses in KIND deployments.
    Instead of using one ServiceAccount with one ClusterRole break it down per component.
    This makes it easier to track which permissions are required by which piece
    and avoid the mess of granting high privilages to everyone.
    
    Signed-off-by: Patryk Diak <pdiak@redhat.com>

--- contrib/kind.sh
+++ contrib/kind.sh
@@ -966,6 +966,10 @@ install_ovn() {
   run_kubectl apply -f policy.networking.k8s.io_adminnetworkpolicies.yaml
   run_kubectl apply -f policy.networking.k8s.io_baselineadminnetworkpolicies.yaml
   run_kubectl apply -f ovn-setup.yaml
+  run_kubectl apply -f rbac-ovnkube-cluster-manager.yaml
+  run_kubectl apply -f rbac-ovnkube-master.yaml
+  run_kubectl apply -f rbac-ovnkube-node.yaml
+  run_kubectl apply -f rbac-ovnkube-db.yaml
   MASTER_NODES=$(kind get nodes --name "${KIND_CLUSTER_NAME}" | sort | head -n "${KIND_NUM_MASTER}")
   # We want OVN HA not Kubernetes HA
   # leverage the kubeadm well-known label node-role.kubernetes.io/control-plane=
