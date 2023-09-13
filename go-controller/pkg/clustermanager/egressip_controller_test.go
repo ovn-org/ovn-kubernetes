1756511bc6cd68b3a1f9b9a871ba536c7b6c3d57
Martin Kennelly <mkennell@redhat.com>
2023-09-13 10:49:31 +0100

    EIP test: fix incorrect node name passed to allocator
    Signed-off-by: Martin Kennelly <mkennell@redhat.com>

--- go-controller/pkg/clustermanager/egressip_controller_test.go
+++ go-controller/pkg/clustermanager/egressip_controller_test.go
@@ -3852,7 +3852,7 @@ var _ = ginkgo.Describe("OVN cluster-manager EgressIP Operations", func() {
 				)
 
 				fakeClusterManagerOVN.eIPC.allocator.cache[egressNode1.name] = &egressNode1
-				fakeClusterManagerOVN.eIPC.allocator.cache[egressNode1.name] = &egressNode2
+				fakeClusterManagerOVN.eIPC.allocator.cache[egressNode2.name] = &egressNode2
 
 				_, err := fakeClusterManagerOVN.eIPC.WatchEgressIP()
 				gomega.Expect(err).NotTo(gomega.HaveOccurred())
