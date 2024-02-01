a7aa34d95fd72fd93890e5a0170d894917c13975
Enrique Llorente <ellorent@redhat.com>
2024-02-01 06:25:07 +0100

    kubevirt, e2e: Use simple tcp server
    Using the "Reuse" golang http client connection attribute to check that
    the connection is not broken is a little hard to debug, this change
    replace the http client and http agnhost server with a simple golang TCP
    server injected using fedora coreos ignition, its simpler and test is
    faster.
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

---
+++ test/e2e/kubevirt/echoserver/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	bind_address := fmt.Sprintf(":%s", os.Args[1])
+
+	listener, err := net.Listen("tcp", bind_address)
+	if err != nil {
+		panic(fmt.Errorf("failed listening to %s: %w", bind_address, err))
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(fmt.Errorf("failed accepting connection: %w", err))
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	log.Printf("Handling connection %s\n", conn.RemoteAddr())
+	defer func() {
+		log.Printf("Closing connection %s\n", conn.RemoteAddr())
+		conn.Close()
+	}()
+	for {
+		receivedMsg := make([]byte, 1024)
+		_, err := conn.Read(receivedMsg)
+		if err != nil {
+			if err != nil {
+				if err != io.EOF {
+					log.Printf("EOF")
+				} else {
+					log.Printf("failed reading data: %v\n", err)
+				}
+				return
+			}
+		}
+		conn.Write(receivedMsg)
+	}
+}
