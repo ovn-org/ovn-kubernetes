6a570d541e4ceedb82633870826a47aec9d25ebc
Flavio Fernandes <ffernandes@nvidia.com>
2024-03-12 19:19:29 +0000

    wip
    Signed-off-by: Flavio Fernandes <ffernandes@nvidia.com>

--- contrib/kind-helm.sh
+++ contrib/kind-helm.sh
@@ -6,30 +6,53 @@ export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && p
 
 set_default_params() {
   # Set default values
+  export OVN_HA=${OVN_HA:-false}
+  export KIND_NUM_WORKER=${KIND_NUM_WORKER:-1}
   export KIND_CLUSTER_NAME=${KIND_CLUSTER_NAME:-ovn}
+  export OVN_IMAGE=${OVN_IMAGE:-'ghcr.io/ovn-org/ovn-kubernetes/ovn-kube-u:helm'}
+
   # Setup KUBECONFIG patch based on cluster-name
   export KUBECONFIG=${KUBECONFIG:-${HOME}/${KIND_CLUSTER_NAME}.conf}
 }
 
 usage() {
     echo "usage: kind-helm.sh [--delete]"
+    echo "       [ -ha | --ha-enabled ]"
+    echo "       [ -wk | --num-workers <num> ]"
     echo "       [ -cn | --cluster-name ]"
     echo "       [ -h ]"
     echo ""
+    echo "--delete                            Delete current cluster"
+    echo "-ha  | --ha-enabled                 Enable high availability. DEFAULT: HA Disabled."
+    echo "-wk  | --num-workers                Number of worker nodes. DEFAULT: 1 worker"
+    echo "-cn  | --cluster-name               Configure the kind cluster's name"
+    echo "                                    nodes and no HA - 0 worker nodes."
+
 }
 
 parse_args() {
     while [ "$1" != "" ]; do
         case $1 in
-           --delete )                          delete
-                                               exit
-                                               ;;
-            -cn | --cluster-name )             shift
-                                               KIND_CLUSTER_NAME=$1
-                                               set_default_params
-                                               ;;
-            * )                                usage
-                                               exit 1
+            --delete )                          delete
+                                                exit
+                                                ;;
+            -ha | --ha-enabled )                OVN_HA=true
+                                                ;;
+            -wk | --num-workers )               shift
+                                                if ! [[ "$1" =~ ^[0-9]+$ ]]; then
+                                                    echo "Invalid num-workers: $1"
+                                                    usage
+                                                    exit 1
+                                                fi
+                                                KIND_NUM_WORKER=$1
+                                                ;;
+            -cn | --cluster-name )              shift
+                                                KIND_CLUSTER_NAME=$1
+                                                # Setup KUBECONFIG
+                                                set_default_params
+                                                ;;
+            * )                                 usage
+                                                exit 1
         esac
         shift
     done
@@ -72,7 +95,7 @@ docker_disable_ipv6() {
   # internal bridge has IPv6 disable and can't move the IPv6 from the eth0 interface.
   # We can enable IPv6 always in the container, since the docker setup with IPv4 only
   # is not very common. 
-  KIND_NODES=$(kind get nodes --name "${KIND_CLUSTER_NAME}")
+  KIND_NODES=$(kind get nodes --name "${KIND_CLUSTER_NAME}" | grep -v external-load-balancer)
   for n in $KIND_NODES; do
     docker exec "$n" sysctl --ignore net.ipv6.conf.all.disable_ipv6=0
     docker exec "$n" sysctl --ignore net.ipv6.conf.all.forwarding=1
@@ -80,7 +103,10 @@ docker_disable_ipv6() {
 }
 
 build_ovn_image() {
-    OVN_IMAGE="ghcr.io/ovn-org/ovn-kubernetes/ovn-kube-u:helm"
+    if [ "${SKIP_OVN_IMAGE_REBUILD}" == "true" ]; then
+      echo "Explicitly instructed not to rebuild ovn image: ${OVN_IMAGE}"
+      return
+    fi
 
     # Build ovn image
     pushd ${SCRIPT_DIR}/../go-controller
@@ -94,6 +120,18 @@ build_ovn_image() {
     echo "ref: $(git rev-parse  --symbolic-full-name HEAD)  commit: $(git rev-parse  HEAD)" > git_info
     docker build -t "${OVN_IMAGE}" -f Dockerfile.fedora .
     popd
+}
+
+get_image() {
+    local image_and_tag="${1:-$OVN_IMAGE}"  # Use $1 if provided, otherwise use $OVN_IMAGE
+    local image="${image_and_tag%%:*}"  # Extract everything before the first colon
+    echo "$image"
+}
+
+get_tag() {
+    local image_and_tag="${1:-$OVN_IMAGE}"  # Use $1 if provided, otherwise use $OVN_IMAGE
+    local tag="${image_and_tag##*:}"  # Extract everything after the last colon
+    echo "$tag"
 }
 
 coredns_patch() {
@@ -131,36 +169,62 @@ coredns_patch() {
 }
 
 create_kind_cluster() {
-    # create a cluster, with 1 controller and 1 worker node
+# Fetch the number of worker nodes and HA status from environment variables
+    kind_num_worker="${KIND_NUM_WORKER:-1}"  # Default to 1 if not set
+    ovn_ha="${OVN_HA:-false}"  # Default to false if not set
+
+    # Start of the kind configuration
     kind_cluster_name=ovn-helm
     cat <<EOT > /tmp/kind.yaml
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
-- role: control-plane
-- role: worker
-- role: worker
-- role: worker
+EOT
+
+    # Add control-plane nodes based on OVN_HA status
+    echo "- role: control-plane" >> /tmp/kind.yaml  # Add a single control-plane node if not HA
+    if [ "$ovn_ha" = true ]; then
+        for i in {2..3}; do  # Add 3 control-plane nodes for HA
+            echo "- role: control-plane" >> /tmp/kind.yaml
+        done
+    fi
+
+    # Add worker nodes based on KIND_NUM_WORKER
+    for i in $(seq 1 $kind_num_worker); do
+        echo "- role: worker" >> /tmp/kind.yaml
+    done
+
+    # Add networking configuration
+    cat <<EOT >> /tmp/kind.yaml
 networking:
   disableDefaultCNI: true
   kubeProxyMode: none
 EOT
+
     kind delete clusters $KIND_CLUSTER_NAME ||:
     kind create cluster --name $KIND_CLUSTER_NAME --config /tmp/kind.yaml
-    kind load docker-image --name $KIND_CLUSTER_NAME ghcr.io/ovn-org/ovn-kubernetes/ovn-kube-u:helm
+    kind load docker-image --name $KIND_CLUSTER_NAME $OVN_IMAGE
+
+    # When using HA, label controller nodes to host db
+    [ "${OVN_HA}" = "false" ] || \
+      kubectl label nodes k8s.ovn.org/ovnkube-db=true --overwrite \
+        -l node-role.kubernetes.io/control-plane
 }
 
 create_ovn_kubernetes() {
     cd ${SCRIPT_DIR}/../helm/ovn-kubernetes
+
     helm install ovn-kubernetes . -f values.yaml \
         --set k8sAPIServer="https://$(kubectl get pods -n kube-system -l component=kube-apiserver -o jsonpath='{.items[0].status.hostIP}'):6443" \
         --set ovnkube-identity.replicas=$(kubectl get node -l node-role.kubernetes.io/control-plane --no-headers | wc -l) \
-        --set global.image.repository=ghcr.io/ovn-org/ovn-kubernetes/ovn-kube-u --set global.image.tag=helm
+        --set global.image.repository=$(get_image) \
+        --set global.image.tag=$(get_tag) \
+        --set tags.ovnkube-db-raft=$(if [ "${OVN_HA}" = "true" ]; then echo "true"; else echo "false"; fi) \
+        --set tags.ovnkube-db=$(if [ "${OVN_HA}" = "false" ]; then echo "true"; else echo "false"; fi)
 }
 
 delete() {
-  helm uninstall ovn-kubernetes
-  sleep 5
+  helm uninstall ovn-kubernetes && sleep 5 ||:
   kind delete cluster --name "${KIND_CLUSTER_NAME:-ovn}"
 }
 
