a51d5bfd66b6ebe3cb0967576740b20e7316a021
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2024-03-13 10:42:55 +0000

    Copy health check functions to its own package
    Health check is used both by egress ip and egress services controllers
    and could potentially be used for more stuff in the future. It makes
    sense for the code to live in its own package.
    
    It's a copy and not a move due to the way egress service mocks those
    functions on their tests. This will be cleaned up on a later commit.
    
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

---
+++ go-controller/pkg/clustermanager/healthcheck/healthcheck.go
@@ -0,0 +1,101 @@
+package healthcheck
+
+import (
+	"context"
+	"net"
+	"os"
+	"syscall"
+	"time"
+
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/ovn/healthcheck"
+	"k8s.io/klog/v2"
+)
+
+// IsReachable checks the reachability of a node through the health check
+// service if its port is provided (!=0) or through the well-known discard
+// service. A 0 timeout inhibits the check.
+func IsReachable(nodeName string, mgmtIPs []net.IP, healthClient healthcheck.EgressIPHealthClient, port, timeout int) bool {
+	// Check if we need to do node reachability check
+	if timeout == 0 {
+		return true
+	}
+	if port == 0 {
+		return isReachableLegacy(nodeName, mgmtIPs, timeout)
+	}
+	return isReachableViaGRPC(mgmtIPs, healthClient, port, timeout)
+}
+
+func isReachableViaGRPC(mgmtIPs []net.IP, client healthcheck.EgressIPHealthClient, port, timeout int) bool {
+	dialCtx, dialCancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+	defer dialCancel()
+
+	if !client.IsConnected() {
+		// gRPC session is not up. Attempt to connect and if that suceeds, we will declare node as reacheable.
+		return client.Connect(dialCtx, mgmtIPs, port)
+	}
+
+	// gRPC session is already established. Send a probe, which will succeed, or close the session.
+	return client.Probe(dialCtx)
+}
+
+func isReachableLegacy(node string, mgmtIPs []net.IP, totalTimeout int) bool {
+	var retryTimeOut, initialRetryTimeOut time.Duration
+
+	numMgmtIPs := len(mgmtIPs)
+	if numMgmtIPs == 0 {
+		return false
+	}
+
+	switch totalTimeout {
+	// Check if we need to do node reachability check
+	case 0:
+		return true
+	case 1:
+		// Using time duration for initial retry with 700/numIPs msec and retry of 100/numIPs msec
+		// to ensure total wait time will be in range with the configured value including a sleep of 100msec between attempts.
+		initialRetryTimeOut = time.Duration(700/numMgmtIPs) * time.Millisecond
+		retryTimeOut = time.Duration(100/numMgmtIPs) * time.Millisecond
+	default:
+		// Using time duration for initial retry with 900/numIPs msec
+		// to ensure total wait time will be in range with the configured value including a sleep of 100msec between attempts.
+		initialRetryTimeOut = time.Duration(900/numMgmtIPs) * time.Millisecond
+		retryTimeOut = initialRetryTimeOut
+	}
+
+	timeout := initialRetryTimeOut
+	endTime := time.Now().Add(time.Second * time.Duration(totalTimeout))
+	for time.Now().Before(endTime) {
+		for _, ip := range mgmtIPs {
+			if dialDiscardService(ip, timeout) {
+				return true
+			}
+		}
+		time.Sleep(100 * time.Millisecond)
+		timeout = retryTimeOut
+	}
+	klog.Errorf("Failed reachability check for %s", node)
+	return false
+}
+
+// Blantant copy from: https://github.com/openshift/sdn/blob/master/pkg/network/common/egressip.go#L499-L505
+// Ping a node and return whether or not we think it is online. We do this by trying to
+// open a TCP connection to the "discard" service (port 9); if the node is offline, the
+// attempt will either time out with no response, or else return "no route to host" (and
+// we will return false). If the node is online then we presumably will get a "connection
+// refused" error; but the code below assumes that anything other than timeout or "no
+// route" indicates that the node is online.
+func dialDiscardService(ip net.IP, timeout time.Duration) bool {
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(ip.String(), "9"), timeout)
+	if conn != nil {
+		conn.Close()
+	}
+	if opErr, ok := err.(*net.OpError); ok {
+		if opErr.Timeout() {
+			return false
+		}
+		if sysErr, ok := opErr.Err.(*os.SyscallError); ok && sysErr.Err == syscall.EHOSTUNREACH {
+			return false
+		}
+	}
+	return true
+}
