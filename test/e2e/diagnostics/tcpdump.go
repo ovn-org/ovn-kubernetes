1b1a6bbab744fb36d712acce3bbd644de4329907
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2024-04-18 09:05:39 +0000

    Fix e2e diagnostics tcpdump daemonset name
    To avoid invalid character `_`.
    
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

--- test/e2e/diagnostics/tcpdump.go
+++ test/e2e/diagnostics/tcpdump.go
@@ -2,6 +2,7 @@ package diagnostics
 
 import (
 	"fmt"
+	"strings"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -16,7 +17,7 @@ func (d *Diagnostics) TCPDumpDaemonSet(ifaces []string, expression string) {
 	By("Creating tcpdump daemonsets")
 	daemonSets := []appsv1.DaemonSet{}
 	for _, iface := range ifaces {
-		daemonSetName := "node-tcpdump-" + iface
+		daemonSetName := "node-tcpdump-" + strings.ReplaceAll(iface, "_", "-")
 		cmd := fmt.Sprintf("tcpdump -vvv -nne -i %s %s", iface, expression)
 		daemonSets = append(daemonSets, d.composeDiagnosticsDaemonSet(daemonSetName, cmd, "node-tcpdump"))
 	}
