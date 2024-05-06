d2dc3cbfdd7e199b73c253845680fb01f43530a5
Arnab Ghosh <arnabghosh89@gmail.com>
2024-05-06 16:29:02 +0530

    Add logic to unfurl Aggregate type error in IsSuppressedError function
    errors.As() is not supported[1] with Aggregate type error. This PR
    is to unfurl Aggregate type error into a slice of builtin error by
    using Aggregate.Errors() function. errors.As() can then be used
    with each element of the slice.
    
    This PR also wraps 'annotation not found' errors for remote zone
    nodes with SuppressedError type to suppress expected logs for remote
    zone nodes going forward.
    
    Upstream Issue: https://github.com/ovn-org/ovn-kubernetes/issues/4332
    [1] - https://pkg.go.dev/k8s.io/apimachinery/pkg/util/errors#Aggregate
    
    Signed-off-by: Arnab Ghosh <arnabghosh89@gmail.com>

--- go-controller/pkg/types/errors.go
+++ go-controller/pkg/types/errors.go
@@ -3,6 +3,8 @@ package types
 import (
 	"errors"
 	"fmt"
+
+	kerrors "k8s.io/apimachinery/pkg/util/errors"
 )
 
 type SuppressedError struct {
@@ -25,5 +27,18 @@ func NewSuppressedError(err error) error {
 
 func IsSuppressedError(err error) bool {
 	var suppressedError *SuppressedError
+	// errors.As() is not supported with Aggregate type error. Aggregate.Errors() converts an
+	// Aggregate type error into a slice of builtin error and then errors.As() can be used
+	if agg, ok := err.(kerrors.Aggregate); ok && err != nil {
+		suppress := false
+		for _, err := range agg.Errors() {
+			if errors.As(err, &suppressedError) {
+				suppress = true
+			} else {
+				return false
+			}
+		}
+		return suppress
+	}
 	return errors.As(err, &suppressedError)
 }
