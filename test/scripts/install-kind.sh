f452bb3a125a488a2baa8e5cbf243a13e75a8efc
Jacob Tanenbaum <jtanenba@redhat.com>
2024-06-13 14:09:14 -0400

    bump k8s version and supporting pkgs for e2e's
    k8s.io/kubernetes -> v1.30.2
    sigs.k8s.io/controller-runtime -> v0.18.4
    all other supporting k8s.io packages to 0.30.2
    
    Signed-off-by: Jacob Tanenbaum <jtanenba@redhat.com>

--- test/scripts/install-kind.sh
+++ test/scripts/install-kind.sh
@@ -45,7 +45,7 @@ install_kind() {
 }
 
 pushd $TMP_DIR
-K8S_VERSION="v1.29.2"
+K8S_VERSION="v1.30.2"
 
 # Install kubectl for K8S_VERSION in use
 # (to get latest stable version: $(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt )
