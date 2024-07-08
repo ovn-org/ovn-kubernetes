c190083d9026ec25d7f62fdefad5bfe40f727345
Enrique Llorente <ellorent@redhat.com>
2024-07-08 16:28:01 +0200

    user-def-nets, cni: configure the additional net iface
    Co-authored-by: Miguel Duarte Barroso <mdbarroso@redhat.com>
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- go-controller/pkg/testing/util.go
+++ go-controller/pkg/testing/util.go
@@ -8,7 +8,7 @@ import (
 )
 
 func GenerateNAD(networkName, name, namespace, topology, cidr, role string) *nadapi.NetworkAttachmentDefinition {
-	nadSpec := fmt.Sprintf(
+	return GenerateNADWithConfig(name, namespace, fmt.Sprintf(
 		`
 {
         "cniVersion": "0.4.0",
@@ -26,12 +26,35 @@ func GenerateNAD(networkName, name, namespace, topology, cidr, role string) *nad
 		cidr,
 		fmt.Sprintf("%s/%s", namespace, name),
 		role,
-	)
+	))
+}
+func GenerateNADWithoutMTU(networkName, name, namespace, topology, cidr, role string) *nadapi.NetworkAttachmentDefinition {
+	return GenerateNADWithConfig(name, namespace, fmt.Sprintf(
+		`
+{
+        "cniVersion": "0.4.0",
+        "name": %q,
+        "type": "ovn-k8s-cni-overlay",
+        "topology":%q,
+        "subnets": %q,
+        "netAttachDefName": %q,
+        "role": %q
+}
+`,
+		networkName,
+		topology,
+		cidr,
+		fmt.Sprintf("%s/%s", namespace, name),
+		role,
+	))
+}
+
+func GenerateNADWithConfig(name, namespace, config string) *nadapi.NetworkAttachmentDefinition {
 	return &nadapi.NetworkAttachmentDefinition{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
 			Namespace: namespace,
 		},
-		Spec: nadapi.NetworkAttachmentDefinitionSpec{Config: nadSpec},
+		Spec: nadapi.NetworkAttachmentDefinitionSpec{Config: config},
 	}
 }
