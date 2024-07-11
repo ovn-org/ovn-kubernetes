632bd635c1a425740757c1fcfffe56ff9724488a
Or Mergi <ormergi@redhat.com>
2024-07-11 15:35:50 +0300

    userdefinednetwork, template: Impl net-attach-def template
    In case network role is primary and join subentes are not spesified,
    set the default join subnets, currently:
      IPv4: 100.65.0.0/24
      IPv6: fd99::/64
    
    Create the NADs with the following label
      k8s.ovn.org/user-defined-network
    Having a label on UDN related NADs enables easier management
    (e.g.: listing all UDN related NAD) and
    integration (e.g.: act on UDN related NAD changes).
    
    Join subnets overlap check will be done in follow up commit.
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>

--- go-controller/pkg/clustermanager/userdefinednetwork/template/net-attach-def-template.go
+++ go-controller/pkg/clustermanager/userdefinednetwork/template/net-attach-def-template.go
@@ -1,15 +1,122 @@
 package template
 
 import (
-	"errors"
+	"encoding/json"
+	"fmt"
+	"net"
+	"strings"
 
 	netv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	userdefinednetworkv1 "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/userdefinednetwork/v1"
+
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
 )
 
+const (
+	cniVersion              = "1.0.0"
+	ovnK8sCNIOverlay        = "ovn-k8s-cni-overlay"
+	labelUserDefinedNetwork = "k8s.ovn.org/user-defined-network"
+)
+
+var udnKind = userdefinednetworkv1.SchemeGroupVersion.WithKind("UserDefinedNetwork")
+
 // RenderNetAttachDefManifest return NetworkAttachmentDefinition according to the given UserDefinedNetwork spec
-func RenderNetAttachDefManifest(_ *userdefinednetworkv1.UserDefinedNetwork) (*netv1.NetworkAttachmentDefinition, error) {
-	// TODO: implement NAD creation from UDN spec
-	return nil, errors.New("implement me")
+func RenderNetAttachDefManifest(udn *userdefinednetworkv1.UserDefinedNetwork) (*netv1.NetworkAttachmentDefinition, error) {
+	if udn == nil {
+		return nil, nil
+	}
+
+	cniNetConf, err := renderCNINetworkConfig(udn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to render CNI network config: %w", err)
+	}
+	cniNetConfRaw, err := json.Marshal(cniNetConf)
+	if err != nil {
+		return nil, err
+	}
+
+	return &netv1.NetworkAttachmentDefinition{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:            udn.Name,
+			OwnerReferences: []metav1.OwnerReference{*metav1.NewControllerRef(udn, udnKind)},
+			Labels: map[string]string{
+				labelUserDefinedNetwork: "",
+			},
+		},
+		Spec: netv1.NetworkAttachmentDefinitionSpec{
+			Config: string(cniNetConfRaw),
+		},
+	}, nil
+}
+
+func renderCNINetworkConfig(udn *userdefinednetworkv1.UserDefinedNetwork) (map[string]interface{}, error) {
+	networkName := udn.Namespace + "." + udn.Name
+	nadNamespaceName := util.GetNADName(udn.Namespace, udn.Name)
+
+	netConf := map[string]interface{}{
+		"cniVersion":       cniVersion,
+		"type":             ovnK8sCNIOverlay,
+		"name":             networkName,
+		"netAttachDefName": nadNamespaceName,
+		"topology":         strings.ToLower(string(udn.Spec.Topology)),
+		"role":             strings.ToLower(string(udn.Spec.Role)),
+	}
+
+	if udn.Spec.MTU > 0 {
+		netConf["mtu"] = udn.Spec.MTU
+	}
+
+	primaryNetwork := udn.Spec.Role == userdefinednetworkv1.NetworkRolePrimary
+
+	if primaryNetwork && udn.Spec.Topology == userdefinednetworkv1.NetworkTopologyLocalnet {
+		return nil, fmt.Errorf("locannet topology is not supported for primary network")
+	}
+
+	if primaryNetwork {
+		if len(udn.Spec.JoinSubnets) == 0 {
+			netConf["joinSubnets"] = types.UserDefinedPrimaryNetworkJoinSubnetV4 + "," + types.UserDefinedPrimaryNetworkJoinSubnetV6
+		} else {
+			if err := validateSubnets(udn.Spec.JoinSubnets); err != nil {
+				return nil, fmt.Errorf("invalid join subnets: %w", err)
+			}
+			netConf["joinSubnets"] = strings.Join(udn.Spec.JoinSubnets, ",")
+		}
+	}
+
+	if udn.Spec.IPAMLifecycle == userdefinednetworkv1.IPAMLifecyclePersistent {
+		if udn.Spec.Topology == userdefinednetworkv1.NetworkTopologyLayer2 ||
+			udn.Spec.Topology == userdefinednetworkv1.NetworkTopologyLocalnet {
+			netConf["allowPersistentIPs"] = true
+		} else {
+			return nil, fmt.Errorf("IPAMLifyecycle Persistent is supported for Layer2 and Localnet topology")
+		}
+	}
+
+	if len(udn.Spec.Subnets) > 0 {
+		if err := validateSubnets(udn.Spec.Subnets); err != nil {
+			return nil, fmt.Errorf("invalid subnets: %w", err)
+		}
+		netConf["subnets"] = strings.Join(udn.Spec.Subnets, ",")
+	}
+
+	if len(udn.Spec.ExcludeSubnets) > 0 {
+		if err := validateSubnets(udn.Spec.ExcludeSubnets); err != nil {
+			return nil, fmt.Errorf("invalid exclude subnets: %w", err)
+		}
+		netConf["excludeSubnets"] = strings.Join(udn.Spec.ExcludeSubnets, ",")
+	}
+
+	return netConf, nil
+}
+
+func validateSubnets(subnets []string) error {
+	for _, subent := range subnets {
+		if _, _, err := net.ParseCIDR(subent); err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- go-controller/pkg/clustermanager/userdefinednetwork/template/net-attach-def-template_test.go
+++ go-controller/pkg/clustermanager/userdefinednetwork/template/net-attach-def-template_test.go
@@ -2,12 +2,122 @@ package template
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/pointer"
+
+	netv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
+
+	udnv1 "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/crd/userdefinednetwork/v1"
 )
 
 var _ = Describe("NetAttachDefTemplate", func() {
-	It("should fail", func() {
+	const udnTypeName = "UserDefinedNetwork"
+
+	var udnApiVersion = udnv1.SchemeGroupVersion.String()
+
+	DescribeTable("should fail given",
+		func(spec *udnv1.UserDefinedNetworkSpec) {
+			udn := &udnv1.UserDefinedNetwork{
+				ObjectMeta: metav1.ObjectMeta{Namespace: "mynamespace", Name: "test-net", UID: "1"},
+				Spec:       *spec,
+			}
+			_, err := RenderNetAttachDefManifest(udn)
+			Expect(err).To(HaveOccurred())
+		},
+		Entry("invalid subnets", &udnv1.UserDefinedNetworkSpec{Subnets: []string{"abc"}}),
+		Entry("invalid exclude subnets", &udnv1.UserDefinedNetworkSpec{ExcludeSubnets: []string{"abc"}}),
+		Entry("invalid join subnets", &udnv1.UserDefinedNetworkSpec{Role: udnv1.NetworkRolePrimary, JoinSubnets: []string{"abc"}}),
+		Entry("topology=localnet & role=primary", &udnv1.UserDefinedNetworkSpec{
+			Topology: udnv1.NetworkTopologyLocalnet, Role: udnv1.NetworkRolePrimary}),
+		Entry("ipamLifecycle=persistent & topology=Layer3", &udnv1.UserDefinedNetworkSpec{
+			IPAMLifecycle: udnv1.IPAMLifecyclePersistent, Topology: udnv1.NetworkTopologyLayer3}),
+	)
+
+	It("should return nil given no NAD", func() {
 		_, err := RenderNetAttachDefManifest(nil)
-		Expect(err).To(HaveOccurred())
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should create net attach from spec", func() {
+		udn := &udnv1.UserDefinedNetwork{
+			ObjectMeta: metav1.ObjectMeta{Namespace: "mynamespace", Name: "test-net", UID: "1"},
+			Spec: udnv1.UserDefinedNetworkSpec{
+				Topology:       udnv1.NetworkTopologyLayer2,
+				Role:           udnv1.NetworkRolePrimary,
+				Subnets:        []string{"192.168.100.0/24", "2001:DBB::/64"},
+				ExcludeSubnets: []string{"192.168.100.0/26"},
+				JoinSubnets:    []string{"100.61.0.0/24", "fd90::/64"},
+				MTU:            1500,
+				IPAMLifecycle:  udnv1.IPAMLifecyclePersistent,
+			},
+		}
+		expectedNAD := &netv1.NetworkAttachmentDefinition{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "test-net",
+				Labels: map[string]string{
+					"k8s.ovn.org/user-defined-network": "",
+				},
+				OwnerReferences: []metav1.OwnerReference{
+					{
+						APIVersion:         udnApiVersion,
+						Kind:               udnTypeName,
+						Name:               "test-net",
+						UID:                "1",
+						BlockOwnerDeletion: pointer.Bool(true),
+						Controller:         pointer.Bool(true),
+					},
+				},
+			},
+			Spec: netv1.NetworkAttachmentDefinitionSpec{
+				Config: `{
+						"cniVersion": "1.0.0",
+						"name": "mynamespace.test-net",
+						"type": "ovn-k8s-cni-overlay",
+						"netAttachDefName": "mynamespace/test-net",
+						"topology": "layer2",
+						"role": "primary",
+						"mtu": 1500,
+						"subnets": "192.168.100.0/24,2001:DBB::/64",
+						"excludeSubnets": "192.168.100.0/26",
+						"joinSubnets": "100.61.0.0/24,fd90::/64",
+ 						"allowPersistentIPs": true
+					}`,
+			},
+		}
+
+		nad, err := RenderNetAttachDefManifest(udn)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(nad.TypeMeta).To(Equal(expectedNAD.TypeMeta))
+		Expect(nad.ObjectMeta).To(Equal(expectedNAD.ObjectMeta))
+		Expect(nad.Spec.Config).To(MatchJSON(expectedNAD.Spec.Config))
+	})
+
+	It("when network role is primary, no join-subnet specified, should set default join subnet", func() {
+		udn := &udnv1.UserDefinedNetwork{
+			ObjectMeta: metav1.ObjectMeta{Namespace: "mynamespace", Name: "test-net", UID: "1"},
+			Spec: udnv1.UserDefinedNetworkSpec{
+				Topology: udnv1.NetworkTopologyLayer2,
+				Role:     udnv1.NetworkRolePrimary,
+				Subnets:  []string{"192.168.100.0/24"},
+			},
+		}
+
+		nad, err := RenderNetAttachDefManifest(udn)
+		Expect(err).NotTo(HaveOccurred())
+
+		expectedCNIConf := `{
+			"cniVersion":"1.0.0",
+			"type":"ovn-k8s-cni-overlay",
+			"name":"mynamespace.test-net",
+			"netAttachDefName":"mynamespace/test-net",
+			"topology":"layer2",
+            "role":"primary",
+			"subnets":"192.168.100.0/24",
+			"joinSubnets":"100.65.0.0/16,fd99::/64"
+		}`
+		Expect(nad.Spec.Config).To(MatchJSON(expectedCNIConf))
 	})
 })
