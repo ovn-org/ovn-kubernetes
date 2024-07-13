e78f421d6e2dcfdbd72f3ae61b38d23115395fb9
Flavio Fernandes <ffernandes@nvidia.com>
2024-07-13 16:52:59 +0000

    Add flag to enable the feature
    This commit adds a new flag called
    enable-network-qos, which can be used
    to feature gate the Network QoS feature.
    
    The following flag is exposed in kind
    scripts as -nqe or --network-qos-enable:
    
       kind.sh -nqe
       kind-helm.sh -nqe
    
    Alternatively, it can be enabled via an
    exported shell variable:
    
       OVN_NETWORK_QOS_ENABLE=true kind.sh
    
    Signed-off-by: Flavio Fernandes <ffernandes@nvidia.com>

--- contrib/kind.sh
+++ contrib/kind.sh
@@ -78,6 +78,7 @@ usage() {
     echo "                 [-cm | --compact-mode]"
     echo "                 [-ic | --enable-interconnect]"
     echo "                 [-rae | --enable-route-advertisements]"
+    echo "                 [-nqe | --network-qos-enable]"
     echo "                 [--isolated]"
     echo "                 [-dns | --enable-dnsnameresolver]"
     echo "                 [-obs | --observability]"
@@ -135,6 +136,7 @@ usage() {
     echo "-sm  | --scale-metrics                Enable scale metrics"
     echo "-cm  | --compact-mode                 Enable compact mode, ovnkube master and node run in the same process."
     echo "-ic  | --enable-interconnect          Enable interconnect with each node as a zone (only valid if OVN_HA is false)"
+    echo "-nqe | --network-qos-enable           Enable network QoS. DEFAULT: Disabled."
     echo "--disable-ovnkube-identity            Disable per-node cert and ovnkube-identity webhook"
     echo "-npz | --nodes-per-zone               If interconnect is enabled, number of nodes per zone (Default 1). If this value > 1, then (total k8s nodes (workers + 1) / num of nodes per zone) should be zero."
     echo "-mtu                                  Define the overlay mtu"
@@ -331,6 +333,8 @@ parse_args() {
             -mtu  )                             shift
                                                 OVN_MTU=$1
                                                 ;;
+            -nqe | --network-qos-enable )       OVN_NETWORK_QOS_ENABLE=true
+                                                ;;
             --delete )                          delete
                                                 exit
                                                 ;;
@@ -421,6 +425,7 @@ print_params() {
        fi
      fi
      echo "OVN_ENABLE_OVNKUBE_IDENTITY = $OVN_ENABLE_OVNKUBE_IDENTITY"
+     echo "OVN_NETWORK_QOS_ENABLE = $OVN_NETWORK_QOS_ENABLE"
      echo "KIND_NUM_WORKER = $KIND_NUM_WORKER"
      echo "OVN_MTU= $OVN_MTU"
      echo "OVN_ENABLE_DNSNAMERESOLVER= $OVN_ENABLE_DNSNAMERESOLVER"
@@ -573,6 +578,7 @@ set_default_params() {
   KIND_NUM_MASTER=1
   OVN_ENABLE_INTERCONNECT=${OVN_ENABLE_INTERCONNECT:-false}
   OVN_ENABLE_OVNKUBE_IDENTITY=${OVN_ENABLE_OVNKUBE_IDENTITY:-true}
+  OVN_NETWORK_QOS_ENABLE=${OVN_NETWORK_QOS_ENABLE:-false}
 
 
   if [ "$OVN_COMPACT_MODE" == true ] && [ "$OVN_ENABLE_INTERCONNECT" != false ]; then
@@ -872,6 +878,7 @@ create_ovn_kube_manifests() {
     --enable-multi-external-gateway=true \
     --enable-ovnkube-identity="${OVN_ENABLE_OVNKUBE_IDENTITY}" \
     --enable-persistent-ips=true \
+    --network-qos-enable="${OVN_NETWORK_QOS_ENABLE}" \
     --mtu="${OVN_MTU}" \
     --enable-dnsnameresolver="${OVN_ENABLE_DNSNAMERESOLVER}" \
     --mtu="${OVN_MTU}" \
@@ -956,6 +963,7 @@ install_ovn() {
   run_kubectl apply -f k8s.ovn.org_egressqoses.yaml
   run_kubectl apply -f k8s.ovn.org_egressservices.yaml
   run_kubectl apply -f k8s.ovn.org_adminpolicybasedexternalroutes.yaml
+  run_kubectl apply -f k8s.ovn.org_networkqoses.yaml
   run_kubectl apply -f k8s.ovn.org_userdefinednetworks.yaml
   run_kubectl apply -f k8s.ovn.org_clusteruserdefinednetworks.yaml
   run_kubectl apply -f k8s.ovn.org_routeadvertisements.yaml
