b1924fa59de64f94994e8dad8aa54ae5eaeee0ad
Martin Kennelly <mkennell@redhat.com>
2024-07-13 15:55:20 +0100

    Default bridge: add EIP SNAT
    Signed-off-by: Martin Kennelly <mkennell@redhat.com>

--- go-controller/pkg/node/default_node_network_controller.go
+++ go-controller/pkg/node/default_node_network_controller.go
@@ -106,6 +106,7 @@ type DefaultNodeNetworkController struct {
 	// Node healthcheck server for cloud load balancers
 	healthzServer *proxierHealthUpdater
 	routeManager  *routemanager.Controller
+	linkManager   *linkmanager.Controller
 
 	// retry framework for namespaces, used for the removal of stale conntrack entries for external gateways
 	retryNamespaces *retry.RetryFramework
@@ -131,7 +132,7 @@ type preStartSetup struct {
 func newDefaultNodeNetworkController(cnnci *CommonNodeNetworkControllerInfo, stopChan chan struct{},
 	wg *sync.WaitGroup, routeManager *routemanager.Controller) *DefaultNodeNetworkController {
 
-	return &DefaultNodeNetworkController{
+	dnnc := &DefaultNodeNetworkController{
 		BaseNodeNetworkController: BaseNodeNetworkController{
 			CommonNodeNetworkControllerInfo: *cnnci,
 			NetInfo:                         &util.DefaultNetInfo{},
@@ -140,6 +141,8 @@ func newDefaultNodeNetworkController(cnnci *CommonNodeNetworkControllerInfo, sto
 		},
 		routeManager: routeManager,
 	}
+	dnnc.linkManager = linkmanager.NewController(cnnci.name, config.IPv4Mode, config.IPv6Mode, dnnc.updateGatewayMAC)
+	return dnnc
 }
 
 // NewDefaultNodeNetworkController creates a new network controller for node management of the default network
@@ -1193,13 +1196,10 @@ func (nc *DefaultNodeNetworkController) Start(ctx context.Context) error {
 		}
 	}
 
-	// create link manager, will work for egress IP as well as monitoring MAC changes to default gw bridge
-	linkManager := linkmanager.NewController(nc.name, config.IPv4Mode, config.IPv6Mode, nc.updateGatewayMAC)
-
 	if config.OVNKubernetesFeature.EnableEgressIP && !util.PlatformTypeIsEgressIPCloudProvider() {
 		c, err := egressip.NewController(nc.Kube, nc.watchFactory.EgressIPInformer(), nc.watchFactory.NodeInformer(),
 			nc.watchFactory.NamespaceInformer(), nc.watchFactory.PodCoreInformer(), nc.routeManager, config.IPv4Mode,
-			config.IPv6Mode, nc.name, linkManager)
+			config.IPv6Mode, nc.name, nc.linkManager)
 		if err != nil {
 			return fmt.Errorf("failed to create egress IP controller: %v", err)
 		}
@@ -1210,7 +1210,7 @@ func (nc *DefaultNodeNetworkController) Start(ctx context.Context) error {
 		klog.Infof("Egress IP for secondary host network is disabled")
 	}
 
-	linkManager.Run(nc.stopChan, nc.wg)
+	nc.linkManager.Run(nc.stopChan, nc.wg)
 
 	nc.wg.Add(1)
 	go func() {
