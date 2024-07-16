943452b16a251e443811ee85261040992fbbdd70
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2024-07-16 09:19:21 +0000

    Network controllers reconcile VRF changes
    Add the ability for network controllers to reconcile some network
    information changes. Currently just changes of the VRFs the network is
    leaking/advertising to. Support for reconciling NAD changes is not
    included in this commit.
    
    Currently reconciles if the network is advertised or not:
    - for OVN network controller to configure or not the pod IP to node IP
      SNAT on the GR for a node of its zone
    - for node network controller to configure or not br-ex flows to
      redirect pod IP ingress traffic to the OVN network
    
    This should be enough to provide direct ingress capabilities for the
    default network in SGW mode.
    
    Note that secondary network controllers don't reconcile anything as
    route advertising is not supported on them. Also cluster manager network
    controllers don't reconcile much as they don't have the need.
    
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

--- go-controller/pkg/node/gateway_init.go
+++ go-controller/pkg/node/gateway_init.go
@@ -435,6 +435,7 @@ func (nc *DefaultNodeNetworkController) initGatewayMainStart(gw *gateway, waiter
 	if portClaimWatcher != nil {
 		gw.portClaimWatcher = portClaimWatcher
 	}
+	gw.isRoutingAdvertised = nc.isRoutingAdvertised()
 
 	initGwFunc := func() error {
 		return gw.Init(nc.stopChan, nc.wg)
