2efd1f76f41c251cfe246462bc2d8a004117985c
Enrique Llorente <ellorent@redhat.com>
2024-07-30 12:42:24 +0200

    udn, gateway: Use physnet
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- go-controller/pkg/ovn/gateway.go
+++ go-controller/pkg/ovn/gateway.go
@@ -1039,9 +1039,8 @@ func (gw *GatewayManager) syncNodeGateway(
 }
 
 func physNetName(netInfo util.NetInfo) string {
-	physnetName := types.PhysicalNetworkName
-	if netInfo.IsSecondary() {
-		physnetName = netInfo.GetNetworkName()
+	if netInfo.IsDefault() || netInfo.IsPrimaryNetwork() {
+		return types.PhysicalNetworkName
 	}
-	return physnetName
+	return netInfo.GetNetworkName()
 }
