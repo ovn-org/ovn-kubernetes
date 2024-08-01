0982c4e71d51f538aa3cd8ea1584dbce87d9b43e
Enrique Llorente <ellorent@redhat.com>
2024-08-01 07:59:57 +0200

    tools: logs and exec
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

---
+++ contrib/logs.sh
@@ -0,0 +1,9 @@
+#/bin/bash -xe
+node=$1
+shift
+name=$1
+shift
+container=$1
+shift
+pod=$(kubectl get pods -n ovn-kubernetes --field-selector spec.nodeName=$node -l name=$name -o name)
+kubectl logs -n ovn-kubernetes $pod -c $container
