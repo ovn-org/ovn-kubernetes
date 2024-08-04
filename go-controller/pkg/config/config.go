d431a61499428f1ce66852a22de93e60cfbd7246
Nadia Pinaeva <n.m.pinaeva@gmail.com>
2024-08-04 12:22:58 +0200

    Add config options to enable observability.
    Signed-off-by: Nadia Pinaeva <n.m.pinaeva@gmail.com>

--- go-controller/pkg/config/config.go
+++ go-controller/pkg/config/config.go
@@ -413,6 +413,7 @@ type OVNKubernetesFeatureConfig struct {
 	EnablePersistentIPs             bool `gcfg:"enable-persistent-ips"`
 	EnableDNSNameResolver           bool `gcfg:"enable-dns-name-resolver"`
 	EnableServiceTemplateSupport    bool `gcfg:"enable-svc-template-support"`
+	EnableObservability             bool `gcfg:"enable-observability"`
 }
 
 // GatewayMode holds the node gateway mode
@@ -1082,6 +1083,12 @@ var OVNK8sFeatureFlags = []cli.Flag{
 		Usage:       "Configure to use svc-template with ovn-kubernetes.",
 		Destination: &cliConfig.OVNKubernetesFeature.EnableServiceTemplateSupport,
 		Value:       OVNKubernetesFeature.EnableServiceTemplateSupport,
+	},
+	&cli.BoolFlag{
+		Name:        "enable-observability",
+		Usage:       "Configure to use OVN sampling with ovn-kubernetes.",
+		Destination: &cliConfig.OVNKubernetesFeature.EnableObservability,
+		Value:       OVNKubernetesFeature.EnableObservability,
 	},
 }
 
