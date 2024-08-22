afc2bfcf634a540c3f1f523d7d2e9e2609b996ec
Miguel Duarte Barroso <mdbarroso@redhat.com>
2024-08-22 18:05:20 +0200

    udn: set persistentIPs for UDN ifaces
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- go-controller/pkg/util/multi_network.go
+++ go-controller/pkg/util/multi_network.go
@@ -939,6 +939,13 @@ func GetPodNADToNetworkMappingWithActiveNetwork(pod *kapi.Pod, nInfo NetInfo, ac
 		Name:      activeNetworkNADKey[1],
 	}
 
+	if nInfo.IsPrimaryNetwork() && nInfo.TopologyType() == types.Layer2Topology && nInfo.AllowsPersistentIPs() {
+		ipamClaimName, wasPersistentIPRequested := pod.Annotations[OvnUDNIPAMClaimName]
+		if wasPersistentIPRequested {
+			networkSelections[activeNetworkNADs[0]].IPAMClaimReference = ipamClaimName
+		}
+	}
+
 	return true, networkSelections, nil
 }
 func IsMultiNetworkPoliciesSupportEnabled() bool {
