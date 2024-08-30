481ab3f1a54bdf674b6d179f2ebaf26663a579d2
Miguel Duarte Barroso <mdbarroso@redhat.com>
2024-08-30 10:45:29 +0200

    udn, cni, tests: ensure we report the UDN interface via CNI result
    Whenever we invoke `cmdAddWithGetCNIResultFunc` it will return a result
    with 2 interfaces: the first for the host side veth, and the second for
    the pod side veth (featuring the sandbox attribute). Check the source
    code at [0] to understand how it happens.
    
    Now that we have changed the production code to *also* reply with the
    result of adding the UDN interface, we are in fact returning 4
    interfaces on the result of `cmdAddWithGetCNIResultFunc`:
    
    index: interface
    ----------------
    0: host side primary UDN
    1: pod side primary UDN
    2: host side cluster default network
    3: pod side cluster default network
    
    [0] - https://github.com/ovn-org/ovn-kubernetes/blob/a1e88dcd0b79b6c5eb033daea178708993f8664e/go-controller/pkg/cni/helper_linux.go#L650
    
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- go-controller/pkg/cni/cni_test.go
+++ go-controller/pkg/cni/cni_test.go
@@ -2,6 +2,8 @@ package cni
 
 import (
 	"context"
+	"fmt"
+	"net"
 	"time"
 
 	. "github.com/onsi/ginkgo"
@@ -9,14 +11,17 @@ import (
 
 	cnitypes "github.com/containernetworking/cni/pkg/types"
 	current "github.com/containernetworking/cni/pkg/types/100"
-	v1nadmocks "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/testing/mocks/github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/client/listers/k8s.cni.cncf.io/v1"
 
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/client-go/kubernetes/fake"
+
+	nadv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
 
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/cni/types"
 	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/config"
+	v1nadmocks "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/testing/mocks/github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/client/listers/k8s.cni.cncf.io/v1"
 	v1mocks "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/testing/mocks/k8s.io/client-go/listers/core/v1"
 	ovntypes "github.com/ovn-org/ovn-kubernetes/go-controller/pkg/types"
 )
@@ -157,6 +162,148 @@ var _ = Describe("Network Segmentation", func() {
 			})
 		})
 
+		Context("pod with a user defined primary network", func() {
+			const (
+				dummyMACHostSide = "07:06:05:04:03:02"
+				nadName          = "tenantred"
+				namespace        = "foo-ns"
+			)
+
+			dummyGetCNIResult := func(request *PodRequest, getter PodInfoGetter, podInterfaceInfo *PodInterfaceInfo) (*current.Result, error) {
+				var gatewayIP net.IP
+				if len(podInterfaceInfo.Gateways) > 0 {
+					gatewayIP = podInterfaceInfo.Gateways[0]
+				}
+				var ips []*current.IPConfig
+				ifaceIdx := 1 // host side of the veth is 0; pod side of the veth is 1
+				for _, ip := range podInterfaceInfo.IPs {
+					ips = append(ips, &current.IPConfig{Address: *ip, Gateway: gatewayIP, Interface: &ifaceIdx})
+				}
+				ifaceName := "eth0"
+				if request.netName != "default" {
+					ifaceName = "ovn-udn1"
+				}
+
+				interfaces := []*current.Interface{
+					{
+						Name: "host_" + ifaceName,
+						Mac:  dummyMACHostSide,
+					},
+					{
+						Name:    ifaceName,
+						Mac:     podInterfaceInfo.MAC.String(),
+						Sandbox: "bobloblaw",
+					},
+				}
+				return &current.Result{
+					CNIVersion: "0.3.1",
+					Interfaces: interfaces,
+					IPs:        ips,
+				}, nil
+			}
+
+			BeforeEach(func() {
+				pod = &v1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      pr.PodName,
+						Namespace: pr.PodNamespace,
+						Annotations: map[string]string{
+							"k8s.ovn.org/pod-networks": `{"default":{"ip_address":"100.10.10.3/24","mac_address":"0a:58:fd:98:00:01", "role":"infrastructure-locked"}, "meganet":{"ip_addresses":["10.10.10.30/24","fd10::3/64"],"mac_address":"02:03:04:05:06:07", "role":"primary"}}`,
+						},
+					},
+				}
+				nadNamespaceLister := &v1nadmocks.NetworkAttachmentDefinitionNamespaceLister{}
+				nadNamespaceLister.On("List", labels.Everything()).Return([]*nadv1.NetworkAttachmentDefinition{{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      nadName,
+						Namespace: namespace,
+					},
+					Spec: nadv1.NetworkAttachmentDefinitionSpec{
+						Config: dummyPrimaryUDNConfig(namespace, nadName),
+					},
+				}}, nil)
+				nadLister.On("NetworkAttachmentDefinitions", "foo-ns").Return(nadNamespaceLister)
+
+				getCNIResultStub = dummyGetCNIResult
+			})
+
+			It("should return the information of both the default net and the primary UDN in the result", func() {
+				podNamespaceLister.On("Get", pr.PodName).Return(pod, nil)
+				response, err := pr.cmdAddWithGetCNIResultFunc(kubeAuth, clientSet, getCNIResultStub)
+				Expect(err).NotTo(HaveOccurred())
+				// for every interface added, we return 2 interfaces; the host side of the
+				// veth, then the pod side of the veth.
+				// thus, the UDN interface idx will be 3:
+				// idx: iface
+				//   0: host side primary UDN
+				//   1: pod side primary UDN
+				//   2: host side default network
+				//   3: pod side default network
+				podDefaultClusterNetIfaceIDX := 3
+				podUDNIfaceIDX := 1
+				Expect(response.Result).To(Equal(
+					&current.Result{
+						CNIVersion: "0.3.1",
+						Interfaces: []*current.Interface{
+							{
+								Name: "host_ovn-udn1",
+								Mac:  dummyMACHostSide,
+							},
+							{
+								Name:    "ovn-udn1",
+								Mac:     "02:03:04:05:06:07",
+								Sandbox: "bobloblaw",
+							}, {
+								Name: "host_eth0",
+								Mac:  dummyMACHostSide,
+							},
+							{
+								Name:    "eth0",
+								Mac:     "0a:58:fd:98:00:01",
+								Sandbox: "bobloblaw",
+							},
+						},
+						IPs: []*current.IPConfig{
+							{
+								Address: net.IPNet{
+									IP:   net.ParseIP("10.10.10.30"),
+									Mask: net.CIDRMask(24, 32),
+								},
+								Interface: &podUDNIfaceIDX,
+							},
+							{
+								Address: net.IPNet{
+									IP:   net.ParseIP("fd10::3"),
+									Mask: net.CIDRMask(64, 128),
+								},
+								Interface: &podUDNIfaceIDX,
+							},
+							{
+								Address: net.IPNet{
+									IP:   net.ParseIP("100.10.10.3"),
+									Mask: net.CIDRMask(24, 32),
+								},
+								Interface: &podDefaultClusterNetIfaceIDX,
+							},
+						},
+					},
+				))
+			})
+		})
 	})
 
 })
+
+func dummyPrimaryUDNConfig(ns, nadName string) string {
+	namespacedName := fmt.Sprintf("%s/%s", ns, nadName)
+	return fmt.Sprintf(`
+    {
+            "name": "tenantred",
+            "type": "ovn-k8s-cni-overlay",
+            "topology": "layer2",
+            "subnets": "10.10.0.0/16,fd10::0/64",
+            "netAttachDefName": %q,
+            "role": "primary"
+    }
+`, namespacedName)
+}
