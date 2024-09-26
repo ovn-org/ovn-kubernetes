4b58ba5c99889dfbca2803d49607f645ebb2b2b8
Enrique Llorente <ellorent@redhat.com>
2024-09-26 11:28:47 +0200

    kubevirt, dhcp: Add fqdn and router/dns/mtu opts
    To integrate DHCPv4 and DHCPv6 with secondary networks we should
    parameterize dns server, router and ipv4 mtu.
    
    It also add fqdn DHCPv6 option to reach parity with DHCPv4.
    
    Signed-off-by: Enrique Llorente <ellorent@redhat.com>

--- go-controller/pkg/kubevirt/dhcp_test.go
+++ go-controller/pkg/kubevirt/dhcp_test.go
@@ -7,35 +7,20 @@ import (
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	ktypes "k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/kubernetes/fake"
 
-	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/factory"
-	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/util"
+	"github.com/ovn-org/ovn-kubernetes/go-controller/pkg/nbdb"
 )
 
 var _ = Describe("Kubevirt", func() {
 	type dhcpTest struct {
 		cidrs                             []string
 		controllerName, namespace, vmName string
-		dns                               *corev1.Service
+		opts                              []DHCPConfigsOpt
 		expectedDHCPConfigs               dhcpConfigs
 		expectedError                     string
 	}
 	var (
-		svc = func(namespace string, name string, clusterIPs []string) *corev1.Service {
-			return &corev1.Service{
-				ObjectMeta: metav1.ObjectMeta{
-					Namespace: "kube-system",
-					Name:      "kube-dns",
-				},
-				Spec: corev1.ServiceSpec{
-					ClusterIPs: clusterIPs,
-				},
-			}
-		}
 		key = func(namespace, name string) ktypes.NamespacedName {
 			return ktypes.NamespacedName{Namespace: namespace, Name: name}
 		}
@@ -46,101 +31,181 @@ var _ = Describe("Kubevirt", func() {
 		}
 	)
 	DescribeTable("composing dhcp options should success", func(t dhcpTest) {
-		svcs := []corev1.Service{}
-		if t.dns != nil {
-			svcs = append(svcs, *t.dns)
-		}
-		fakeClient := &util.OVNMasterClientset{
-			KubeClient: fake.NewSimpleClientset(&corev1.ServiceList{
-				Items: svcs,
-			}),
-		}
-		watcher, err := factory.NewMasterWatchFactory(fakeClient)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(watcher.Start()).To(Succeed())
-
 		cidrs := []*net.IPNet{}
 		for _, cidr := range t.cidrs {
 			cidrs = append(cidrs, parseCIDR(cidr))
 		}
-		obtaineddhcpConfigs, err := composeDHCPConfigs(watcher, t.controllerName, ktypes.NamespacedName{Namespace: t.namespace, Name: t.vmName}, cidrs)
+		obtaineddhcpConfigs, err := composeDHCPConfigs(t.controllerName, ktypes.NamespacedName{Namespace: t.namespace, Name: t.vmName}, cidrs, t.opts...)
 		Expect(err).ToNot(HaveOccurred())
 		Expect(obtaineddhcpConfigs.V4).To(Equal(t.expectedDHCPConfigs.V4))
 		Expect(obtaineddhcpConfigs.V6).To(Equal(t.expectedDHCPConfigs.V6))
 	},
-		Entry("IPv4 Single stack and k8s dns", dhcpTest{
-			cidrs:               []string{"192.168.25.0/24"},
-			controllerName:      "defaultController",
-			namespace:           "namespace1",
-			vmName:              "foo1",
-			dns:                 svc("kube-system", "kube-dns", []string{"192.167.23.44"}),
-			expectedDHCPConfigs: dhcpConfigs{V4: ComposeDHCPv4Options("192.168.25.0/24", "192.167.23.44", "defaultController", key("namespace1", "foo1"))},
-		}),
-		Entry("IPv6 Single stack and k8s dns", dhcpTest{
-			cidrs:               []string{"2002:0:0:1234::/64"},
-			controllerName:      "defaultController",
-			namespace:           "namespace1",
-			vmName:              "foo1",
-			dns:                 svc("kube-system", "kube-dns", []string{"2001:1:2:3:4:5:6:7"}),
-			expectedDHCPConfigs: dhcpConfigs{V6: ComposeDHCPv6Options("2002:0:0:1234::/64", "2001:1:2:3:4:5:6:7", "defaultController", key("namespace1", "foo1"))},
-		}),
-		Entry("Dual stack and k8s dns", dhcpTest{
-			cidrs:          []string{"192.168.25.0/24", "2002:0:0:1234::/64"},
+		Entry("IPv4 Single stack and dns", dhcpTest{
+			cidrs:          []string{"192.168.25.0/24"},
 			controllerName: "defaultController",
 			namespace:      "namespace1",
 			vmName:         "foo1",
-			dns:            svc("kube-system", "kube-dns", []string{"192.167.23.44", "2001:1:2:3:4:5:6:7"}),
+			opts: []DHCPConfigsOpt{
+				WithIPv4DNSServer("192.167.23.44"),
+				WithIPv4Router("192.168.25.1"),
+				WithIPv4MTU(1500),
+			},
 			expectedDHCPConfigs: dhcpConfigs{
-				V4: ComposeDHCPv4Options("192.168.25.0/24", "192.167.23.44", "defaultController", key("namespace1", "foo1")),
-				V6: ComposeDHCPv6Options("2002:0:0:1234::/64", "2001:1:2:3:4:5:6:7", "defaultController", key("namespace1", "foo1")),
+				V4: &nbdb.DHCPOptions{
+					Cidr: "192.168.25.0/24",
+					ExternalIDs: map[string]string{
+						"k8s.ovn.org/owner-controller": "defaultController",
+						"k8s.ovn.org/owner-type":       "VirtualMachine",
+						"k8s.ovn.org/name":             "namespace1/foo1",
+						"k8s.ovn.org/cidr":             "192.168.25.0/24",
+						"k8s.ovn.org/id":               "defaultController:VirtualMachine:namespace1/foo1:192.168.25.0/24",
+						"k8s.ovn.org/zone":             "local",
+					},
+					Options: map[string]string{
+						"lease_time": "3500",
+						"server_id":  ARPProxyIPv4,
+						"server_mac": ARPProxyMAC,
+						"hostname":   `"foo1"`,
+						"dns_server": "192.167.23.44",
+						"router":     "192.168.25.1",
+						"mtu":        "1500",
+					},
+				},
 			},
 		}),
-		Entry("Dual stack and k8s dns with ipv4 only", dhcpTest{
+		Entry("IPv6 Single stack and dns", dhcpTest{
+			cidrs:          []string{"2002:0:0:1234::/64"},
+			controllerName: "defaultController",
+			namespace:      "namespace1",
+			vmName:         "foo1",
+			opts:           []DHCPConfigsOpt{WithIPv6DNSServer("2001:1:2:3:4:5:6:7")},
+			expectedDHCPConfigs: dhcpConfigs{
+				V6: &nbdb.DHCPOptions{
+					Cidr: "2002:0:0:1234::/64",
+					ExternalIDs: map[string]string{
+						"k8s.ovn.org/owner-controller": "defaultController",
+						"k8s.ovn.org/owner-type":       "VirtualMachine",
+						"k8s.ovn.org/name":             "namespace1/foo1",
+						"k8s.ovn.org/cidr":             "2002.0.0.1234../64",
+						"k8s.ovn.org/id":               "defaultController:VirtualMachine:namespace1/foo1:2002.0.0.1234../64",
+						"k8s.ovn.org/zone":             "local",
+					},
+					Options: map[string]string{
+						"server_id":  "0a:58:6d:6d:c1:50",
+						"fqdn":       `"foo1"`,
+						"dns_server": "2001:1:2:3:4:5:6:7",
+					},
+				},
+			},
+		}),
+		Entry("Dual stack and dns", dhcpTest{
 			cidrs:          []string{"192.168.25.0/24", "2002:0:0:1234::/64"},
 			controllerName: "defaultController",
 			namespace:      "namespace1",
 			vmName:         "foo1",
-			dns:            svc("kube-system", "kube-dns", []string{"192.167.23.44", ""}),
+			opts: []DHCPConfigsOpt{
+				WithIPv4DNSServer("192.167.23.44"),
+				WithIPv6DNSServer("2001:1:2:3:4:5:6:7"),
+			},
 			expectedDHCPConfigs: dhcpConfigs{
-				V4: ComposeDHCPv4Options("192.168.25.0/24", "192.167.23.44", "defaultController", key("namespace1", "foo1")),
-				V6: ComposeDHCPv6Options("2002:0:0:1234::/64", "", "defaultController", key("namespace1", "foo1")),
+				V4: &nbdb.DHCPOptions{
+					Cidr: "192.168.25.0/24",
+					ExternalIDs: map[string]string{
+						"k8s.ovn.org/owner-controller": "defaultController",
+						"k8s.ovn.org/owner-type":       "VirtualMachine",
+						"k8s.ovn.org/name":             "namespace1/foo1",
+						"k8s.ovn.org/cidr":             "192.168.25.0/24",
+						"k8s.ovn.org/id":               "defaultController:VirtualMachine:namespace1/foo1:192.168.25.0/24",
+						"k8s.ovn.org/zone":             "local",
+					},
+					Options: map[string]string{
+						"lease_time": "3500",
+						"server_id":  ARPProxyIPv4,
+						"server_mac": ARPProxyMAC,
+						"hostname":   `"foo1"`,
+						"dns_server": "192.167.23.44",
+					},
+				},
+				V6: &nbdb.DHCPOptions{
+					Cidr: "2002:0:0:1234::/64",
+					ExternalIDs: map[string]string{
+						"k8s.ovn.org/owner-controller": "defaultController",
+						"k8s.ovn.org/owner-type":       "VirtualMachine",
+						"k8s.ovn.org/name":             "namespace1/foo1",
+						"k8s.ovn.org/cidr":             "2002.0.0.1234../64",
+						"k8s.ovn.org/id":               "defaultController:VirtualMachine:namespace1/foo1:2002.0.0.1234../64",
+						"k8s.ovn.org/zone":             "local",
+					},
+					Options: map[string]string{
+						"server_id":  "0a:58:6d:6d:c1:50",
+						"fqdn":       `"foo1"`,
+						"dns_server": "2001:1:2:3:4:5:6:7",
+					},
+				},
+			},
+		}),
+
+		Entry("Dual stack with single IPv4 dns server", dhcpTest{
+			cidrs:          []string{"192.168.25.0/24", "2002:0:0:1234::/64"},
+			controllerName: "defaultController",
+			namespace:      "namespace1",
+			vmName:         "foo1",
+			opts: []DHCPConfigsOpt{
+				WithIPv4DNSServer("192.167.23.44"),
+				WithIPv6DNSServer(""),
+			},
+			expectedDHCPConfigs: dhcpConfigs{
+				V4: &nbdb.DHCPOptions{
+					Cidr: "192.168.25.0/24",
+					ExternalIDs: map[string]string{
+						"k8s.ovn.org/owner-controller": "defaultController",
+						"k8s.ovn.org/owner-type":       "VirtualMachine",
+						"k8s.ovn.org/name":             "namespace1/foo1",
+						"k8s.ovn.org/cidr":             "192.168.25.0/24",
+						"k8s.ovn.org/id":               "defaultController:VirtualMachine:namespace1/foo1:192.168.25.0/24",
+						"k8s.ovn.org/zone":             "local",
+					},
+					Options: map[string]string{
+						"lease_time": "3500",
+						"server_id":  ARPProxyIPv4,
+						"server_mac": ARPProxyMAC,
+						"hostname":   `"foo1"`,
+						"dns_server": "192.167.23.44",
+					},
+				},
+				V6: &nbdb.DHCPOptions{
+					Cidr: "2002:0:0:1234::/64",
+					ExternalIDs: map[string]string{
+						"k8s.ovn.org/owner-controller": "defaultController",
+						"k8s.ovn.org/owner-type":       "VirtualMachine",
+						"k8s.ovn.org/name":             "namespace1/foo1",
+						"k8s.ovn.org/cidr":             "2002.0.0.1234../64",
+						"k8s.ovn.org/id":               "defaultController:VirtualMachine:namespace1/foo1:2002.0.0.1234../64",
+						"k8s.ovn.org/zone":             "local",
+					},
+					Options: map[string]string{
+						"server_id": "0a:58:6d:6d:c1:50",
+						"fqdn":      `"foo1"`,
+					},
+				},
 			},
 		}),
 	)
 
 	DescribeTable("composing dhcp options should fail", func(t dhcpTest) {
-		svcs := []corev1.Service{}
-		if t.dns != nil {
-			svcs = append(svcs, *t.dns)
-		}
-		fakeClient := &util.OVNMasterClientset{
-			KubeClient: fake.NewSimpleClientset(&corev1.ServiceList{
-				Items: svcs,
-			}),
-		}
-		watcher, err := factory.NewMasterWatchFactory(fakeClient)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(watcher.Start()).To(Succeed())
-
 		cidrs := []*net.IPNet{}
 		for _, cidr := range t.cidrs {
 			cidrs = append(cidrs, parseCIDR(cidr))
 		}
-		_, err = composeDHCPConfigs(watcher, t.controllerName, key(t.namespace, t.vmName), cidrs)
+		_, err := composeDHCPConfigs(t.controllerName, key(t.namespace, t.vmName), cidrs)
 		Expect(err).To(MatchError(t.expectedError))
 	},
 		Entry("No cidr should fail", dhcpTest{
 			expectedError: "missing podIPs to compose dhcp options",
 		}),
-		Entry("No dns should fail", dhcpTest{
-			vmName:        "vm1",
-			cidrs:         []string{"192.168.3.0/24"},
-			expectedError: `failed retrieving dns service cluster ip: service "kube-dns" not found`,
-		}),
 		Entry("No hostname should fail", dhcpTest{
 			vmName:        "",
 			cidrs:         []string{"192.168.25.0/24"},
-			dns:           svc("kube-system", "kube-dns", []string{"192.167.23.44"}),
 			expectedError: "missing vmName to compose dhcp options",
 		}),
 	)
