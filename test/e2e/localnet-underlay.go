f68e6fdadc5ac7baf622435b1854d47a0a4ba7d7
Miguel Duarte Barroso <mdbarroso@redhat.com>
2024-10-01 17:43:31 +0200

    localnet, tests: add e2e tests to share bridge mappings
    Adds an e2e tests that asserts the same bridge mapping can be shared
    between multiple networks.
    
    Signed-off-by: Miguel Duarte Barroso <mdbarroso@redhat.com>

--- test/e2e/localnet-underlay.go
+++ test/e2e/localnet-underlay.go
@@ -3,6 +3,8 @@ package e2e
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/exec"
 	"strings"
 
 	v1 "k8s.io/api/core/v1"
@@ -159,4 +161,49 @@ func bridgeMapping(physnet, ovsBridge string) BridgeMapping {
 		physnet:   physnet,
 		ovsBridge: ovsBridge,
 	}
+}
+
+// TODO: make this function idempotent; use golang netlink instead
+func createVLANInterface(deviceName string, vlanID string, ipAddress *string) error {
+	vlan := vlanName(deviceName, vlanID)
+	cmd := exec.Command("sudo", "ip", "link", "add", "link", deviceName, "name", vlan, "type", "vlan", "id", vlanID)
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to create vlan interface %s: %v", vlan, err)
+	}
+
+	cmd = exec.Command("sudo", "ip", "link", "set", "dev", vlan, "up")
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to enable vlan interface %s: %v", vlan, err)
+	}
+
+	if ipAddress != nil {
+		cmd = exec.Command("sudo", "ip", "addr", "add", *ipAddress, "dev", vlan)
+		cmd.Stderr = os.Stderr
+
+		if err := cmd.Run(); err != nil {
+			return fmt.Errorf("failed to define the vlan interface %q IP Address %s: %v", vlan, *ipAddress, err)
+		}
+	}
+	return nil
+}
+
+// TODO: make this function idempotent; use golang netlink instead
+func deleteVLANInterface(deviceName string, vlanID string) error {
+	vlan := vlanName(deviceName, vlanID)
+	cmd := exec.Command("sudo", "ip", "link", "del", vlan)
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("failed to delete vlan interface %s: %v", vlan, err)
+	}
+	return nil
+}
+
+func vlanName(deviceName string, vlanID string) string {
+	// MAX IFSIZE 16; got to truncate it to add the vlan suffix
+	if len(deviceName)+len(vlanID)+1 > 16 {
+		deviceName = deviceName[:len(deviceName)-len(vlanID)-1]
+	}
+	return fmt.Sprintf("%s.%s", deviceName, vlanID)
 }
