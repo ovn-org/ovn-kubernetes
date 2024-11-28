6f7cb5625ae68696b5f7fae4dfd79fec0d68bbeb
Jaime Caamaño Ruiz <jcaamano@redhat.com>
2024-11-28 12:39:47 +0000

    VRF manager: manage VRFs with any name
    Instead of considering managed VRFs those that follow the mp<id>-udn-vrf
    naming template, use the table number: those vrfs associated to a table
    within our reserved block of table numbers are managed by us. The block
    right now is anything higher than RoutingTableIDStart (1000). This
    allows to manage VRFs with any name which is desirable if the name is
    going to be exposed through BGP.
    
    Signed-off-by: Jaime Caamaño Ruiz <jcaamano@redhat.com>

--- go-controller/pkg/controllermanager/node_controller_manager_test.go
+++ go-controller/pkg/controllermanager/node_controller_manager_test.go
@@ -179,12 +179,12 @@ var _ = Describe("Healthcheck tests", func() {
 
 	Context("verify cleanup of deleted networks", func() {
 		var (
-			staleNetID uint   = 100
+			staleNetID uint   = 1000
 			nodeName   string = "worker1"
 			nad               = ovntest.GenerateNAD("bluenet", "rednad", "greenamespace",
 				types.Layer3Topology, "100.128.0.0/16", types.NetworkRolePrimary)
 			netName      = "bluenet"
-			netID        = 3
+			netID        = 1003
 			v4NodeSubnet = "10.128.0.0/24"
 			v6NodeSubnet = "ae70::66/112"
 			testNS       ns.NetNS
