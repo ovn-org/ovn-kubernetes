24b2bb455229a5f8a223f8b20ce175cd681a40fa
Ram Lavi <ralavi@redhat.com>
2024-12-04 14:51:31 +0200

    test/e2e, multihoming: Check VLAN-ID change on NAD update
    Add tests that make sure that:
    - the N/S connectivity is broken after NAD updating the VLAN-ID.
    - the N/S connectivity is restored after the server networking is
    reconfigured to the new VLAN-ID.
    
    Signed-off-by: Ram Lavi <ralavi@redhat.com>

--- test/e2e/multihoming.go
+++ test/e2e/multihoming.go
@@ -760,12 +760,15 @@ var _ = Describe("Multi Homing", func() {
 						excludedSubnetLowerRange2 = "60.128.0.128/26" // Excludes IPs from 60.128.0.128 to 60.128.0.191
 						excludedSubnetUpperRange1 = "60.128.0.208/28" // Excludes IPs from 60.128.0.208 to 60.128.0.223
 						excludedSubnetUpperRange2 = "60.128.0.224/27" // Excludes IPs from 60.128.0.224 to 60.128.0.255
+						newLocalnetVLANID         = 30
 					)
 					BeforeEach(func() {
 						By("setting new MTU")
 						netConfig.mtu = expectedChangedMTU
 						By("setting new subnets to leave a smaller range")
 						netConfig.excludeCIDRs = []string{excludedSubnetLowerRange1, excludedSubnetLowerRange2, excludedSubnetUpperRange1, excludedSubnetUpperRange2}
+						By("setting new VLAN-ID")
+						netConfig.vlanID = newLocalnetVLANID
 						p := []byte(fmt.Sprintf(`[{"op":"replace","path":"/spec/config","value":%q}]`, generateNADSpec(netConfig)))
 						Expect(patchNADSpec(nadClient, netConfig.name, netConfig.namespace, p)).To(Succeed())
 					})
@@ -796,6 +799,36 @@ var _ = Describe("Multi Homing", func() {
 							Expect(err).NotTo(HaveOccurred())
 							return inRange(newDesiredRange, clientIP)
 						}).Should(Succeed(), "pod's secondary NIC is not allocated in the desired range")
+					})
+
+					It("can no longer communicate over a localnet secondary network from pod to the underlay service", func() {
+						clientPodConfig := podConfiguration{
+							name:        clientPodName,
+							namespace:   f.Namespace.Name,
+							attachments: []nadapi.NetworkSelectionElement{{Name: secondaryNetworkName}},
+						}
+						kickstartPod(cs, clientPodConfig)
+
+						By("asserting the *client* pod can no longer contact the underlay service")
+						Eventually(connectToServer).WithArguments(clientPodConfig, underlayServiceIP, servicePort).Should(MatchError(ContainSubstring("exit code 28")))
+					})
+
+					Context("and the service connected to the underlay is reconfigured to connect to the new VLAN-ID", func() {
+						BeforeEach(func() {
+							Expect(reconfigureVLANAccessPort(nodes, secondaryInterfaceName, newLocalnetVLANID)).To(Succeed())
+						})
+
+						It("can now communicate over a localnet secondary network from pod to the underlay service", func() {
+							clientPodConfig := podConfiguration{
+								name:        clientPodName,
+								namespace:   f.Namespace.Name,
+								attachments: []nadapi.NetworkSelectionElement{{Name: secondaryNetworkName}},
+							}
+							kickstartPod(cs, clientPodConfig)
+
+							By("asserting the *client* pod can contact the underlay service")
+							Eventually(connectToServer).WithArguments(clientPodConfig, underlayServiceIP, servicePort).Should(Succeed())
+						})
 					})
 				})
 
